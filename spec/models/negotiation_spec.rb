4a9a8831e9bd4d6cff64fe58ff60fd7cf34962ac
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 09:54:02 +0100

    añadidos atributos relacionados con token

--- spec/models/negotiation_spec.rb
+++ spec/models/negotiation_spec.rb
@@ -2,24 +2,24 @@
 require 'spec_helper'
 
 describe Negotiation do
-
   describe 'Relations' do
     it { should embed_many :proposals }
     it { should embed_many :messages }
   end
 
   describe 'Attributes' do
-    pending("TODO: Attributes")
     it { should be_timestamped_document }
+    it { should have_field(:token_owner).of_type(Moped::BSON::ObjectId) }
+    it { should have_field(:token_state).of_type(String) }
   end
 
   describe 'Validations' do
     #Relations
-    pending("PREGUNTA: Relations Validations")
     it { should validate_presence_of(:proposals) }
     it { should validate_presence_of(:messages) }
     #Attributes
-    pending("TODO: Attributes Validations")
+    it { should validate_presence_of(:token_owner) }
+    it { should validate_presence_of(:token_state) }
   end
 
   #Behaviour
@@ -27,5 +27,4 @@ describe Negotiation do
     let (:negotiation) { Fabricate(:negotiation) }
     specify { negotiation.should be_valid }
   end
-
 end
