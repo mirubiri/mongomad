3da4e994174d2aed69f3be3f4805e86d1ade8db1
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 11:00:00 +0100

    estetica

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -18,9 +18,9 @@ describe Thing do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :stock }
-    it { should validate_numericality_of(:stock).to_allow(nil:false,
-                                                          only_integer:true,
-                                                          greater_than_or_equal_to:0) }
+    it { should validate_numericality_of(:stock).to_allow(nil: false,
+                                                          only_integer: true,
+                                                          greater_than_or_equal_to: 0) }
   end
 
   #Behaviour
