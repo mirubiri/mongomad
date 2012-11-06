b250d95955b1797fce6841084f0e23769dd713cb
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 10:40:33 +0100

    pegada plantilla de estructura

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,21 @@
 class Request
-  include Mongoid::Document
+
+  #Modules
+  #TODO: Modules (or DELETE)
+
+  #Relations
+  #TODO: Relations (or DELETE)
+
+  #Attributes
+  #TODO: Attributes (or DELETE)
+
+  #Validations (Relations)
+  #TODO: Validations Relations(or DELETE)
+
+  #Validations (Attributes)
+  #TODO: Validations Attributes(or DELETE)
+
+  #Behaviour
+  #TODO: Behaviour (or DELETE)
+
 end
