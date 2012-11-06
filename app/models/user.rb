f9814d8f1ab3b088f0fe6c05c48d298b1e2c7e57
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 12:39:27 +0100

    clase validada. añadido todo lo necesario :P

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
-
   #Modules
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   #Relations
   embeds_one :profile
@@ -12,18 +12,10 @@ class User
   has_and_belongs_to_many :negotiations, inverse_of: nil
   has_and_belongs_to_many :deals, inverse_of: nil
 
-  #Attributes
-  #TODO: Attributes (or DELETE)
-  field :name, type: String
-
   #Validations (Relations)
   validates :profile,
             presence: true
 
-  #Validations (Attributes)
-  #TODO: Validations Attributes(or DELETE)
-
   #Behaviour
   #TODO: Behaviour (or DELETE)
-
 end
