b46161822154e17497de1c1cc8cf98d670d83d19
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 11:04:03 +0100

    clase validada, eliminados atributos antiguos sobrantes y validaciones

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,23 +1,17 @@
 class Thing
-
   #Modules
   include Mongoid::Document
 
   #Relations
   embedded_in :user
 
-
   #Attributes
-  #TODO: Attributes (or DELETE)
   field :name, type: String
   field :description, type: String
   field :stock, type: Integer, default: 1
 
-  #Validations (Relations)
   #Validations (Attributes)
-  #TODO: Validations Attributes(or DELETE)
-  validates :thing_box,
-            :name,
+  validates :name,
             :description,
             :stock,
             presence: true
@@ -25,9 +19,8 @@ class Thing
   validates :stock,
             allow_nil: false,
             numericality: { greater_than_or_equal_to: 0,
-                            only_integer:true }
+                            only_integer: true }
 
   #Behaviour
   #TODO: Behaviour (or DELETE)
-
 end
