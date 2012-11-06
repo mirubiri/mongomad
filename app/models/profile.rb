582c0cae11e4137c7c9dba49ae69ed1e961beb7f
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 12:30:29 +0100

    +atributos y validaciones

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,22 +1,40 @@
 class Profile
-
   #Modules
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   #Relations
   embedded_in :user
 
-
   #Attributes
-  #TODO: Attributes (or DELETE)
-
-  #Validations (Relations)
-
+  field :name, type: String
+  field :surname, type: String
+  field :nickname, type: String
+  field :password, type: String
+  field :sex, type: String
+  field :country, type: String
+  field :flag_url, type: String
+  field :email, type: String
+  field :delivery_address, type: String
+  field :phone_number, type: String
+  field :website, type: String
+  field :birth_date, type: DateTime
 
   #Validations (Attributes)
-  #TODO: Validations Attributes(or DELETE)
+  validates :name,
+            :surname,
+            :nickname,
+            :password,
+            :sex,
+            :country,
+            :flag_url,
+            :email,
+            :delivery_address,
+            :phone_number,
+            :website,
+            :birth_date,
+            presence: true
 
   #Behaviour
   #TODO: Behaviour (or DELETE)
-
 end
