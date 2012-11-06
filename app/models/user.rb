7c230c981172df35ea7fb3f4a107b314fc228d78
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 12:28:20 +0100

    añadidos atributos y validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User
   #Relations
   embeds_one :profile
   embeds_many :things
-  has_and_belongs_to_many :offer_inbox, class_name: "Offer", inverse_of: nil
-  has_and_belongs_to_many :offer_outbox, class_name: "Offer", inverse_of: nil
+  embeds_many :requests
+  has_and_belongs_to_many :sent_offers, class_name: "Offer", inverse_of: nil
+  has_and_belongs_to_many :received_offers, class_name: "Offer", inverse_of: nil
   has_and_belongs_to_many :negotiations, inverse_of: nil
   has_and_belongs_to_many :deals, inverse_of: nil
 
