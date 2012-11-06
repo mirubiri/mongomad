f777a62263b63d442d2d599c7060e13ce6edd3a3
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 09:45:22 +0100

    lineas en blanco borradas

--- spec/models/receiver_spec.rb
+++ spec/models/receiver_spec.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 
 describe Receiver do
-
   describe 'Relations' do
     it { should be_embedded_in :polymorphic_receiver }
     it { should embed_many :products }
@@ -24,5 +23,4 @@ describe Receiver do
     let (:receiver) { Fabricate(:receiver) }
     specify { receiver.should be_valid }
   end
-
 end
