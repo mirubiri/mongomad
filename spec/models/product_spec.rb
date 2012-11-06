90341141e172cdc5a7eb92a151c39780add34b07
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 12:12:49 +0100

    corregir error en validacion ¬¬

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,7 +16,7 @@ describe Product do
 
   describe 'Validations' do
     #Attributes
-    it { should validate_presence_of :thind_id }
+    it { should validate_presence_of :thing_id }
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :quantity }
