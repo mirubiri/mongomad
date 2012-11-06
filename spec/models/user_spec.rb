cf6b8ecb4e82b3c223cb7e87f4952ac395c6d0d1
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 09:37:24 +0100

    renombrado: offer_inbox -> sent_offers, offer_outbox-> received_offers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ describe User do
   describe 'Relations' do
     it { should embed_one :profile }
     it { should embed_many :things }
-    it { should have_and_belong_to_many(:offer_inbox).of_type(Offer).as_inverse_of(nil) }
-    it { should have_and_belong_to_many(:offer_outbox).of_type(Offer).as_inverse_of(nil) }
+    it { should have_and_belong_to_many(:sent_offers).of_type(Offer).as_inverse_of(nil) }
+    it { should have_and_belong_to_many(:received_offers).of_type(Offer).as_inverse_of(nil) }
     it { should have_and_belong_to_many(:negotiations).as_inverse_of(nil) }
     it { should have_and_belong_to_many(:deals).as_inverse_of(nil) }
   end
@@ -27,5 +27,4 @@ describe User do
     let (:user) { Fabricate(:user) }
     specify { user.should be_valid }
   end
-
 end
