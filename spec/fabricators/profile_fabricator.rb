510c2204dea8c6802d87be835167da4d30cad2c8
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-06 14:08:33 +0100

    Cambios en profile y user

--- spec/fabricators/profile_fabricator.rb
+++ spec/fabricators/profile_fabricator.rb
@@ -1,2 +1,13 @@
 Fabricator(:profile) do
+  name 'nombre'
+  surname 'apellido'
+  nickname 'nick'
+  password 'clave'
+  sex 'Hombre'
+  country 'mongolandia'
+  email 'un@correo.es'
+  delivery_address 'c/adcadca N23 7A'
+  phone_number '123 45 67 89'
+  website 'www.hola.com'
+  birth_date '10-10-1998'
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,2 +1,10 @@
 Fabricator(:user) do
+  after_build do |user|
+    #Incrustadas
+    user.profile = Fabricate(:profile)
+    user.things << Fabricate(:thing)
+    user.requests << Fabricate(:request)
+
+    #Relacionadas
+  end
 end
