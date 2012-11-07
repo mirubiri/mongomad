8f95611ba7d1bcf8248a2bde3ac3e584994a5f3f
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-07 11:55:42 +0100

    .

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:negotiation) do
   proposals { [Fabricate.build(:proposal)] }
   messages { [Fabricate.build(:message)] }
-  token_owner  { Fabricate.build(:user)._id }
+  token_owner { Fabricate.build(:user)._id }
   token_state true
 end
