c560a56b545f30d15312477b643659d07cd3f9e5
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-07 14:26:43 +0100

    +validate user presence

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,6 +9,10 @@ class Request
   #Attributes
   field :text, type: String
 
+  #Validations (Relations)
+  validates :user,
+            presence: true
+
   #Validations (Attributes)
   validates :text,
             presence: true
