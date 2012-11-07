60c44cc86dc910c00ff78d43599c93589b77585b
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-07 11:27:42 +0100

    fabrica implementada ok

--- spec/fabricators/offer_fabricator.rb
+++ spec/fabricators/offer_fabricator.rb
@@ -1,2 +1,6 @@
 Fabricator(:offer) do
+  composer { Fabricate.build(:composer) }
+  receiver { Fabricate.build(:receiver) }
+  money { Fabricate.build(:money) }
+  initial_message 'initial message'
 end
