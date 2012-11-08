e6d1b4e52916b048694e5ad4faa312e2848a0d1a
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-08 10:14:13 +0100

    implementacion inicial de polymorphic_money fabricator

--- spec/fabricators/polymorphic_money_fabricator.rb
+++ spec/fabricators/polymorphic_money_fabricator.rb
@@ -1,2 +1,7 @@
 Fabricator(:polymorphic_money) do
+  #Relations
+  offer
+  after_build do |polymorphic_money|
+    polymorphic_money.money = Fabricate.build(:money,polymorphic_money:polymorphic_money)
+  end
 end
