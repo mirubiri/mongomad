d9dc857a739e58cc800bf0d666a0169d36e7cda5
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-08 02:35:43 +0100

    fix: cambiado user.request a user.requests

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:user) do
   after_build do |user|
     user.profile = Fabricate.build(:profile,user:user)
     user.things << Fabricate.build(:thing,user:user)
-    user.request << Fabricate.build(:request,user:user)
+    user.requests << Fabricate.build(:request,user:user)
   end
 end
