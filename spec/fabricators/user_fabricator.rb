1d23ec5bbc3db71bba99a27122230b78a9287167
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-13 03:52:44 +0100

    user_fabricator: Faltaba una 's' en requests

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:user) do
   profile { Fabricate.build(:profile) }
   things(count:1) { Fabricate.build(:thing) }
-  request(count:1) { Fabricate.build(:request) }
+  requests(count:1) { Fabricate.build(:request) }
 end
