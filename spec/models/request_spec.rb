489f1ab34189e66b699923eb35a6ba4f54be5ac5
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-13 14:32:44 +0100

    THING: test completo y clase validada :)

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -19,23 +19,22 @@ describe Request do
   end
 
   describe '#publish' do
-    it 'saves a valid request' do
+    it '>saves a valid request' do
       request=Fabricate.build(:request)
       request.publish.should be_true
     end
-    it 'not saves a not valid request' do
+    it '>not saves a not valid request' do
       request=Fabricate.build(:request,text:nil)
       request.publish.should be_false
     end
   end
 
   describe '#unpublish' do
-    it 'delete a valid request' do
-      Fabricate(:request)
-      request=Request.all.last
+    it '>delete a saved request' do
+      request=Fabricate(:request)
       request.unpublish.should be_true
     end
-    it 'not delete a not published request' do
+    it '>not delete a not saved request' do
       request=Fabricate.build(:request)
       request.unpublish.should be_false
    end
