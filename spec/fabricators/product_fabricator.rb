d624d08b358cd242e082525018b54823fa956517
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-13 07:44:00 +0100

    Fabricators:minor fixes

--- spec/fabricators/product_fabricator.rb
+++ spec/fabricators/product_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:product) do
-  polymorphic_product { Fabricate(:composer,products:nil) }
+  polymorphic_product { Fabricate.build(:composer,products:nil,polymorphic_composer:nil) }
   thing_id 'an existing thing_id'
   name 'name'
   description 'description'
--- spec/fabricators/proposal_fabricator.rb
+++ spec/fabricators/proposal_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:proposal) do
   polymorphic_proposal { Fabricate.build(:negotiation,proposals:nil) }
   money nil
-  composer { Fabricate.build(:composer) }
-  receiver { Fabricate.build(:receiver) }
+  composer { Fabricate.build(:composer,polymorphic_composer:nil) }
+  receiver { Fabricate.build(:receiver,polymorphic_receiver:nil) }
 end
