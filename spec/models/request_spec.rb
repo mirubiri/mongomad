c49c987873c9d76ec30c1fc8712d2fc536afe698
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-14 00:38:54 +0100

    Test refactor to standard notation

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -19,22 +19,22 @@ describe Request do
   end
 
   describe '#publish' do
-    it '>saves a valid request' do
+    it 'saves a valid request' do
       request=Fabricate.build(:request)
       request.publish.should be_true
     end
-    it '>not saves a not valid request' do
+    it 'not saves a not valid request' do
       request=Fabricate.build(:request,text:nil)
       request.publish.should be_false
     end
   end
 
   describe '#unpublish' do
-    it '>delete a saved request' do
+    it 'delete a saved request' do
       request=Fabricate(:request)
       request.unpublish.should be_true
     end
-    it '>not delete a not saved request' do
+    it 'not delete a not saved request' do
       request=Fabricate.build(:request)
       request.unpublish.should be_false
    end
