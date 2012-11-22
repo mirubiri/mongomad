19a8a846c9bd77728f6fabe92a2ce6aef70731ab
Medico <medico@medico.(none)>
2012-11-22 12:47:27 +0100

    reestructirados test. misma estructura que model

---
+++ spec/models/deal/agreement/message_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Message do
+  let(:message) { Fabricate.build(:message) }
+  after(:each) { message && message.polymorphic_message.destroy }
+
+  describe 'Relations' do
+    it { should be_embedded_in :polymorphic_message }
+  end
+
+  describe 'Attributes' do
+    it { should be_timestamped_document }
+    it { should have_field(:sender_id).of_type(Moped::BSON::ObjectId) }
+    it { should have_fields(:sender_name,
+                            :text)
+                            .of_type(String) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :polymorphic_message }
+    it { should validate_presence_of :sender_id }
+    it { should validate_presence_of :sender_name }
+    it { should validate_presence_of :text }
+  end
+
+  xit "Cada mensaje debe ir acompanado del avatar del usuario que lo escribe"
+
+  describe 'Factories' do
+
+    specify { message.should be_valid }
+    specify { message.save.should be_true }
+  end
+end
---
+++ spec/models/negotiation/offer/receiver/image_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Image do
+  let(:image) { Fabricate.build(:image) }
+  after(:each) do
+    image.destroy
+  end
+  describe 'Relations' do
+    it { should be_embedded_in :polymorphic_image }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :polymorphic_image }
+    it { should validate_presence_of :file }
+  end
+
+  describe 'Factories' do
+    specify { expect(image.valid?).to be_true }
+    specify { expect(image.save).to be_true }
+  end
+end
