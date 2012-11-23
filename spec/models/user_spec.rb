0cf2a748c9cda94a284fc5239cb57637b047f1f3
Medico <medico@medico.(none)>
2012-11-23 14:14:41 +0100

    modificado usuario con la nueva monstructura

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,13 @@ describe User do
   after(:each) { user && user.destroy }
 
   describe 'Relations' do
-    it { should embed_one :profile }
-    it { should embed_many :things }
-    it { should have_and_belong_to_many(:requests).as_inverse_of(nil) }
+    it { should embed_one(:profile).of_type(User::Profile) }
+    it { should embed_many(:things).of_type(User::Thing) }
+    it { should have_and_belong_to_many(:requests).of_type(Request).as_inverse_of(nil) }
     it { should have_and_belong_to_many(:sent_offers).of_type(Offer).as_inverse_of(nil) }
     it { should have_and_belong_to_many(:received_offers).of_type(Offer).as_inverse_of(nil) }
-    it { should have_and_belong_to_many(:negotiations).as_inverse_of(nil) }
-    it { should have_and_belong_to_many(:deals).as_inverse_of(nil) }
+    it { should have_and_belong_to_many(:negotiations).of_type(Negotiation).as_inverse_of(nil) }
+    it { should have_and_belong_to_many(:deals).of_type(Deal).as_inverse_of(nil) }
   end
 
   describe 'Attributes' do
@@ -23,7 +23,7 @@ describe User do
   end
 
   describe 'Factories' do
-    specify { expect(user.valid?).to be_true }
-    specify { expect(user.save).to be_true,"A ver que tenemos #{user.errors.messages}" }
+    specify { user.should be_valid }
+    specify { user.save.should be_true }
   end
 end
