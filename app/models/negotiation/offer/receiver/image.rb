557dd336b789dc501d99773b04b93b36998f0d90
Medico <medico@medico.(none)>
2012-11-23 14:05:44 +0100

    Reestructurados modelos y test de modelos. (faltan las fabricas:P)

--- app/models/negotiation/offer/receiver/image.rb
+++ app/models/negotiation/offer/receiver/image.rb
@@ -1,20 +1,20 @@
-class Negotiation::Offer::Receiver::Image
-  include Mongoid::Document
-  include Mongoid::Paperclip
+class Negotiation::Offer::Composer::Image
+    include Mongoid::Document
+    include Mongoid::Paperclip
 
-  embedded_in :product
+    embedded_in :product
 
-  has_mongoid_attached_file :file
+    has_mongoid_attached_file :file
 
-  #,
-    #:styles => {
-      #:original => ['1920x1680>', :jpg],
-      #:small    => ['100x100#',   :jpg],
-      #:medium   => ['250x250',    :jpg],
-      #:large    => ['500x500>',   :jpg]
-    #}
+    #,
+      #:styles => {
+        #:original => ['1920x1680>', :jpg],
+        #:small    => ['100x100#',   :jpg],
+        #:medium   => ['250x250',    :jpg],
+        #:large    => ['500x500>',   :jpg]
+      #}
 
-  validates :file,
-            :product,
-            presence: true
+    validates :file,
+              :product,
+              presence: true
 end
