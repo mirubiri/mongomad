47fa7c55156d879f789f966d928be9db1bd97afe
Sergio <sergio.detorre@improcex.com>
2012-11-27 13:12:36 +0100

    RoutesSpec -> user & user/offers done

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,7 +1,10 @@
 require "spec_helper"
 
 describe UsersController do
+
   describe "routing" do
+
+    #rutas del usuario a pelo, xD
 
     it "routes to #index" do
       get("/users").should route_to("users#index")
@@ -20,7 +23,7 @@ describe UsersController do
     end
 
     it "routes to #create" do
-      post("/users").should route_to("users#create")
+      post("/users").should route_to("devise/registrations#create")
     end
 
     it "routes to #update" do
@@ -30,6 +33,38 @@ describe UsersController do
     it "routes to #destroy" do
       delete("/users/1").should route_to("users#destroy", :id => "1")
     end
+
+    #rutas de las ofertas de un usuario
+
+    it "routes to users/offers#index" do
+      get("/users/1/offers").should route_to("offers#index",  :user_id => "1")
+    end
+
+    it "routes to users/offers#new" do
+      get("/users/1/offers/new").should route_to("offers#new",  :user_id => "1")
+    end
+
+    it "routes to users/offers#show" do
+      get("/users/1/offers/1").should route_to("offers#show",  :user_id => "1",:id => "1")
+    end
+
+    it "routes to users/offers#edit" do
+      get("/users/1/offers/1/edit").should route_to("offers#edit", :user_id => "1",:id => "1")
+    end
+
+    it "routes to users/offers#create" do
+      post("/users/1/offers").should route_to("offers#create",  :user_id => "1")
+    end
+
+    it "routes to users/offers#update" do
+      put("/users/1/offers/1").should route_to("offers#update", :user_id => "1",:id => "1")
+    end
+
+    it "routes to users/offers#destroy" do
+      delete("/users/1/offers/1").should route_to("offers#destroy",  :user_id => "1",:id => "1")
+    end
+
+
 
   end
 end
