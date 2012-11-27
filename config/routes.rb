8891900f6a92a16349c24b5c67c83c9b69c57d66
Sergio <sergio.detorre@improcex.com>
2012-11-27 12:46:51 +0100

    Agregadas rutas principales de la plataforma

--- config/routes.rb
+++ config/routes.rb
@@ -1,74 +1,30 @@
 Mongomad::Application.routes.draw do
 
+  #Devise gestiona a los usuarios y sus sesiones
   devise_for :users
 
-  resources :deals, :offers, :negotiations, :requests
+  #La ruta por defecto sera a la pantalla inicial de la plataforma
+  root :to => "devise/registrations#new"  
 
-  devise_scope :user do
-    get "/", :to => "devise/sessions#new"
+  #Recursos del user
+  resources :users do
+    resources :profile do
+      resources :image
+    end
+    resources :requests 
+    resources :things do
+      resources :images
+    end   
+    resources :offers do
+      resources :images, :composer, :receiver, :money
+    end
+    resources :negotiations do
+      resources :proposals, :messages
+    end
+    resources :deals do
+      resources :agreement, :messages
+    end   
   end
 
-  authenticated :user do
-    root :to => "users#show"
-  end
-
-  root :to => "devise/sessions#new"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 
 end
