981d396107b341d95915d1709be9b37a8166d13e
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-27 11:01:49 +0100

    cambios en las clases product (en todas)

--- spec/models/deal/agreement/proposal/receiver/product_spec.rb
+++ spec/models/deal/agreement/proposal/receiver/product_spec.rb
@@ -6,7 +6,7 @@ describe Deal::Agreement::Proposal::Receiver::Product do
 
   describe 'Relations' do
     it { should be_embedded_in(:receiver).of_type(Deal::Agreement::Proposal::Receiver) }
-    it { should embed_one(:secondary_image).of_type(Deal::Agreement::Proposal::Receiver::Product::Image) }
+    it { should embed_manyt(:secondary_images).of_type(Deal::Agreement::Proposal::Receiver::Product::Image) }
   end
 
   describe 'Attributes' do
@@ -19,7 +19,6 @@ describe Deal::Agreement::Proposal::Receiver::Product do
 
   describe 'Validations' do
     it { should validate_presence_of :receiver }
-    it { should validate_presence_of :secondary_image }
     it { should validate_presence_of :thing_id }
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
