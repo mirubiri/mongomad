0beba12ee82635a9962ead4a3864e0bb978c360d
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 11:24:11 +0100

    fabrica negotiation_proposal_composer_product_image completa

--- spec/fabricators/negotiation/proposal/composer/negotiation_proposal_composer_product_image_fabricator.rb
+++ spec/fabricators/negotiation/proposal/composer/negotiation_proposal_composer_product_image_fabricator.rb
@@ -1,4 +1,4 @@
-Fabricator(:image) do
-  polymorphic_image { Fabricate.build(:product,main_image:nil) }
+Fabricator(:negotiation_proposal_composer_product_image, class_name: "Negotiation::Proposal::Composer::Product::Image") do
+  product nil
   file { File.open('app/assets/images/rails.png') }
 end
