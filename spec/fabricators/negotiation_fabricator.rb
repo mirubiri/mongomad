b35d3ee437f13463a81a9f1daa75eae6e765a652
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 10:54:22 +0100

    fabrica negotiation completada

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -1,6 +1,4 @@
 Fabricator(:negotiation) do
-  offers(count:1) { Fabricate.build(:offer,polymorphic_offer:nil,composer:Fabricate.build(:composer,_id:'el id de composer'),receiver:Fabricate.build(:receiver,_id:'el id de receiver')) }
-  messages(count:1) { Fabricate.build(:message,polymorphic_message:nil) }
-  token_user_id 'an existing token_user_id'
-  token_state true
+  proposals nil
+  messages  nil
 end
