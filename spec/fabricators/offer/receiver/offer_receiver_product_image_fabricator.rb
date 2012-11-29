35fe2bb382e6873b57bf9c40e220b102784b0f42
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 11:10:49 +0100

    offer_receiver_product_image offer_receiver_product_image offer_receiver_product_image fabrica offer_receiver_product_image completa

--- spec/fabricators/offer/receiver/offer_receiver_product_image_fabricator.rb
+++ spec/fabricators/offer/receiver/offer_receiver_product_image_fabricator.rb
@@ -1,4 +1,4 @@
-Fabricator(:image) do
-  polymorphic_image { Fabricate.build(:product,main_image:nil) }
+Fabricator(:offer_receiver_product_image, class_name: "Offer::Receiver::Product::Image") do
+  product nil
   file { File.open('app/assets/images/rails.png') }
 end
