ce9da84aca738811435422a27260a00892388419
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 11:41:56 +0100

    fabrica deal_agreement completada

--- spec/fabricators/deal/deal_agreement_fabricator.rb
+++ spec/fabricators/deal/deal_agreement_fabricator.rb
@@ -1,5 +1,5 @@
-Fabricator(:agreement) do
-  deal { Fabricate.build(:deal,agreement:nil) }
-  offers(count:1) { Fabricate.build(:offer,composer:Fabricate.build(:composer,_id:'user_id'),
-                                                  receiver:Fabricate.build(:receiver,_id:'user_id')) }
+Fabricator(:deal_agreement, class_name: "Deal::Agreement") do
+  deal nil
+  proposals nil
+  messages  nil
 end
