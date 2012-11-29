bbbb2e191d4aadbea377f5e59cda60fc1b315fcc
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 10:53:14 +0100

    fabrica request completa

--- spec/fabricators/request_fabricator.rb
+++ spec/fabricators/request_fabricator.rb
@@ -1,5 +1,6 @@
-Fabricator(:user_request, class_name: 'User::Request') do
-  user_id 'an existing user_id'
-  user_name 'owner name'
-  text 'text'
+Fabricator(:request) do
+  user_id   'user_id'
+  user_name 'user_name'
+  text      'text'
+  image     nil
 end
