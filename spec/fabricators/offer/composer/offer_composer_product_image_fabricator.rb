bb91f143f1b3fca5ed05efa4744fde5dab423f52
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 11:11:19 +0100

    fabrica offer_composer_product_image completa

--- spec/fabricators/offer/composer/offer_composer_product_image_fabricator.rb
+++ spec/fabricators/offer/composer/offer_composer_product_image_fabricator.rb
@@ -1,4 +1,4 @@
-Fabricator(:image) do
-  polymorphic_image { Fabricate.build(:product,main_image:nil) }
+Fabricator(:offer_composer_product_image, class_name: "Offer::Composer::Product::Image") do
+  product nil
   file { File.open('app/assets/images/rails.png') }
 end
