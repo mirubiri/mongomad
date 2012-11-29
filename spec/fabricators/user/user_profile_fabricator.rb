59747f07a176b3ca4c5268f42ad99cd64f3bd47a
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 10:57:41 +0100

    fabrica user_profile completa

--- spec/fabricators/user/user_profile_fabricator.rb
+++ spec/fabricators/user/user_profile_fabricator.rb
@@ -1,13 +1,13 @@
-Fabricator(:user_profile, class_name: 'User::Profile') do
-  user nil
-  name {Faker::Name.first_name}
-  surname {Faker::Name.last_name}
-  nickname {Faker::Internet.user_name }
-  sex 'men'
-  country {Faker::Address.country}
-  birth_date '10-10-2000'
-  website nil
+Fabricator(:user_profile, class_name: "User::Profile") do
+  user             nil
+  name             {Faker::Name.first_name}
+  surname          {Faker::Name.last_name}
+  nickname         {Faker::Internet.user_name }
+  sex              'man'
+  country          {Faker::Address.country}
   delivery_address nil
-  phone_number nil
-  image {Fabricate.build(:image,polymorphic_image:nil)}
+  phone_number     nil
+  website          nil
+  birth_date      '10-10-2000'
+  image            nil
 end
