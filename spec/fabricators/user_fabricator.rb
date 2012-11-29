a650a92d8a7488ba15af9e65caf66e248d9d1e72
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 10:48:06 +0100

    fabrica user completa

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,11 +1,11 @@
 Fabricator(:user) do
   profile nil
   things nil
-  email { Faker::Internet.email }
-  password 'password'
-  received_offers nil
+  request nil
   sent_offers nil
+  received_offers nil
   negotiations nil
   deals nil
-  request nil
+  email { Faker::Internet.email }
+  encrypted_password 'encrypted_password'
 end
