589381270fb42474f15a84253e6b3d5b9fb8a655
Alejandro Arrufat <mirubiri@gmail.com>
2012-11-29 11:06:48 +0100

    fabrica offer_money completada

--- spec/fabricators/offer/offer_money_fabricator.rb
+++ spec/fabricators/offer/offer_money_fabricator.rb
@@ -1,5 +1,5 @@
-Fabricator(:money) do
-  polymorphic_money { Fabricate.build(:offer) }
-  user_id 'an exiting user_id'
+Fabricator(:offer_money, class_name: "Offer::Money") do
+  offer nil
+  user_id 'user_id'
   quantity 100
 end
