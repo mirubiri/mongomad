7795bd3e61f78fd1f76e35f4bf6255537565cab3
Medico <voldemortthegreat@gmail.com>
2012-12-04 13:46:00 +0100

    .

--- app/models/deal/agreement/proposal.rb
+++ app/models/deal/agreement/proposal.rb
@@ -3,9 +3,9 @@ class Deal::Agreement::Proposal
   include Mongoid::Timestamps
 
   embedded_in :agreement, class_name: "Deal::Agreement"
-  embeds_one :composer, class_name: "Deal::Agreement::Proposal::Composer", cascade_callbacks: true
-  embeds_one :receiver, class_name: "Deal::Agreement::Proposal::Receiver", cascade_callbacks: true
-  embeds_one :money, class_name: "Deal::Agreement::Proposal::Money", cascade_callbacks: true
+  embeds_one :composer,   class_name: "Deal::Agreement::Proposal::Composer", cascade_callbacks: true
+  embeds_one :receiver,   class_name: "Deal::Agreement::Proposal::Receiver", cascade_callbacks: true
+  embeds_one :money,      class_name: "Deal::Agreement::Proposal::Money", cascade_callbacks: true
 
   validates :agreement,
             :composer,
