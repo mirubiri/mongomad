c52bcbbb875f5cdbae64ff5252cc13e57fc0eac5
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-04 14:58:11 +0100

    Added _ to :id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all.to_a
     @requests = User.requests
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:_id])
 
     respond_to do |format|
       format.html show.html.erb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(params[:_id])
   end
 
   # POST /users
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    @user = User.find(params[:_id])
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
+    @user = User.find(params[:_id])
     @user.destroy
 
     respond_to do |format|
