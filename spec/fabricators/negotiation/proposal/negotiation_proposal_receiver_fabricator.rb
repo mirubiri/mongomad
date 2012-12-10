fd6f1e7877866cd599cba142fe0624f9ea266e94
Medico <voldemortthegreat@gmail.com>
2012-12-10 13:01:39 +0100

    cambiado valor image

--- spec/fabricators/negotiation/proposal/negotiation_proposal_receiver_fabricator.rb
+++ spec/fabricators/negotiation/proposal/negotiation_proposal_receiver_fabricator.rb
@@ -3,5 +3,5 @@ Fabricator(:negotiation_proposal_receiver, class_name: "Negotiation::Proposal::R
   products(count:1) { Fabricate.build(:negotiation_proposal_receiver_product) }
   user_id           'user_id'
   name              'name'
-  image             { File.open('app/assets/images/rails.png') }
+  image             'app/assets/images/rails.png'
 end
