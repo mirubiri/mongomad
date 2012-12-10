158297ca10fdf5683270fbb82df822bcbaea02a2
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:35:15 +0100

    cambiado   field :image,   type: String

--- app/models/negotiation/proposal/composer.rb
+++ app/models/negotiation/proposal/composer.rb
@@ -1,13 +1,12 @@
 class Negotiation::Proposal::Composer
   include Mongoid::Document
-  include Mongoid::Paperclip
 
   embedded_in :proposal, class_name: "Negotiation::Proposal"
   embeds_many :products, class_name: "Negotiation::Proposal::Composer::Product", cascade_callbacks: true
 
   field :user_id, type: Moped::BSON::ObjectId
   field :name,    type: String
-  has_mongoid_attached_file :image
+  field :image,   type: String
 
   validates :proposal,
             :products,
