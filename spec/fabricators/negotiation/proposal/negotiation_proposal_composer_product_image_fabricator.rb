b968327a54990a23f17c7448c10f6ecae16369b2
Medico <voldemortthegreat@gmail.com>
2012-12-10 13:03:06 +0100

    cambiado valor de file

--- spec/fabricators/negotiation/proposal/negotiation_proposal_composer_product_image_fabricator.rb
+++ spec/fabricators/negotiation/proposal/negotiation_proposal_composer_product_image_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:negotiation_proposal_composer_product_image, class_name: "Negotiation::Proposal::Composer::Product::Image") do
   product nil
-  file    { File.open('app/assets/images/rails.png') }
+  file    'app/assets/images/rails.png'
 end
