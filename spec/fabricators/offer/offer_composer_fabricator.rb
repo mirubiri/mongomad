26d5fd61a1f1129cbc456cf8fb9203dc2d098104
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:58:54 +0100

    cambiado valor image

--- spec/fabricators/offer/offer_composer_fabricator.rb
+++ spec/fabricators/offer/offer_composer_fabricator.rb
@@ -3,5 +3,5 @@ Fabricator(:offer_composer, class_name: "Offer::Composer") do
   products(count:1) { Fabricate.build(:offer_composer_product) }
   user_id           'user_id'
   name              'name'
-  image             { File.open('app/assets/images/rails.png') }
+  image             'app/assets/images/rails.png'
 end
