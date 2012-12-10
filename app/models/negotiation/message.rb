c676fc25b7ac2cb78ed591544e0c37effea0a914
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:39:31 +0100

    cambiado   field :image,  type: String

--- app/models/negotiation/message.rb
+++ app/models/negotiation/message.rb
@@ -1,19 +1,18 @@
 class Negotiation::Message
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include Mongoid::Paperclip
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-    embedded_in :negotiation
+  embedded_in :negotiation
 
-    field :user_id,   type: Moped::BSON::ObjectId
-    field :user_name, type: String
-    field :text,      type: String
-    has_mongoid_attached_file :image
+  field :user_id,   type: Moped::BSON::ObjectId
+  field :user_name, type: String
+  field :text,      type: String
+  field :image,     type: String
 
-    validates :negotiation,
-              :user_id,
-              :user_name,
-              :text,
-              :image,
-              presence: true
+  validates :negotiation,
+            :user_id,
+            :user_name,
+            :text,
+            :image,
+            presence: true
 end
