be1c247b56340e48c49d0764cbac4380d9270708
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:44:40 +0100

    añadido test

--- spec/models/offer/offer_receiver_product_image_spec.rb
+++ spec/models/offer/offer_receiver_product_image_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Offer::Receiver::Product::Image do
   let(:image) { Fabricate.build(:offer).receiver.products[0].secondary_images[0] }
 
-
   describe 'Relations' do
     it { should be_embedded_in(:product).of_type(Offer::Receiver::Product) }
   end
 
   describe 'Attributes' do
-    # TODO: ¿Validar campo 'file' (Paperclip)?
+    it { should have_field(:file).of_type(String) }
   end
 
   describe 'Validations' do
