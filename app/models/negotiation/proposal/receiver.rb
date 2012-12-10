bc5197e47b27c45df98b18fa560cc700763de67e
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:35:00 +0100

    eliminado paperclip

--- app/models/negotiation/proposal/receiver.rb
+++ app/models/negotiation/proposal/receiver.rb
@@ -1,6 +1,5 @@
 class Negotiation::Proposal::Receiver
   include Mongoid::Document
-  include Mongoid::Paperclip
 
   embedded_in :proposal, class_name: "Negotiation::Proposal"
   embeds_many :products, class_name: "Negotiation::Proposal::Receiver::Product", cascade_callbacks: true
