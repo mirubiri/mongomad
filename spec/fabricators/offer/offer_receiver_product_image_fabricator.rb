a09f5b8c40ca9005a86a6b30ab5d288e40ad928d
Medico <voldemortthegreat@gmail.com>
2012-12-10 13:00:19 +0100

    cambiado valor file

--- spec/fabricators/offer/offer_receiver_product_image_fabricator.rb
+++ spec/fabricators/offer/offer_receiver_product_image_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:offer_receiver_product_image, class_name: "Offer::Receiver::Product::Image") do
   product nil
-  file    { File.open('app/assets/images/rails.png') }
+  file    'app/assets/images/rails.png'
 end
