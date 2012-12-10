e35d874f15a079fa59450d9ffbda908a6fbe7f0a
Medico <voldemortthegreat@gmail.com>
2012-12-10 12:31:08 +0100

    cambiado nombre campo

--- app/models/offer/receiver.rb
+++ app/models/offer/receiver.rb
@@ -4,9 +4,9 @@ class Offer::Receiver
   embedded_in :offer
   embeds_many :products, class_name: "Offer::Receiver::Product", cascade_callbacks: true
 
-  field :user_id,    type: Moped::BSON::ObjectId
-  field :name,       type: String
-  field :main_image, type: String
+  field :user_id, type: Moped::BSON::ObjectId
+  field :name,    type: String
+  field :image,   type: String
 
   validates :offer,
             :products,
