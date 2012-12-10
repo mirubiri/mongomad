8c4fe0648e4cd656daf4ad5e8c474e1406d8c61d
Medico <voldemortthegreat@gmail.com>
2012-12-10 13:05:33 +0100

    .

--- app/models/deal/agreement/message.rb
+++ app/models/deal/agreement/message.rb
@@ -1,19 +1,19 @@
 class Deal::Agreement::Message
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include Mongoid::Paperclip
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Paperclip
 
-    embedded_in :agreement, class_name: "Deal::Agreement"
+  embedded_in :agreement, class_name: "Deal::Agreement"
 
-    field :user_id,   type: Moped::BSON::ObjectId
-    field :user_name, type: String
-    field :text,      type: String
-    has_mongoid_attached_file :image
+  field :user_id,   type: Moped::BSON::ObjectId
+  field :user_name, type: String
+  field :text,      type: String
+  has_mongoid_attached_file :image
 
-    validates :agreement,
-              :user_id,
-              :user_name,
-              :text,
-              :image,
-              presence: true
+  validates :agreement,
+            :user_id,
+            :user_name,
+            :text,
+            :image,
+            presence: true
 end
