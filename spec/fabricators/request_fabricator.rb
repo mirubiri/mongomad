1d923e97140dd71a6610af42a511b4ce6cdc684c
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-20 11:59:24 +0100

    arreglada también la fabrica de requests (liada entre si era ruta o fichero :P)

--- spec/fabricators/request_fabricator.rb
+++ spec/fabricators/request_fabricator.rb
@@ -2,5 +2,12 @@ Fabricator(:request) do
   user_id   'user_id'
   user_name 'user_name'
   text      'el texto de mi peticion que es un poco largo'
-  image     { File.open('app/assets/images/rails.png') }
+  image     'image_path'
+
+  after_build do |request|
+
+    image_path='/images/rails.png'
+    request.image=image_path
+
+  end
 end
