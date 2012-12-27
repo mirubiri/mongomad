38222ed235886d63d3d9c44bef375c1b91262af2
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-27 10:36:13 +0100

    added 1 request to user fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,11 +1,11 @@
 Fabricator(:user) do
-  profile         { Fabricate.build(:user_profile) }
-  things(count:1) { Fabricate.build(:user_thing) }
-  requests        { Fabricate.build(:request) }
-  sent_offers     nil
-  received_offers nil
-  negotiations    nil
-  deals           nil
-  email           { Faker::Internet.email }
-  password        'password'
+  profile           { Fabricate.build(:user_profile) }
+  things(count:1)   { Fabricate.build(:user_thing) }
+  requests(count:1) { Fabricate.build(:request) }
+  sent_offers       nil
+  received_offers   nil
+  negotiations      nil
+  deals             nil
+  email             { Faker::Internet.email }
+  password          'password'
 end
