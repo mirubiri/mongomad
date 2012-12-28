7244d0ccdbb3d4024a935dcd6101e5da6da1e502
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-28 10:31:25 +0100

    fabrica reducida al minimo

--- spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_composer_product_fabricator.rb
+++ spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_composer_product_fabricator.rb
@@ -1,9 +1,9 @@
 Fabricator(:deal_agreement_proposal_composer_product, class_name: "Deal::Agreement::Proposal::Composer::Product") do
-  composer                  nil
-  secondary_images(count:1) { Fabricate.build(:deal_agreement_proposal_composer_product_image) }
-  thing_id                  'thing_id'
-  name                      'name'
-  description               'description'
-  quantity                  5
-  main_image                'main_image_path'
+  composer         nil
+  secondary_images nil
+  thing_id         'thing_id'
+  name             'name'
+  description      'description'
+  quantity         10
+  main_image       'main_image_path'
 end
