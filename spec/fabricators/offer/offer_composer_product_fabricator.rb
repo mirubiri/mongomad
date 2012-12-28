9c30719cb6886f70c0f4c36cd422b3c22305ceac
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-28 10:24:12 +0100

    .

--- spec/fabricators/offer/offer_composer_product_fabricator.rb
+++ spec/fabricators/offer/offer_composer_product_fabricator.rb
@@ -4,6 +4,6 @@ Fabricator(:offer_composer_product, class_name: "Offer::Composer::Product") do
   thing_id         'thing_id'
   name             'name'
   description      'description'
-  quantity            10
+  quantity         10
   main_image       'main_image_path'
 end
