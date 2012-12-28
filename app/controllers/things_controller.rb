101a1eeea8ccf065c970c40fa07bbe02d2ec714d
Sergio <sergio.detorre@improcex.com>
2012-12-28 13:51:52 +0100

    Agregada la seccion de productos sin funcionalidad de momento

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,2 +1,92 @@
 class ThingsController < ApplicationController
+  # GET /things
+  # GET /things.json
+
+  def sub_layout
+    "things"
+  end
+
+  def index
+    @user = user_logged
+    @things = @user.things.to_a
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json # renders index.js.erb
+    end
+  end
+
+  # GET /things/1
+  # GET /things/1.json
+  def show
+    @thing = Thing.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @thing }
+    end
+  end
+
+  # GET /things/new
+  # GET /things/new.json
+  def new
+    @thing = Thing.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @thing }
+    end
+  end
+
+  # GET /things/1/edit
+  def edit
+    @thing = Thing.find(params[:id])
+  end
+
+  # POST /things
+  # POST /things.json
+  def create
+    @thing = Thing.new(params[:thing])
+
+    respond_to do |format|
+      if @thing.save
+        format.html { redirect_to @thing, notice: 'thing was successfully created.' }
+        format.json { render json: @thing, status: :created, location: @thing }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @thing.errors,
+          status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /things/1
+  # PUT /things/1.json
+  def update
+    @thing = Thing.find(params[:id])
+
+    respond_to do |format|
+      if @thing.update_attributes(params[:thing])
+        format.html { redirect_to @thing, notice: 'thing was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @thing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /things/1
+  # DELETE /things/1.json
+  def destroy
+    @thing = Thing.find(params[:id])
+    @thing.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_things_url }
+      format.json { head :no_content }
+    end
+  end
+
+
 end
