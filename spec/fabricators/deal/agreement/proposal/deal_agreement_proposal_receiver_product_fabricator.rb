234ce01b025547e2fa2097c8ea511f0d7fb04409
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-28 10:31:55 +0100

    fabrica reducida al minimo

--- spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_receiver_product_fabricator.rb
+++ spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_receiver_product_fabricator.rb
@@ -1,9 +1,9 @@
 Fabricator(:deal_agreement_proposal_receiver_product, class_name: "Deal::Agreement::Proposal::Receiver::Product") do
-  receiver                  nil
-  secondary_images(count:1) { Fabricate.build(:deal_agreement_proposal_receiver_product_image) }
-  thing_id                  'thing_id'
-  name                      'name'
-  description               'description'
-  quantity                  5
-  main_image                'main_image_path'
+  receiver         nil
+  secondary_images nil
+  thing_id         'thing_id'
+  name             'name'
+  description      'description'
+  quantity         10
+  main_image       'main_image_path'
 end
