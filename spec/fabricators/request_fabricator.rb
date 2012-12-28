baeee0e373b183fb9a95888859be484bb7b84242
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-28 10:19:11 +0100

    Fabricator(:request) reducida al minimo

--- spec/fabricators/request_fabricator.rb
+++ spec/fabricators/request_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:request) do
   user_id   'user_id'
   user_name 'user_name'
-  text      'el texto de mi peticion que es un poco largo'
+  text      'el texto de mi peticion que es un poco largo y ocupa mas de una linea'
   image     'image_path'
 end
