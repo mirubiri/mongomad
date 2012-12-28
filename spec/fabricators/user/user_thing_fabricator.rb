5b473bc72944a3f0209d90e2e481915cb6698794
Alejandro Arrufat <mirubiri@gmail.com>
2012-12-28 10:21:56 +0100

    fabrica reducida al minimo

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -1,8 +1,8 @@
 Fabricator(:user_thing, class_name: "User::Thing") do
-  user                      nil
-  secondary_images(count:1) { Fabricate.build(:user_thing_image) }
-  name                      'name'
-  description               'description'
-  stock                     10
-  main_image                'main_image_path'
+  user             nil
+  secondary_images nil
+  name             'name'
+  description      'description'
+  stock            10
+  main_image       'main_image_path'
 end
