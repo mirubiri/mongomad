4f1b6406231e84d5c595a78b8be4c2427cbca2e5
Medico <voldemortthegreat@gmail.com>
2013-01-02 14:55:34 +0100

    corrected

--- spec/fabricators/user/user_thing_image_fabricator.rb
+++ spec/fabricators/user/user_thing_image_fabricator.rb
@@ -1,13 +1,9 @@
 Fabricator(:user_thing_image, class_name: "User::Thing::Image") do
-  thing do
-
-          Fabricate.build(:user_thing,secondary_images:nil)
-        end
+  thing { Fabricate.build(:user_thing,secondary_images:nil) }
   file  { File.open('app/assets/images/rails.png') }
 
   after_build do |image|
     image.thing &&
     image.thing.main_image = image.thing.secondary_images.last.file.url
   end
-
 end
