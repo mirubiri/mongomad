4603971208d87acf54012bb311a9b6821903d011
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-03 11:49:19 +0100

    fabricas de usuario completas y tests pasados

--- spec/fabricators/user/user_thing_image_fabricator.rb
+++ spec/fabricators/user/user_thing_image_fabricator.rb
@@ -1,9 +1,9 @@
 Fabricator(:user_thing_image, class_name: "User::Thing::Image") do
-  thing { Fabricate.build(:user_thing,secondary_images:nil) }
+  thing { Fabricate.build(:user_thing, images:nil) }
   file  { File.open('app/assets/images/rails.png') }
 
   after_build do |image|
     image.thing &&
-    image.thing.main_image = image.thing.secondary_images.last.file.url
+    image.thing.main_image = image.thing.images.last.file.url
   end
 end
