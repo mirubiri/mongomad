a4e0cd0bae2cd4864bb88ad97723d5d55c2883f8
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-03 11:44:59 +0100

    .

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -1,9 +1,9 @@
 Fabricator(:user_thing, class_name: "User::Thing") do
-  user                      { Fabricate.build(:user, things:nil) }
-  secondary_images(count:1) { Fabricate.build(:user_thing_image, thing:nil) }
-  name                      'name'
-  description               'description'
-  stock                     10
-  main_image                { |attrs| attrs[:secondary_images] &&
-                                      attrs[:secondary_images].last.file.url }
+  user            { Fabricate.build(:user, things:nil) }
+  images(count:1) { Fabricate.build(:user_thing_image, thing:nil) }
+  name            'name'
+  description     'description'
+  stock           10
+  main_image      { |attrs| attrs[:images] &&
+                            attrs[:images].last.file.url }
 end
