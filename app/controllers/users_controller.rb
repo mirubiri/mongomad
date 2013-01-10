6744fef90595023b51fe3fe82698ab076c225043
Sergio <sergio.detorre@improcex.com>
2013-01-10 14:25:15 +0100

    Filtrado el controller de User, no necesito buscar a @user pq ya lo tengo en el entorno del index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,6 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.all.to_a
-    @requests = User.requests
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
