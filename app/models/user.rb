86d57e665270e4f05caf7fde65784cb4d68911e6
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-10 13:04:53 +0100

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  embeds_one :profile, class_name: "User::Profile", cascade_callbacks: true
-  embeds_many :things, class_name: "User::Thing", cascade_callbacks: true
+  embeds_one  :profile, class_name: "User::Profile", cascade_callbacks: true
+  embeds_many :things,  class_name: "User::Thing", cascade_callbacks: true
 
-  has_and_belongs_to_many :requests, inverse_of: nil
-  has_and_belongs_to_many :sent_offers, class_name: "Offer", inverse_of: nil
+  has_and_belongs_to_many :requests,        inverse_of: nil
+  has_and_belongs_to_many :sent_offers,     class_name: "Offer", inverse_of: nil
   has_and_belongs_to_many :received_offers, class_name: "Offer", inverse_of: nil
-  has_and_belongs_to_many :negotiations, inverse_of: nil
-  has_and_belongs_to_many :deals, inverse_of: nil
+  has_and_belongs_to_many :negotiations,    inverse_of: nil
+  has_and_belongs_to_many :deals,           inverse_of: nil
 
   validates :profile,
             presence: true
