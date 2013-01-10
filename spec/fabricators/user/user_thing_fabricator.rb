3378ea0f4b23fe1abc13f713761b457a7162ec45
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-10 12:57:13 +0100

    descripcion más larga para pruebas de interfaz

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:user_thing, class_name: "User::Thing") do
   user            nil
   images(count:1) { Fabricate.build(:user_thing_image) }
   name            'name'
-  description     'description'
+  description     'this is the thing\'s description. this is the most beautiful thing you have ever seen! if you need it, it\'s yours!'
   stock           1
   main_image      { |attrs| attrs[:images].last.file.url }
 end
