e2f499a17c9a08971fe56c4ac9cf7245ba7a079c
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-14 12:30:54 +0100

    fabricas offer arregaladas. User y offer perfect

--- spec/fabricators/offer/offer_fabricator.rb
+++ spec/fabricators/offer/offer_fabricator.rb
@@ -1,24 +1,11 @@
 Fabricator(:offer) do
-  composer        nil
-  receiver        nil
+  users(count: 2) { Fabricate.build(:user_with_things) }
+  composer        { |attrs| Fabricate.build(:offer_composer, user:attrs[:users].first) }
+  receiver        { |attrs| Fabricate.build(:offer_composer, user:attrs[:users].last) }
   money           nil
   initial_message 'this is offer\'s initial message. it can\'t be too long but has to be long enought to try the interface.'
-
-  after_build do |offer|
-    user_composer = Fabricate(:user_with_things)
-    offer.composer = Fabricate.build(:offer_composer, user:user_composer)
-    user_composer.sent_offers << offer
-
-  	user_receiver = Fabricate(:user_with_things)
-    offer.receiver = Fabricate.build(:offer_receiver, user:user_receiver)
-    user_receiver.received_offers << offer
-  end
 end
 
 Fabricator(:offer_with_money, from: :offer) do
-  money { Fabricate.build(:offer_money, user_id:nil) }
-
-  after_build do |offer|
-    offer.money.user_id = offer.composer._id
-  end
+  money { |attrs| Fabricate.build(:offer_money, user_id:attrs[:users].first._id) }
 end
