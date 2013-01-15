e870ec06b8d8c8b1dc46ac665ba09ef8474640b7
Sergio <sergio.detorre@improcex.com>
2013-01-15 14:48:49 +0100

    modificada la negociacion para que aparezca con sus campos bien rellenados a partir de las fabricas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @offers = @user.received_offers
+    @offers = @user.offers.all
+    @request = Request.new
 
     respond_to do |format|
       format.html # show.html.erb
