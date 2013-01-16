758f4b69a70b60a7b473efe788535a8375fe4c44
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-16 10:32:04 +0100

    Eliminado el build

--- spec/fabricators/negotiation/negotiation_fabricator.rb
+++ spec/fabricators/negotiation/negotiation_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:negotiation) do
   transient           :offer
-  offer               { Fabricate.build(:offer) }
+  offer               { Fabricate(:offer) }
   users               { |attrs| attrs[:offer].users }
   proposals(count: 1) { |attrs| Fabricate.build(:negotiation_proposal, offer:attrs[:offer]) }
   messages(count: 1)  { |attrs| Fabricate.build(:negotiation_message, offer:attrs[:offer]) }
