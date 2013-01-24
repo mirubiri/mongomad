ee52ea3896da29425a774274d1ed15f2b64890dc
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-24 11:44:58 +0100

    cambiada la imagen para que coja la que le viene

--- spec/fabricators/deal/agreement/deal_agreement_message_fabricator.rb
+++ spec/fabricators/deal/agreement/deal_agreement_message_fabricator.rb
@@ -3,5 +3,5 @@ Fabricator(:deal_agreement_message, class_name: "Deal::Agreement::Message") do
   agreement nil
   user_name { |attrs| attrs[:message].user_name }
   text      'this is an old negotiation\'s message. it was written by one of the users when they were negotiating.'
-  image     { File.open('app/assets/images/message.png') }
+  image     { |attrs| File.open(attrs[:message].image.path) }
 end
