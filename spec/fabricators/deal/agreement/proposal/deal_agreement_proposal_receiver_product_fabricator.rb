85d97466e3165736c1b2c3238c38ab9503ba247d
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-24 11:47:43 +0100

    Esteticién 2 :P

--- spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_receiver_product_fabricator.rb
+++ spec/fabricators/deal/agreement/proposal/deal_agreement_proposal_receiver_product_fabricator.rb
@@ -5,5 +5,5 @@ Fabricator(:deal_agreement_proposal_receiver_product, class_name: "Deal::Agreeme
   name        { |attrs| attrs[:product].name }
   description { |attrs| attrs[:product].description }
   quantity    { |attrs| attrs[:product].quantity }
-  image       { File.open('app/assets/images/car.png') }
+  image       { |attrs| File.open(attrs[:product].image.path) }
 end
--- spec/fabricators/negotiation/negotiation_fabricator.rb
+++ spec/fabricators/negotiation/negotiation_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:negotiation) do
   transient :offer
   offer               { Fabricate(:offer) }
-  users               { |attrs| [ attrs[:offer].user_composer,attrs[:offer].user_receiver ] }
+  users               { |attrs| [ attrs[:offer].user_composer, attrs[:offer].user_receiver ] }
   proposals(count: 1) { |attrs| Fabricate.build(:negotiation_proposal, offer:attrs[:offer]) }
-  messages(count: 1)  { |attrs| Fabricate.build(:negotiation_message, composer:attrs[:offer].composer) }
+  messages(count: 1)  { |attrs| Fabricate.build(:negotiation_message, composer:attrs[:offer].user_composer) }
 end
