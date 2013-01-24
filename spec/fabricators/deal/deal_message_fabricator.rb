0396b9614d6bfdb8494cbab4f86ecb4eddfe1071
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-24 11:44:02 +0100

    cambiada image para que coja la que le viene

--- spec/fabricators/deal/deal_message_fabricator.rb
+++ spec/fabricators/deal/deal_message_fabricator.rb
@@ -3,5 +3,5 @@ Fabricator(:deal_message, class_name: "Deal::Message") do
   deal      nil
   user_name { |attrs| attrs[:message].user_name }
   text      'this is the first deal\'s message. it\'s written by the users when they close a deal.'
-  image     { File.open('app/assets/images/message.png') }
+  image     { |attrs| File.open(attrs[:message].image.path) }
 end
