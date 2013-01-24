93d284d47d218f0e7d31d438e3aade2857253efe
Sergio <sergio.detorre@improcex.com>
2013-01-24 09:36:09 +0100

    agregada la funcionalidad para eliminar peticiones

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -87,7 +87,7 @@ class RequestsController < ApplicationController
     @request.destroy
 
     respond_to do |format|
-      format.html { redirect_to requests_url }
+      format.html { redirect_to user_offers_url }
       format.json { head :no_content }
     end
   end
