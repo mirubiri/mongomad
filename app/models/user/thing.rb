72e0b6f3f25c2b0498e5706e07273db427aab4c6
Sergio <sergio.detorre@improcex.com>
2013-01-28 14:34:08 +0100

    test 'to_product' pasados

--- app/models/user/thing.rb
+++ app/models/user/thing.rb
@@ -20,4 +20,16 @@ class User::Thing
             allow_nil: false,
             numericality: { only_integer: true,
                             greater_than_or_equal_to: 0 }
+
+  def to_product(quantity)
+    raise StandardError, 'Quantity not valid' if quantity <= 0
+    raise StandardError, 'Not enough stock avaliable' if self.stock < quantity 
+    product = Offer::Composer::Product.new
+    product.thing_id = self._id
+    product.name = self.name
+    product.description = self.description
+    product.quantity = quantity
+    product.image = File.open(self.image.path) 
+    product
+  end                           
 end
