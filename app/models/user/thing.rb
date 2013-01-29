e9ae1d89430aa225fa259d4996e15a3d0312e9b4
Alejandro Arrufat <mirubiri@gmail.com>
2013-01-29 12:24:37 +0100

    añadidos to_offer_composer_product y to_offer_receiver_product

--- app/models/user/thing.rb
+++ app/models/user/thing.rb
@@ -21,15 +21,27 @@ class User::Thing
             numericality: { only_integer: true,
                             greater_than_or_equal_to: 0 }
 
-  def to_product(quantity)
+  def to_offer_composer_product(quantity)
     raise StandardError, 'Quantity not valid' if quantity <= 0
-    raise StandardError, 'Not enough stock avaliable' if self.stock < quantity 
+    raise StandardError, 'Not enough stock avaliable' if self.stock < quantity
     product = Offer::Composer::Product.new
     product.thing_id = self._id
     product.name = self.name
     product.description = self.description
     product.quantity = quantity
-    product.image = File.open(self.image.path) 
+    product.image = File.open(self.image.path)
     product
-  end                           
+  end
+
+  def to_offer_receiver_product(quantity)
+    raise StandardError, 'Quantity not valid' if quantity <= 0
+    raise StandardError, 'Not enough stock avaliable' if self.stock < quantity
+    product = Offer::Receiver::Product.new
+    product.thing_id = self._id
+    product.name = self.name
+    product.description = self.description
+    product.quantity = quantity
+    product.image = File.open(self.image.path)
+    product
+  end
 end
