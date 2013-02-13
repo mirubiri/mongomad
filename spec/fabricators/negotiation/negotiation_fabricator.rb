0e8ee5e9fbb072db2e0693fdd09d076eca3aec48
Alejandro Arrufat <mirubiri@gmail.com>
2013-02-13 14:02:16 +0100

    cambiado token_state de cadena a symbol

--- spec/fabricators/negotiation/negotiation_fabricator.rb
+++ spec/fabricators/negotiation/negotiation_fabricator.rb
@@ -5,5 +5,5 @@ Fabricator(:negotiation) do
   proposals(count: 1) { |attrs| Fabricate.build(:negotiation_proposal, offer:attrs[:offer]) }
   messages(count: 1)  { |attrs| Fabricate.build(:negotiation_message, composer:attrs[:offer].composer) }
   token_user_id       { |attrs| attrs[:users].first._id }
-  token_state         "propose"
+  token_state         :propose
 end
