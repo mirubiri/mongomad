ca49d13ebaf385ecc693bd8222c0578c3cde21a2
Alejandro Arrufat <mirubiri@gmail.com>
2013-02-15 14:22:03 +0100

    .

--- spec/fabricators/request/request_fabricator.rb
+++ spec/fabricators/request/request_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:request) do
-  user       { Fabricate(:user) }
-  user_name  { |attrs| attrs[:user].profile.nickname }
+  user       { Fabricate(:user)                        }
+  user_name  { |attrs| attrs[:user].profile.nickname   }
   text       "this is a request's text. it's a long text and it's more than one line long to try our beautiful interface."
   image_name { |attrs| attrs[:user].profile.image_name }
 end
