801049c519268f171e037b702012cae36a2a75a2
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-07 13:26:34 +0100

    add random description

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:user_thing, class_name: "User::Thing") do
   user        nil
   name        'name'
-  description 'thing description'
+  description { Faker::Lorem.sentence(word_count=10) }
   stock       5
   image       { File.open('app/assets/images/car.png') }
   image_name  'car.png'
