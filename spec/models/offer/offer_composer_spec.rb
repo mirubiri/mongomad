7969c6183cdf89f5e5c4faa39d43d1d65dc786c1
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-08 12:17:01 +0100

    update offer_composer tests

--- spec/models/offer/offer_composer_spec.rb
+++ spec/models/offer/offer_composer_spec.rb
@@ -50,29 +50,82 @@ describe Offer::Composer do
     end
 
     it 'raise exception if any parameter is not correct' do
-
-      thing = products_params[:composer_things].first
-      products_params[:composer_things][products_params[:composer_things].length]
-            request.publish
-      expect { request.publish }.to raise_error
+      products_params[:composer_things].first = nil
+      expect { composer.add_products(products_params) }.to raise_error
     end
 
-    it 'calls to self_update' do
+    it 'calls to self_update method' do
       composer.should_receive(:self_update!)
       composer.add_products(products_params)
     end
 
+    it 'calls update_user_data method' do
+      composer.should_receive(:update_user_data)
+      composer.add_products(products_params)
+    end
 
-
-    xit 'add all products in a single operation'
-
-
+    it 'calls update_products method' do
+      composer.should_receive(:update_products)
+      composer.add_products(products_params)
+    end
   end
 
+  describe '#self_update!' do
+    let(:new_composer) do
+      new_composer = composer.dup
+    end
+
+    it 'returns self it self_update! success' do
+      new_composer.self_update!
+      new_composer.should be_like composer
+    end
+
+    it 'raise error if self_update! fails' do
+      new_composer.offer.user_composer = nil
+      expect { new_composer.self_update! }.to raise_error
+    end
+
+    it 'returns a valid composer' do
+      new_composer.self_update!
+      new_composer.should be_valid
+    end
+
+    it 'updates composer name with the current user_composer name' do
+      new_composer.offer.user_composer.profile.stub(:name).and_return('updated')
+      new_composer.self_update!
+      new_composer.name.should eq 'updated'
+    end
+
+    it 'updates composer image_name with the current user_composer image_name' do
+      new_composer.offer.user_composer.profile.stub(:image_name).and_return('updated.png')
+      new_composer.self_update!
+      new_composer.image_name.should eq 'updated.png'
+    end
+
+    context 'When composer(offer) is published' do
+      before { new_composer.offer.publish }
+
+      it 'calls reload method' do
+        new_composer.should_receive(:reload)
+        new_composer.self_update!
+      end
+
+      it 'save the changes' do
+        new_composer.should_receive(:save)
+        new_composer.self_update!
+      end
+    end
+
+    context 'When composer(offer) is not published' do
+      it 'does not save the changes' do
+        new_composer.should_not_receive(:save)
+        new_composer.self_update!
+      end
+    end
+  end
 
 =begin
-  describe '#self_update' do
-    it 'calls update_user_data' do
+  it 'calls update_user_data' do
       # Use relationship.target to access to the wrapped object
       composer.target.should_receive(:update_user_data)
       composer.self_update
@@ -94,8 +147,6 @@ describe Offer::Composer do
       composer.self_update
       composer.image_name.should eq 'updated.png'
     end
-  end
-
 
   describe '#alter_contents(params)' do
     after { composer.alter_contents(composer_things_params) }
@@ -116,9 +167,5 @@ describe Offer::Composer do
   describe '#update_products' do
     xit 'update the information of all products'
   end
-
-
-
-
 =end
 end
