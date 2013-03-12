581ad082c7cff00e1d63adfdc2e6f2b83c4fe74e
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-12 13:08:01 +0100

    steticien (error -> exception)

--- spec/models/request/request_spec.rb
+++ spec/models/request/request_spec.rb
@@ -141,7 +141,7 @@ describe Request do
       new_request.should be_like request
     end
 
-    it 'raises error if self_update! fails' do
+    it 'raises exception if self_update! fails' do
       request.user = nil
       expect { request.self_update! }.to raise_error
     end
