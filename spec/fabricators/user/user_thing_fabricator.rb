c7a98e079b92f74b27c36af98097c83c8a4a4d42
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-13 10:58:37 +0100

    add random name for thing and change car pic :P

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -1,10 +1,10 @@
 Fabricator(:user_thing, class_name: "User::Thing") do
   user        nil
-  name        'name'
-  description { Faker::Lorem.sentence(word_count=10) }
+  name        { Faker::Lorem.word }
+  description { Faker::Lorem.sentence(word_count=15) }
   stock       5
-  image       { File.open('app/assets/images/car.png') }
-  image_name  'car.png'
+  image       { File.open('app/assets/images/monkey.png') }
+  image_name  'monkey.png'
 
   after_build do |thing|
     thing.image.store!
