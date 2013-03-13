e09554827fcf794364d1dd57e87891dd1a1f6447
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-13 14:30:17 +0100

    update and implement offer_composer tests

--- spec/models/offer/offer_composer_spec.rb
+++ spec/models/offer/offer_composer_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Offer::Composer do
-  xlet(:user_composer) { Fabricate(:user_with_things) }
-  xlet(:user_receiver) { Fabricate(:user_with_things) }
-  xlet(:offer) { Fabricate.build(:offer, user_composer:user_composer, user_receiver:user_receiver) }
-  xlet(:composer) { offer.composer }
-  xlet(:products_params) { params_for_offer(offer)[:composer_things] }
-  xlet(:thing) { User.where('things._id' => Moped::BSON::ObjectId(products_params.last[:thing_id])).first.things.find(products_params.last[:thing_id]) }
+  let(:offer) { Fabricate.build(:offer,
+    user_composer:Fabricate(:user_with_things),
+    user_receiver:Fabricate(:user_with_things))
+  }
+  let(:composer) { offer.composer }
+  let(:products_params) { params_for_offer(offer)[:composer_things] }
 
   describe 'Relations' do
     it { should be_embedded_in :offer }
@@ -40,53 +40,73 @@ describe Offer::Composer do
     end
   end
 
-
-
   describe '#add_products(products_params)' do
-#comprobar las composer ids
-
-    it 'add a product to composer from each parameter passed' do
+    it 'add a list of products from given parameters' do
       new_composer = offer.composer.clone
       new_composer.products.destroy
       new_composer.add_products(products_params)
       new_composer.products.should be_like composer.products
     end
 
-    it 'does not persist the composer' do
-      composer.should_not be_persisted
-    end
-
-    it 'raises exception if any product thing_id is nil' do
-      products_params.last[:thing_id] = nil
-      expect { composer.add_products(products_params) }.to raise_error
-    end
-
-    it 'raises exception if any product thing does not belong to correct user' do
-      products_params.last[:thing_id] = user_receiver.things.last._id
-      expect { composer.add_products(products_params) }.to raise_error
-    end
-
-    it 'raises exception if any product quantity is not correct' do
-      products_params.last[:quantity] = thing.stock + 1
-      expect { composer.add_products(products_params) }.to raise_error
-    end
-
     it 'calls to self_update method' do
       composer.should_receive(:self_update!)
       composer.add_products(products_params)
     end
+
+    it 'returns a valid composer' do
+      composer.add_products(products_params)
+      composer.should be_valid
+    end
+
+    it 'does not persist the composer' do
+      composer.should_not be_persisted
+    end
+
+    it 'raises exception if self_update! fails' do
+      composer.stub(:self_update!).and_raise("StandardError")
+      expect { composer.self_update! }.to raise_error
+    end
+  end
+
+  describe '#alter_contents(products_params)' do
+    it 'removes the current list of products' do
+      composer.products.should_receive(:destroy)
+      composer.alter_contents(products_params)
+    end
+
+    it 'add a new list of products from the given parameters' do
+      composer.should_receive(:add_products).with(products_params)
+    end
   end
 
   describe '#self_update!' do
-    it 'returns self if self_update! success' do
-      new_composer = composer.dup
-      new_composer.self_update!
-      new_composer.should be_like composer
+    it 'updates name with the current user_composer name' do
+      composer.name.stub(:name).and_return('updated')
+      composer.self_update!
+      composer.name.should eq 'updated'
     end
 
-    it 'raise error if self_update! fails' do
-      composer.offer.user_composer = nil
-      expect { composer.self_update! }.to raise_error
+    it 'updates image_name with the current user_composer image_name' do
+      composer.image_name.stub(:image_name).and_return('updated.png')
+      composer.self_update!
+      composer.composer.image_name.should eq 'updated.png'
+    end
+
+    it 'calls update_user_data method' do
+      composer.should_receive(:update_user_data)
+      composer.self_update!
+    end
+
+    it 'calls update_products method' do
+      composer.should_receive(:update_products)
+      composer.self_update!
+    end
+
+    it 'calls to product.self_update! method for each product' do
+      composer.product.each do |product|
+        product.should_receive(:self_update!)
+      end
+      composer.self_update!
     end
 
     it 'returns a valid composer' do
@@ -94,31 +114,27 @@ describe Offer::Composer do
       composer.should be_valid
     end
 
-    it 'updates composer name with the current user_composer name' do
-      composer.offer.user_composer.profile.stub(:name).and_return('updated')
-      composer.self_update!
-      composer.name.should eq 'updated'
+    it 'returns self if self_update! success' do
+      new_composer = composer.dup
+      new_composer.self_update!
+      new_composer.should be_like composer
     end
 
-    it 'updates composer image_name with the current user_composer image_name' do
-      composer.offer.user_composer.profile.stub(:image_name).and_return('updated.png')
-      composer.self_update!
-      composer.image_name.should eq 'updated.png'
+    it 'raises exception if self_update! fails' do
+      composer.stub(:self_update!).and_raise("StandardError")
+      expect { composer.self_update! }.to raise_error
     end
 
-    it 'calls to self_update method' do
-      composer.should_receive(:self_update!)
-      composer.add_products(products_params)
+    it 'raises exception if user_composer is nil' do
+      composer.offer.user_composer = nil
+      expect { composer.self_update! }.to raise_error
     end
 
-    it 'calls update_user_data method' do
-      composer.should_receive(:update_user_data)
-      composer.add_products(products_params)
-    end
-
-    it 'calls update_products method' do
-      composer.should_receive(:update_products)
-      composer.add_products(products_params)
+    it 'raises exception if user_composer is not correct' do
+      user = Fabricate.build(:user)
+      composer.offer.user_composer = user
+      user.destroy
+      expect { composer.self_update! }.to raise_error
     end
 
     context 'When offer is published' do
@@ -129,7 +145,7 @@ describe Offer::Composer do
         composer.self_update!
       end
 
-      it 'save the changes' do
+      it 'save changes' do
         composer.should_receive(:save)
         composer.self_update!
       end
@@ -141,55 +157,10 @@ describe Offer::Composer do
         composer.self_update!
       end
 
-      it 'does not save the changes' do
+      it 'does not save changes' do
         composer.should_not_receive(:save)
         composer.self_update!
       end
     end
   end
-
-=begin
-  it 'calls update_user_data' do
-      # Use relationship.target to access to the wrapped object
-      composer.target.should_receive(:update_user_data)
-      composer.self_update
-    end
-
-    it 'calls update_products' do
-      composer.target.should_receive(:update_products)
-      composer.self_update
-    end
-
-    it 'updates composer name with the current user composer name' do
-      composer.offer.user_composer.profile.stub(:name).and_return('updated')
-      composer.self_update
-      composer.name.should eq 'updated'
-    end
-
-    it 'updates composer image_name with the current user composer image_name' do
-      composer.offer.user_composer.profile.stub(:image_name).and_return('updated.png')
-      composer.self_update
-      composer.image_name.should eq 'updated.png'
-    end
-
-  describe '#alter_contents(params)' do
-    after { composer.alter_contents(composer_things_params) }
-
-    it 'removes the current list of products' do
-      composer.products.should_receive(:destroy)
-    end
-
-    it 'add the given list of products' do
-      composer.target.should_receive(:add_products).with(composer_things_params)
-    end
-
-    it 'calls to self_update' do
-      pending 'pensar si llamarlo o no'
-    end
-  end
-
-  describe '#update_products' do
-    xit 'update the information of all products'
-  end
-=end
 end
