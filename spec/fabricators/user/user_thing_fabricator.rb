a7f1343487ef5d643b69e2fe19a8e72ae9ce2058
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-14 10:29:47 +0100

    beautician xD

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:user_thing, class_name: "User::Thing") do
   user        nil
   name        { Faker::Lorem.word }
-  description { Faker::Lorem.sentence(word_count=15) }
+  description { Faker::Lorem.sentence(word_count = 15) }
   stock       5
   image       { File.open('app/assets/images/monkey.png') }
   image_name  'monkey.png'
