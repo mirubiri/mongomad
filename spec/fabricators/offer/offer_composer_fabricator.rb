a7953ee6756fd739481a00bd27ceceb1e1c5bbad
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 14:20:19 +0100

    change name to nickname attribute

--- spec/fabricators/offer/offer_composer_fabricator.rb
+++ spec/fabricators/offer/offer_composer_fabricator.rb
@@ -8,6 +8,6 @@ Fabricator(:offer_composer, class_name: "Offer::Composer") do
     end
     products
   end
-  name       { |attrs| attrs[:user].profile.name }
+  nickname       { |attrs| attrs[:user].profile.nickname }
   image_name { |attrs| attrs[:user].profile.image_name }
 end
