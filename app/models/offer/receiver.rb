03448b21ef22f9f9e65c82248c31ae4c333ca5eb
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 14:22:17 +0100

    change name to nickname attribute change name attribute to nickname

--- app/models/offer/receiver.rb
+++ app/models/offer/receiver.rb
@@ -5,17 +5,17 @@ class Offer::Receiver
   embedded_in :offer
   embeds_many :products, class_name: "Offer::Receiver::Product", cascade_callbacks: true
 
-  field :name, type: String
+  field :nickname, type: String
 
   mount_uploader :image, ProductImageUploader, :mount_on => :image_name
 
   accepts_nested_attributes_for :products
 
-  denormalize :name, :image_name, from:'offer.user_receiver.profile'
+  denormalize :nickname, :image_name, from:'offer.user_receiver.profile'
 
   validates :offer,
     :products,
-    :name,
+    :nickname,
     :image_name,
     presence: true
 end
