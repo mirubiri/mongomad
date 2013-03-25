bae4a7c131a4216e5df452188932773cc812c46c
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 13:23:44 +0100

    remove deprecated code

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -23,42 +23,4 @@ class Offer
 
   validates :initial_message,
     length: { minimum: 1, maximum: 160 }
-
-=begin
-  def self.generate(offer_params=[])
-    offer = new(
-      user_receiver: User.find(offer_params[:user_receiver_id]),
-      initial_message: offer_params[:initial_message]
-    )
-    offer.build_composer.add_products(offer_params[:composer_things])
-    offer.build_receiver.add_products(offer_params[:receiver_things])
-    offer.build_money(user_id: offer_params[:money][:user_id],
-                      quantity: offer_params[:money][:quantity])
-    offer
-  end
-
-
-  def alter_contents(offer_params=[])
-    composer.alter_contents(offer_params[:composer_things])
-    receiver.alter_contents(offer_params[:receiver_things])
-    money.alter_contents(offer_params[:money])
-    self.initial_message = offer_params[:initial_message]
-    true
-  end
-
-  def publish
-    save
-  end
-
-  def unpublish
-    destroy
-  end
-
-  def self_update
-    reload if persisted?
-    receiver.self_update
-    composer.self_update
-    self
-  end
-=end
 end
