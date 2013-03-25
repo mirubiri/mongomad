b1cea5ae64af784cce4751eb7bfb0e2717fa8a88
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 14:08:36 +0100

    remove deprecated test and comment other methosd

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -2,51 +2,19 @@ class Negotiation
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  embeds_many :proposals,   class_name: "Negotiation::Proposal", cascade_callbacks: true
-  embeds_one :conversation, class_name: "Negotiation::Conversation", cascade_callbacks:true
-
   has_and_belongs_to_many :negotiators, class_name: "User"
 
-  validates :proposals,
+  embeds_one  :conversation, class_name: "Negotiation::Conversation", cascade_callbacks: true
+  embeds_many :proposals,    class_name: "Negotiation::Proposal", cascade_callbacks: true
+
+  accepts_nested_attributes_for :negotiators, :conversation, :proposals
+
+  validates :negotiators,
+    :proposals,
     :conversation,
-    :negotiators,
     presence: true
 
 =begin
-  def self.generate(offer)
-    proposal_params = self.proposal_params_from_offer(offer)
-    message_params = self.message_params_from_offer(offer)
-
-    negotiation = Negotiation.new(
-      conversation: Negotiation::Conversation.new()
-    )
-
-    negotiation.propose(proposal_params)
-    negotiation.post_message(message_params)
-    negotiation.negotiators << offer.composer
-    negotiation.negotiators << offer.receiver
-    negotiation
-    #negotiation.self_update
-  end
-
-  def publish
-    save
-  end
-
-  def unpublish
-    destroy
-  end
-
-  def self_update
-    reload if persisted?
-    proposals.each do |proposal|
-      proposal.self_update
-    end
-    conversation.self_update
-    self
-  end
-=end
-
   def kick(negotiator)
     if negotiators.count >= 2
       negotiators.delete(negotiator)
@@ -68,4 +36,5 @@ class Negotiation
     self.conversation.messages << message
     self.publish
   end
+=end
 end
