7b8a8bea2b756f842c16230c3cb40b1e55a2c460
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 14:09:24 +0100

    comment name and image filds and add nested atributtes

--- app/models/negotiation/conversation.rb
+++ app/models/negotiation/conversation.rb
@@ -4,26 +4,21 @@ class Negotiation::Conversation
   embedded_in :negotiation
   embeds_many :messages, class_name: "Negotiation::Conversation::Message", cascade_callbacks: true
 
-  field :starter_negotiator_name,  type: String
-  field :follower_negotiator_name, type: String
+  # field :starter_negotiator_name,  type: String
+  # field :follower_negotiator_name, type: String
 
   # mount_uploader :starter_image,  ProductImageUploader, :mount_on => :starter_negotiator_image_name
   # mount_uploader :follower_image, ProductImageUploader, :mount_on => :follower_negotiator_image_name
 
+  accepts_nested_attributes_for :messages
+
+  # denormalize :name, :image_name, from:''
+
   validates :negotiation,
     :messages,
-    :starter_negotiator_name,
-    :follower_negotiator_name,
-    #:starter_negotiator_image_name,
-    #:follower_negotiator_image_name,
+    # :starter_negotiator_name,
+    # :follower_negotiator_name,
+    # :starter_negotiator_image_name,
+    # :follower_negotiator_image_name,
     presence: true
-=begin
-  def self_update
-    reload if persisted?
-    messages.each do |message|
-      message.self_update
-    end
-    self
-  end
-=end
 end
