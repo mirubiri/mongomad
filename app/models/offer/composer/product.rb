6783aaa600c97fa30acd331dcc5deeaf5c54a202
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-25 13:25:19 +0100

    remove deprecated code

--- app/models/offer/composer/product.rb
+++ app/models/offer/composer/product.rb
@@ -28,39 +28,4 @@ class Offer::Composer::Product
   def thing
     User.where('things._id' => thing_id).first.things.find(self.thing_id)
   end
-
-=begin
-  def self.generate(params)
-    Offer::Composer::Product.new(
-      thing_id:params[:thing_id],
-      quantity:params[:quantity])
-  end
-
-  def alter_contents(params)
-    if persisted?
-      self.quantity = params[:quantity]
-      save
-    else
-      false
-    end
-  end
-
-  def self_update!
-    users = User.where('things._id' => Moped::BSON::ObjectId(self.thing_id))
-    raise "thing is not valid" unless users.count == 1
-    user = users.first
-
-    things = user.things.where('_id' => Moped::BSON::ObjectId(self.thing_id))
-    raise "thing is not valid" unless things.count == 1
-    thing = things.first
-
-    raise "owner thing is not correct" unless composer.offer.user_composer._id == thing.user._id
-
-    reload if persisted?
-    self.name = thing.name
-    self.description = thing.description
-    self.image_name = thing.image_name
-    persisted? ? save : self
-  end
-=end
 end
