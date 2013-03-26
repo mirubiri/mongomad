042d7a2036b4a5d105f36586a4cbc2df42bf80bd
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-26 12:08:21 +0100

    comment all conversation attributes (need think more :P)

--- app/models/negotiation/conversation.rb
+++ app/models/negotiation/conversation.rb
@@ -4,21 +4,22 @@ class Negotiation::Conversation
   embedded_in :negotiation
   embeds_many :messages, class_name: "Negotiation::Conversation::Message", cascade_callbacks: true
 
-  # field :starter_negotiator_name,  type: String
-  # field :follower_negotiator_name, type: String
+  # field :starter_name,  type: String
+  # field :follower_name, type: String
 
-  # mount_uploader :starter_image,  ProductImageUploader, :mount_on => :starter_negotiator_image_name
-  # mount_uploader :follower_image, ProductImageUploader, :mount_on => :follower_negotiator_image_name
+  # mount_uploader :starter_image,  ProductImageUploader, :mount_on => :starter_image_name
+  # mount_uploader :follower_image, ProductImageUploader, :mount_on => :follower_image_name
 
   accepts_nested_attributes_for :messages
 
-  # denormalize :name, :image_name, from:''
+  # denormalize :name, :image_name, from:'negotiators.first.profile'
+  # denormalize :name, :image_name, from:'negotiators.last.profile'
 
   validates :negotiation,
     :messages,
-    # :starter_negotiator_name,
-    # :follower_negotiator_name,
-    # :starter_negotiator_image_name,
-    # :follower_negotiator_image_name,
+    # :starter_name,
+    # :follower_name,
+    # :starter_image_name,
+    # :follower_image_name,
     presence: true
 end
