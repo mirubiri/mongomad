63d059b60ea25ae81f5fdb32e962f61b0fb692c1
Alejandro Arrufat <mirubiri@gmail.com>
2013-03-26 12:16:49 +0100

    change name to nickname, add denormalized and nested attributes

--- app/models/negotiation/proposal/composer.rb
+++ app/models/negotiation/proposal/composer.rb
@@ -1,16 +1,21 @@
 class Negotiation::Proposal::Composer
   include Mongoid::Document
+  include Denormalized
 
   embedded_in :proposal, class_name: "Negotiation::Proposal"
   embeds_many :products, class_name: "Negotiation::Proposal::Composer::Product", cascade_callbacks: true
 
-  field :name, type: String
+  field :nickname, type: String
 
   mount_uploader :image, UserImageUploader, :mount_on => :image_name
 
+  accepts_nested_attributes_for :products
+
+  denormalize :nickname, :image_name
+
   validates :proposal,
     :products,
-    :name,
+    :nickname,
     :image_name,
     presence: true
 =begin
