f4dc6868ab9a092a0b24404ef0a566cf18fa23cf
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-03 12:38:22 +0200

    remove deprecated code

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -13,28 +13,4 @@ class Negotiation
     :proposals,
     :conversation,
     presence: true
-
-=begin
-  def kick(negotiator)
-    if negotiators.count >= 2
-      negotiators.delete(negotiator)
-    else
-      finish
-    end
-  end
-
-  def propose(proposal_params=[])
-    proposal = Negotiation::Proposal.generate(proposal_params)
-    self.proposals << proposal
-    self.publish
-  end
-
-  def post_message(message_params=[])
-    message = Negotiation::Proposal.new()
-      #initial_message:message_params[:initial_message]
-
-    self.conversation.messages << message
-    self.publish
-  end
-=end
 end
