e5ae8cce4bf6cb4f4cfc8422ce1fe44306b4c9be
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-03 12:35:36 +0200

    .

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -3,8 +3,8 @@ Fabricator(:user_thing, class_name: "User::Thing") do
   name        { Faker::Lorem.word }
   description { Faker::Lorem.sentence(word_count = 15) }
   stock       5
-  image       { File.open('app/assets/images/monkey.png') }
-  image_name  'monkey.png'
+  image       { File.open('app/assets/images/house.png') }
+  image_name  'house.png'
 
   after_build do |thing|
     thing.image.store!
