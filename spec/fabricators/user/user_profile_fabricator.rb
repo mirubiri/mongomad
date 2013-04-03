08c3673fbc361cfa663bbfbcd33d03bf95374207
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-03 13:32:54 +0200

    add two new profile fabricators :)

--- spec/fabricators/user/user_profile_fabricator.rb
+++ spec/fabricators/user/user_profile_fabricator.rb
@@ -16,3 +16,33 @@ Fabricator(:user_profile, class_name: "User::Profile") do
     profile.image.store!
   end
 end
+
+Fabricator(:user_sergio_profile, from: "User::Profile") do
+  name       'Sergio'
+  surname    'de Torre'
+  nickname   'deTorre82'
+  sex        'man'
+  country    'Spain'
+  birth_date '12-23-1982'
+  image      { File.open('app/assets/images/sergio.jpg') }
+  image_name 'sergio.jpg'
+
+  after_build do |profile|
+    profile.image.store!
+  end
+end
+
+Fabricator(:user_medico_profile, from: "User::Profile") do
+  name       'Eduardo'
+  surname    'Hormilla'
+  nickname   'medico'
+  sex        'man'
+  country    'Spain'
+  birth_date '05-06-1982'
+  image      { File.open('app/assets/images/medico.jpg') }
+  image_name 'medico.jpg'
+
+  after_build do |profile|
+    profile.image.store!
+  end
+end
