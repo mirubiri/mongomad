b8d1d58fba17f0a0e858bfff2908e19e5f793b59
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-09 12:48:38 +0200

    implements start_negotiation method :)

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -23,4 +23,38 @@ class Offer
 
   validates :initial_message,
     length: { minimum: 1, maximum: 160 }
+
+  def start_negotiation
+    negotiation_params = {
+      negotiators: [ self.user_composer, self.user_receiver ],
+      conversation_attributes: {
+        messages_attributes: [
+          { user_id:self.user_composer_id, text:self.initial_message }
+        ]
+      },
+      proposals_attributes:Array.new
+    }
+
+    proposal_hash = Hash.new
+    proposal_hash[:user_composer_id] = self.user_composer_id
+    proposal_hash[:user_receiver_id] = self.user_receiver_id
+
+    proposal_hash[:composer_attributes] = { products_attributes:Array.new }
+    self.composer.products.each_with_index do |product, index|
+      proposal_hash[:composer_attributes][:products_attributes][index]= { thing_id:product.thing_id, quantity:product.quantity }
+    end
+
+    proposal_hash[:receiver_attributes] = { products_attributes:Array.new }
+    self.receiver.products.each_with_index do |product, index|
+      proposal_hash[:receiver_attributes][:products_attributes][index]= { thing_id:product.thing_id, quantity:product.quantity }
+    end
+
+    proposal_hash[:money_attributes] = { user_id:self.money.user_id, quantity:self.money.quantity }
+
+    negotiation_params[:proposals_attributes][0] = proposal_hash
+
+    negotiation = Negotiation.new(negotiation_params)
+    negotiation.save
+    negotiation
+  end
 end
