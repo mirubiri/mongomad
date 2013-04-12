1c4357bede1d5613da08afbcab7d6ce77be02ff1
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-12 11:02:40 +0200

    rename nickname

--- spec/fabricators/request/request_fabricator.rb
+++ spec/fabricators/request/request_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:request) do
   user       { Fabricate(:user) }
-  nickname   { |attrs| attrs[:user].profile.nickname }
+  nick       { |attrs| attrs[:user].profile.nick }
   text       { Faker::Lorem.sentence(word_count = 10) }
   image_url  { |attrs| attrs[:user].profile.image_url }
 end
