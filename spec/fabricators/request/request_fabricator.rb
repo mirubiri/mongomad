84c01b67c6f9a186922c57cef94318ab1e8b2106
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-12 10:59:34 +0200

    -

--- spec/fabricators/request/request_fabricator.rb
+++ spec/fabricators/request/request_fabricator.rb
@@ -2,5 +2,5 @@ Fabricator(:request) do
   user       { Fabricate(:user) }
   nickname   { |attrs| attrs[:user].profile.nickname }
   text       { Faker::Lorem.sentence(word_count = 10) }
-  image_url { |attrs| attrs[:user].profile.image_url }
+  image_url  { |attrs| attrs[:user].profile.image_url }
 end
