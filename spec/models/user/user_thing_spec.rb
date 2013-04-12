4b676680679992a1253a9b69acff348fd34606ee
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-12 10:50:12 +0200

    fix user_thing test

--- spec/models/user/user_thing_spec.rb
+++ spec/models/user/user_thing_spec.rb
@@ -12,7 +12,7 @@ describe User::Thing do
     it { should have_field(:name).of_type(String) }
     it { should have_field(:description).of_type(String) }
     it { should have_field(:stock).of_type(Integer) }
-    it { should_not have_field(:image_name).of_type(Object) }
+    it { should have_field(:image).of_type(Object) }
   end
 
   describe 'Validations' do
@@ -21,7 +21,6 @@ describe User::Thing do
     it { should validate_presence_of :description }
     it { should validate_presence_of :stock }
     it { should validate_presence_of :image }
-    it { should_not validate_presence_of :image_name }
     it { should validate_numericality_of(:stock).to_allow(nil: false,
                                                           only_integer: true,
                                                           greater_than_or_equal_to: 0) }
