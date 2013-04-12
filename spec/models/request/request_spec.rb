5170c978eb98e3ff2e8db489a00114941b4cd524
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-12 11:03:33 +0200

    rename nickname

--- spec/models/request/request_spec.rb
+++ spec/models/request/request_spec.rb
@@ -10,15 +10,15 @@ describe Request do
 
   describe 'Attributes' do
     it { should be_timestamped_document }
-    it { should have_field(:nickname).of_type(String) }
+    it { should have_field(:nick).of_type(String) }
     it { should have_field(:text).of_type(String) }
     it { should have_field(:image_url).of_type(String) }
-    it { should have_denormalized_fields(:nickname, :image_url).from('user.profile') }
+    it { should have_denormalized_fields(:nick, :image_url).from('user.profile') }
   end
 
   describe 'Validations' do
     it { should validate_presence_of :user }
-    it { should validate_presence_of :nickname }
+    it { should validate_presence_of :nick }
     it { should validate_presence_of :text }
     it { should validate_presence_of :image_url }
     it { should validate_length_of(:text).within(1..160) }
