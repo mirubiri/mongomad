6b711f92c3af66fc5d6aef1a9b5a4acd52d06725
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-15 13:54:05 +0200

    .

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -4,8 +4,8 @@ class Negotiation
 
   has_and_belongs_to_many :negotiators, class_name: "User", inverse_of: :negotiations
 
-  embeds_one  :conversation,class_name: "Negotiation::Conversation", cascade_callbacks: true
-  embeds_many :proposals,class_name: "Negotiation::Proposal", cascade_callbacks: true
+  embeds_one  :conversation, class_name: "Negotiation::Conversation", cascade_callbacks: true
+  embeds_many :proposals,    class_name: "Negotiation::Proposal", cascade_callbacks: true
 
   accepts_nested_attributes_for :conversation, :proposals
 
