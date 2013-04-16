6c842b2107ff061f8171932a2fb7971fa6ee00f3
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-16 14:07:58 +0200

    implement seal method for negotiation

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -13,4 +13,80 @@ class Negotiation
   validates :conversation,
     :proposals,
     presence: true
+
+  def seal
+    deal_params = {
+      signers: self.negotiators,
+      conversation_attributes: {
+        messages_attributes: [ { user_id:self.negotiators.first, text: 'Deal signed correctly' } ]
+      },
+      agreement_attributes:{
+        conversation_attributes: {
+          messages_attributes: Array.new
+        },
+        proposals_attributes: Array.new
+      }
+    }
+
+    self.conversation.messages.each do |message|
+      deal_params[:agreement_attributes][:conversation_attributes][:messages_attributes] << {
+        user_id: message.user_id,
+        nick: message.nick,
+        text: message.text,
+        image_url: message.image_url
+      }
+    end
+
+    self.proposals.each do |proposal|
+      proposal_hash = {
+        user_composer_id: proposal.user_composer_id,
+        user_receiver_id: proposal.user_receiver_id
+      }
+
+      proposal_hash[:composer_attributes] = {
+        nick: proposal.composer.nick,
+        image_url: proposal.composer.image_url,
+        products_attributes: Array.new
+      }
+
+      proposal.composer.products.each do |product|
+        proposal_hash[:composer_attributes][:products_attributes] << {
+          thing_id: product.thing_id,
+          name: product.name,
+          description: product.description,
+          quantity: product.quantity,
+          image_url: product.image_url
+        }
+      end
+
+      proposal_hash[:receiver_attributes] = {
+        nick: proposal.receiver.nick,
+        image_url: proposal.receiver.image_url,
+        products_attributes: Array.new
+      }
+
+      proposal.receiver.products.each do |product|
+        proposal_hash[:receiver_attributes][:products_attributes] << {
+          thing_id: product.thing_id,
+          name: product.name,
+          description: product.description,
+          quantity: product.quantity,
+          image_url: product.image_url
+        }
+      end
+
+      proposal_hash[:money_attributes] = {
+        user_id: proposal.money.user_id,
+        quantity: proposal.money.quantity
+      }
+
+      deal_params[:agreement_attributes][:proposals_attributes] << proposal_hash
+    end
+
+    puts deal_params
+
+    deal = Deal.new(deal_params)
+    deal.save
+    deal
+  end
 end
