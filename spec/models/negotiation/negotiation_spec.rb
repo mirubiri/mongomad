0544f4c24297d6f161127c672392c668a290d060
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-17 12:07:26 +0200

    add test

--- spec/models/negotiation/negotiation_spec.rb
+++ spec/models/negotiation/negotiation_spec.rb
@@ -88,6 +88,43 @@ describe Negotiation do
   describe '#seal(negotiator)' do
     xit 'makes a deal with the current proposal as agreement'
     xit 'finish the negotiation'
+
+
+
+ describe '#start_negotiation' do
+    let!(:negotiation) { offer.start_negotiation }
+
+    it 'returns a negotiation whose conversation has only one message' do
+      expect(negotiation.conversation.messages).to have(1).messages
+    end
+
+    it 'returns a negotiation whose message has the values from original offer' do
+      expect(negotiation.conversation.messages.last.user_id).to eq offer.user_composer_id
+      expect(negotiation.conversation.messages.last.text).to eq offer.initial_message
+    end
+
+    it 'returns a negotiation with only one proposal' do
+      expect(negotiation.proposals).to have(1).proposals
+    end
+
+    it 'returns a negotiation whose proposal has the values from original offer' do
+      expect(negotiation.proposals.last).to be_like offer
+    end
+
+    it 'returns a saved negotiation' do
+      expect(negotiation).to be_persisted
+    end
+
+    it 'add the negotiation to composer in offer' do
+      expect(offer.user_composer.negotiations.first).to eq negotiation
+    end
+
+    it 'add the negotiation to receiver in offer' do
+      expect(offer.user_receiver.negotiations.first).to eq negotiation
+    end
+  end
+
+
   end
 =end
 end
