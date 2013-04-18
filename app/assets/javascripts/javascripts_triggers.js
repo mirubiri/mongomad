5c95eee4a2307fbd946146bfafdacaec6350ba14
Medico <voldemortthegreat@gmail.com>
2013-04-18 10:03:00 +0200

    cambia el orden de ejecucion de los triggers de JS

--- app/assets/javascripts/javascripts_triggers.js
+++ app/assets/javascripts/javascripts_triggers.js
@@ -1,20 +1,20 @@
 $(window).load(function(){
   //slideOptionsPanel();
   ajaxLinksUrlChange();
+  expandContainers();
   reloadPrincipalContainersHeighs();
   matchPrincipalContainersHeighs();
   expandRequestContainer();
-  expandContainers();
   buttonsClickBackgroundChange();
   lastElementPosition();
 });
 
 
 $(window).bind('resize', function() {
+  expandContainers();
   reloadPrincipalContainersHeighs();
   matchPrincipalContainersHeighs();
   expandRequestContainer();
-  expandContainers();
   buttonsClickBackgroundChange();
   activateAlertButton();
 });
