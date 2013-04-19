b718388eea8ba41033613f21968232fd0a2a9438
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-19 13:11:48 +0200

    implement token methods to pass test :)

--- app/models/negotiation/token.rb
+++ app/models/negotiation/token.rb
@@ -12,4 +12,20 @@ class Negotiation::Token
 
   validates :state,
     :inclusion => { :in => [:propose, :accept] }
+
+  def initialize_token
+    proposal = self.negotiation.proposals.last
+
+    if proposal.money.user_id == proposal.user_receiver.id
+      self.user_id = proposal.user_composer.id
+    else
+      self.user_id = proposal.user_receiver.id
+    end
+    self.state = :propose
+  end
+
+  def update_token(user_id, state)
+    self.user_id = user_id
+    self.state = state
+  end
 end
