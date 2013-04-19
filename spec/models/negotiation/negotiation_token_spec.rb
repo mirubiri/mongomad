423fa12b3fe3709dcdf90bbbc3d75b12edf54130
Alejandro Arrufat <mirubiri@gmail.com>
2013-04-19 12:31:34 +0200

    add tests fot initialize_token

--- spec/models/negotiation/negotiation_token_spec.rb
+++ spec/models/negotiation/negotiation_token_spec.rb
@@ -4,6 +4,7 @@ describe Negotiation::Token do
   let(:offer) { Fabricate(:offer) }
   let(:negotiation) { Fabricate.build(:negotiation, offer:offer) }
   let(:token) { negotiation.token }
+  let(:proposal) { negotiation.proposals.last }
 
   describe 'Relations' do
     it { should be_embedded_in :negotiation }
@@ -15,6 +16,7 @@ describe Negotiation::Token do
   end
 
   describe 'Validations' do
+    it { should_not validate_presence_of :negotiation }
     it { should validate_presence_of :user_id }
     it { should validate_presence_of :state }
     it { should validate_inclusion_of(:state).to_allow([:propose, :accept]) }
@@ -29,7 +31,26 @@ describe Negotiation::Token do
   end
 
   describe '#initialize_token' do
-    it 'lo que sea'
+    before { token.initialize_token }
+
+    it 'is in :propose state' do
+      expect(token.state).to eq :propose
+    end
+
+    context 'when receiver offers money' do
+      it 'is owned by the composer' do
+        proposal.money.user_id = proposal.user_receiver.id
+        proposal.money.quantity = '1000'
+        token.initialize_token
+        expect(token.user_id).to eq proposal.user_composer.id
+      end
+    end
+
+    context 'when receiver does not offer money' do
+      it 'is owned by the receiver' do
+        expect(token.user_id).to eq proposal.user_receiver.id
+      end
+    end
   end
 
   describe '#update_token' do
