272376daff51ba26578b5af13cbd0fd7a2b404fe
Medico <voldemortthegreat@gmail.com>
2013-04-26 10:40:15 +0200

    completa el controlador de offer para que funcione ya correctamente la edicion

--- app/assets/javascripts/model_functions/negotiations.js
+++ app/assets/javascripts/model_functions/negotiations.js
@@ -1,3 +1,10 @@
+function maximizeConversationWidth(){
+  var maxWidth = $("#ppal_container").width();
+  var proposalWidth = $('.negotiation_offer:first').width();
+  var conversationWidth = maxWidth - proposalWidth - 17;
+  $('.dialog_container').css({'width': conversationWidth + 'px'});
+}
+
 function maximizeConversationsHeights(){
   $("[id^=negotiation]").each(function(){
     $(this).equalHeightColumns({children:':not(h2,p)'});
