ffdafb3aa06c48168677fe09be242cb3439b0025
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-03 11:28:29 +0200

    remove token field from negotiation fabricator

--- spec/fabricators/negotiation/negotiation_fabricator.rb
+++ spec/fabricators/negotiation/negotiation_fabricator.rb
@@ -4,5 +4,4 @@ Fabricator(:negotiation) do
   negotiators         { |attrs| [ attrs[:offer].user_composer, attrs[:offer].user_receiver ] }
   conversation        { |attrs| Fabricate.build(:negotiation_conversation, user:attrs[:negotiators].first) }
   proposals(count: 1) { |attrs| Fabricate.build(:negotiation_proposal, offer:attrs[:offer]) }
-  token               { |attrs| Fabricate.build(:negotiation_token, user:attrs[:negotiators].first) }
 end
