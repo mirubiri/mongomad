29db91d4d809f05edc43d9d71965ca5256532ff5
Alex <mirubiri@gmail.com>
2013-05-06 21:22:48 +0200

    add image_holder

---
+++ app/services/image_management/image_holder.rb
@@ -0,0 +1,42 @@
+module ImageManagement::ImageHolder
+  extend ActiveSupport::Concern
+
+  included do
+    
+    field :image_url, type: String
+    field :image_fingerprint, type: String
+
+    before_validation :manager_store_image, :unless => :image_stored?
+    
+    after_destroy :manager_destroy_image, :if => :image_stored?
+
+    validates :image_fingerprint,
+              :image_url,
+              presence:true
+  end
+
+  def image
+    image_url
+  end
+
+  def image=(file)
+    @image_manager ||= ImageManagement::ImageManager.new(file:File.open(file))
+  end
+
+  def image_fingerprint=(fingerprint)
+    @image_manager ||= ImageManagement::ImageManager.new(fingerprint:fingerprint)
+  end
+
+  private
+  attr_reader :image_manager
+  
+  def image_stored?
+    image_url.present? && image_fingerprint.present?
+  end
+
+  def manager_store_image
+    _image_manager.try(:store) &&
+    write_attribute(:image_url,image_manager.image[:url]) &&
+    write_attribute(:image_fingerprint,image_manager.image[:fingerprint])
+  end
+end
