9fccb657f0102352bf44af9f699db4d5ba5c88ad
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-08 13:08:00 +0200

    .

--- spec/fabricators/user/user_thing_fabricator.rb
+++ spec/fabricators/user/user_thing_fabricator.rb
@@ -3,10 +3,10 @@ Fabricator(:user_thing, class_name: 'User::Thing') do
   name        { Faker::Lorem.word }
   description { Faker::Lorem.sentence }
   stock       2
-  image  ActionDispatch::Http::UploadedFile.new({
-    :filename     => 'user.png',
+  image       ActionDispatch::Http::UploadedFile.new({
+    :filename     => 'car.png',
     :content_type => 'image/png',
-    :tempfile     => File.new('app/assets/images/user.png')})
+    :tempfile     => File.new('app/assets/images/car.png')})
 end
 
 Fabricator(:user_medico_thing_mp3, from: 'User::Thing') do
@@ -14,7 +14,7 @@ Fabricator(:user_medico_thing_mp3, from: 'User::Thing') do
   name        'Mp3'
   description 'Un reproductor mp3 acojonante de bueno.'
   stock       1000
-  image  ActionDispatch::Http::UploadedFile.new({
+  image       ActionDispatch::Http::UploadedFile.new({
     :filename     => 'medico_mp3.jpg',
     :content_type => 'image/jpg',
     :tempfile     => File.new('app/assets/images/medico_mp3.jpg')})
@@ -25,7 +25,7 @@ Fabricator(:user_medico_thing_cartera, from: 'User::Thing') do
   name        'Cartera'
   description 'Mi cartera llena de dineros no como la tuya que solo tiene polvo.'
   stock       1000
-  image  ActionDispatch::Http::UploadedFile.new({
+  image       ActionDispatch::Http::UploadedFile.new({
     :filename     => 'medico_cartera.jpg',
     :content_type => 'image/jpg',
     :tempfile     => File.new('app/assets/images/medico_cartera.jpg')})
@@ -36,7 +36,7 @@ Fabricator(:user_sergio_thing_mando, from: 'User::Thing') do
   name        'Mando'
   description 'El mando de mi bicicleta mercedes.'
   stock       1000
-  image  ActionDispatch::Http::UploadedFile.new({
+  image       ActionDispatch::Http::UploadedFile.new({
     :filename     => 'sergio_mando.jpg',
     :content_type => 'image/jpg',
     :tempfile     => File.new('app/assets/images/sergio_mando.jpg')})
@@ -47,7 +47,7 @@ Fabricator(:user_sergio_thing_cartera, from: 'User::Thing') do
   name        'Cartera'
   description 'Cartera de titanio reforzado para llevar gran cantidad de moneditas.'
   stock       1000
-  image  ActionDispatch::Http::UploadedFile.new({
+  image       ActionDispatch::Http::UploadedFile.new({
     :filename     => 'sergio_cartera.jpg',
     :content_type => 'image/jpg',
     :tempfile     => File.new('app/assets/images/sergio_cartera.jpg')})
