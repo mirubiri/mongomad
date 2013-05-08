e90d2087920b722352a57d1eff133b8306b71afc
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-08 11:25:49 +0200

    .

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,20 +1,19 @@
 class Request
   include Mongoid::Document
   include Mongoid::Timestamps
+  include ImageManagement::ImageHolder
   include Denormalized
 
   belongs_to :user
 
-  field :nick,      type: String
-  field :text,      type: String
-  field :image_url, type: String
+  field :nick, type: String
+  field :text, type: String
 
-  denormalize :nick, :image_url, from:'user.profile'
+  denormalize :nick, :image_fingerprint, from:'user.profile'
 
   validates :user,
     :nick,
     :text,
-    :image_url,
     presence: true
 
   validates :text,
