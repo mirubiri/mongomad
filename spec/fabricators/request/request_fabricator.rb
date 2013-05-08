2ba3ae93812ab9640cceca352de11efa7464085d
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-08 13:08:10 +0200

    .

--- spec/fabricators/request/request_fabricator.rb
+++ spec/fabricators/request/request_fabricator.rb
@@ -1,6 +1,5 @@
 Fabricator(:request) do
-  user      { Fabricate(:user) }
-  nick      { |attrs| attrs[:user].profile.nick }
-  text      { Faker::Lorem.sentence }
-  image_url { |attrs| attrs[:user].profile.image_url }
+  user { Fabricate(:user) }
+  nick { |attrs| attrs[:user].profile.nick }
+  text { Faker::Lorem.sentence }
 end
