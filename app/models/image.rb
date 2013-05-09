95b8ffe41f931e73a03e72345efae94449da51db
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-09 12:15:27 +0200

    implement test for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,13 @@ class Image
   field :url,         type:String
   field :fingerprint, type:String
   field :references,  type:Integer
+
+  validates :url,
+    :fingerprint,
+    :references,
+    presence: true
+
+  validates :references,
+    allow_nil: false,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
