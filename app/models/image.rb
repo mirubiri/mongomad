df664c960abd10a6d2d9cd5c14ab7ac1dae89458
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-09 12:07:54 +0200

    .

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image
   include Mongoid::Document
-  field :url, type:String
+
+  field :url,         type:String
   field :fingerprint, type:String
-  field :references, type:Integer
+  field :references,  type:Integer
 end
