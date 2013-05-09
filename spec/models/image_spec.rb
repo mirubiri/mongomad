15f73edb0b19a1fc4cc12dd1e82fa30b5a7acd21
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-09 12:15:16 +0200

    add test for image

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Image do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Attributes' do
+    it { should have_field(:url).of_type(String) }
+    it { should have_field(:fingerprint).of_type(String) }
+    it { should have_field(:references).of_type(Integer) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :url }
+    it { should validate_presence_of :fingerprint }
+    it { should validate_presence_of :references }
+    it { should validate_numericality_of(:references).to_allow(nil: false,
+                                                               only_integer: true,
+                                                               greater_than_or_equal_to: 0) }
+  end
 end
