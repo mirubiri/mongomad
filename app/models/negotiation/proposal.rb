6ba6c54fb21b530dfcde43b8ecb9f2423ebd8d7e
Alejandro Arrufat <mirubiri@gmail.com>
2013-05-22 14:23:46 +0200

    add helper can_cancel?

--- app/models/negotiation/proposal.rb
+++ app/models/negotiation/proposal.rb
@@ -62,7 +62,6 @@ class Negotiation::Proposal
     negotiation && negotiation.negotiators.find(user_receiver_id)
   end
 
-  # HELPERS
   def can_sign?(user)
     if (user_receiver_id == user.id) && (state == :unsigned.to_s)
       return true
@@ -73,6 +72,14 @@ class Negotiation::Proposal
 
   def can_confirm?(user)
     if (user_composer_id == user.id && state == :receiver_signed.to_s) || (user_receiver_id == user.id && state == :composer_signed.to_s)
+      return true
+    else
+      return false
+    end
+  end
+
+  def can_cancel?(user)
+    if state == :unsigned.to_s || state == :receiver_signed.to_s || state == :composer_signed.to_s
       return true
     else
       return false
