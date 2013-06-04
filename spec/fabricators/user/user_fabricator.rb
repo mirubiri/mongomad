33514a6c1abb94d10f7282901c364bb9920f7a57
Medico <voldemortthegreat@gmail.com>
2013-06-04 13:14:18 +0200

    add fabricator for sofia user

--- spec/fabricators/user/user_fabricator.rb
+++ spec/fabricators/user/user_fabricator.rb
@@ -37,3 +37,15 @@ Fabricator(:user_sergio, from: :user) do
     user.things << Fabricate.build(:user_sergio_thing_mando)
   end
 end
+
+Fabricator(:user_sofia, from: :user) do
+  profile         { Fabricate.build(:user_sofia_profile) }
+  things(count:1) { Fabricate.build(:user_thing) }
+  email           'sergio@improcex.com'
+  password        'sergio'
+
+  after_build do |user|
+    user.things << Fabricate.build(:user_sofia_thing_perro)
+    user.things << Fabricate.build(:user_sofia_thing_choza)
+  end
+end
