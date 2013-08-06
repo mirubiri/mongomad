8e53c5bbe31f6cf8cf698c83ba7ba7b0f762584f
Sergio <sergio.detorre@improcex.com>
2013-08-06 12:33:24 +0200

    remove unuseless types

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,18 @@ describe User do
   let(:user) { Fabricate.build(:user) }
 
   describe 'Relations' do
-    it { should have_many(:requests) }
+    it { should have_many :requests }
     it { should have_many(:sent_offers).of_type(Offer) }
     it { should have_many(:received_offers).of_type(Offer) }
-    it { should have_and_belong_to_many(:negotiations).of_type(Negotiation) }
-    it { should have_and_belong_to_many(:deals).of_type(Deal) }
-    it { should embed_one(:profile).of_type(Profile) }
-    it { should embed_many(:things).of_type(Thing) }
+    it { should have_and_belong_to_many :negotiations }
+    it { should have_and_belong_to_many :deals }
+    it { should embed_one :profile }
+    it { should embed_many :things }
   end
 
   describe 'Attributes' do
     it { should be_timestamped_document }
-    it { should accept_nested_attributes_for(:profile) }
+    it { should accept_nested_attributes_for :profile }
     it { should have_field(:name).of_type(String) }
   end
 
