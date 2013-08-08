3b65424420be86c80a3b7618bb76a22af35007f7
alex <alex@alex.no>
2013-08-08 10:13:44 +0200

    pass test for Sheet

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,3 +1,9 @@
 class Sheet
   include Mongoid::Document
+  embedded_in :sheet_container, polymorphic:true
+  field :name
+  field :description
+  field :image
+
+  validates_presence_of :name,:description,:image
 end
