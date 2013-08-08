4c876aa1c7b98d6a154ec1c00c500bb8635307b8
alex <alex@alex.no>
2013-08-08 11:16:33 +0200

    pass fabricator tests

--- spec/fabricators/profile_fabricator.rb
+++ spec/fabricators/profile_fabricator.rb
@@ -1,15 +1,13 @@
-# Fabricator(:user_profile, class_name: 'User::Profile') do
-#   user             nil
-#   first_name       { Faker::Name.first_name }
-#   last_name        { Faker::Name.last_name }
-#   gender           'male'
-#   birth_date       '10-10-2000'
-#   language 'english'
-#   image            ActionDispatch::Http::UploadedFile.new({
-#     :filename     => 'user.png',
-#     :content_type => 'image/png',
-#     :tempfile     => File.new('app/assets/images/user.png')})
-# end
+Fabricator(:profile) do
+  user             { Fabricate.build(:user,profile:nil) }
+  first_name       { Faker::Name.first_name }
+  last_name        { Faker::Name.last_name }
+  language 'english'
+#  image            ActionDispatch::Http::UploadedFile.new({
+#    :filename     => 'user.png',
+#    :content_type => 'image/png',
+#    :tempfile     => File.new('app/assets/images/user.png')})
+end
 
 # Fabricator(:user_medico_profile, from: 'User::Profile') do
 #   first_name       'Eduardo'
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,15 +1,9 @@
-# Fabricator(:user) do
-#   profile         { Fabricate.build(:user_profile) }
-#   things          nil
-#   requests        nil
-#   sent_offers     nil
-#   received_offers nil
-#   negotiations    nil
-#   deals           nil
-#   email           { Faker::Internet.email }
-#   name            { Faker::Name.first_name }
-#   password        'password'
-# end
+Fabricator(:user) do
+  profile         { Fabricate.build(:profile) }
+  email           { Faker::Internet.email }
+  name            { Faker::Name.first_name }
+  password        'password'
+end
 
 # Fabricator(:user_with_things, from: :user) do
 #   things(count:2) { Fabricate.build(:user_thing) }
