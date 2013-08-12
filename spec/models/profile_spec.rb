240e0adae0711beb9e76cf7f9df6984f20316358
Medico <voldemortthegreat@gmail.com>
2013-08-12 13:31:11 +0200

    add coordinates disables test to profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,19 +3,16 @@ require 'spec_helper'
 describe Profile do
   let(:profile) { Fabricate.build(:profile) }
 
-  xit 'should have an user photo'
-  xit 'should have the city name or coordinates where user live'
-
   # Relations
   it { should be_embedded_in :user }
 
   # Attributes
   it { should be_timestamped_document }
-  it { should have_fields :gender,:language }
+  it { should have_fields :gender, :language }
   it { should have_field(:birth_date).of_type(Date) }
+  xit 'should have the city name or coordinates where user live'
 
   # Validations
-
   it { should validate_presence_of :language }
 
   # Factories
