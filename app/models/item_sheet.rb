3fe4ee72cd4613288c8052de5d810047e61730a2
Medico <voldemortthegreat@gmail.com>
2013-08-12 13:36:31 +0200

    .

--- app/models/item_sheet.rb
+++ app/models/item_sheet.rb
@@ -1,7 +1,10 @@
 class ItemSheet
   include Mongoid::Document
+
   embedded_in :item_sheet_container, polymorphic:true
+
   field :name
   field :description
-  validates_presence_of :name,:description
+
+  validates_presence_of :name, :description
 end
