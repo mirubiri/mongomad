104543f383433375d35f56895dbf1c488fb3596a
Medico <voldemortthegreat@gmail.com>
2013-08-12 13:32:00 +0200

    add disabled test for user sheet

--- spec/models/user_sheet_spec.rb
+++ spec/models/user_sheet_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe UserSheet do
   let (:sheet) { Fabricate.build(:user_sheet) }
+
+
   # Relations
   it { should be_embedded_in :user_sheet_container }
 
@@ -12,6 +14,7 @@ describe UserSheet do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :nick }
+  xit 'should have an user photo'
 
   # Fabricators
   specify { expect(Fabricate.build(:user_sheet,container: :user)).to be_valid }
