8d7567a67afa9829de184e49c4e9c204729aa3a4
Medico <voldemortthegreat@gmail.com>
2013-08-13 09:58:49 +0200

    add user sheet to request and remove name field

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -3,19 +3,17 @@ require 'spec_helper'
 describe Request do
   let(:request) { Fabricate.build(:request) }
 
-  xit 'should have user photo'
-
   # Relations
   it { should belong_to :user }
+  it { should embed_one(:sheet).of_type(UserSheet) }
 
   # Attributes
   it { should be_timestamped_document }
-  it { should have_field(:name) }
   it { should have_field(:text) }
 
   # Validations
   it { should validate_presence_of :user }
-  it { should validate_presence_of :name }
+  it { should validate_presence_of :sheet }
   it { should validate_presence_of :text }
   it { should validate_length_of(:text).within(1..160) }
 
