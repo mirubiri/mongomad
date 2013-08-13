77a3b9fe154d1f64249f666091fcba2449c8df32
Medico <voldemortthegreat@gmail.com>
2013-08-13 10:11:21 +0200

    .

--- spec/models/user_sheet_spec.rb
+++ spec/models/user_sheet_spec.rb
@@ -5,18 +5,19 @@ describe UserSheet do
 
   xit 'should have an user photo'
   xit 'should have the city name or coordinates where user live'
-  
+
   # Relations
   it { should be_embedded_in :user_sheet_container }
 
   # Attributes
-  it { should have_fields :nick, :first_name, :last_name}
+  it { should have_field :nick }
+  it { should have_field :first_name }
+  it { should have_field :last_name}
 
   # Validations
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :nick }
-  
 
   # Fabricators
   specify { expect(Fabricate.build(:user_sheet, container: :user)).to be_valid }
