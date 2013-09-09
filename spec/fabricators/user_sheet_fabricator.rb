0c9fe470f3c5275f416bc5fc1ac0106d761b4165
alex <alex@alex.no>
2013-09-09 03:01:23 +0200

    clear

--- spec/fabricators/user_sheet_fabricator.rb
+++ spec/fabricators/user_sheet_fabricator.rb
@@ -1,5 +1,2 @@
 Fabricator(:user_sheet) do
-  transient :container
-  initialize_with { Fabricate.build(:user).sheet }
-  user_sheet_container { |attrs| Fabricate.build(attrs[:container]) if attrs[:container] }
 end
