a86462ad0d097b29e9c58e9f10e02fc3000ee8ac
alex <alex@alex.no>
2013-09-09 03:19:20 +0200

    disable uninplemented fabricators

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -1,7 +1,6 @@
+=begin
 Fabricator(:negotiation) do
    transient           :offer
    offer               { Fabricate(:offer) }
-   negotiators         { |attrs| [ attrs[:offer].user_composer, attrs[:offer].user_receiver ] }
-   conversation        { |attrs| Fabricate.build(:negotiation_conversation, user:attrs[:negotiators].first) }
-   proposals(count: 1) { |attrs| Fabricate.build(:negotiation_proposal, offer:attrs[:offer]) }
- end
+end
+=end
