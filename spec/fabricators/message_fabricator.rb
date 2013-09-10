ff150f87cc2a87bb921c52fc350588493329ec80
alex <alex@alex.no>
2013-09-10 06:51:37 +0200

    Makes message factory to work again. Adds user_id to message.

--- spec/fabricators/message_fabricator.rb
+++ spec/fabricators/message_fabricator.rb
@@ -1,4 +1,5 @@
 Fabricator(:message) do
   message_container { Fabricate.build(:negotiation,messages:nil) }
-  text         { Faker::Lorem.sentence }
+  user_id           { |attrs| attrs[:message_container]._users.first.id }
+  text              { Faker::Lorem.sentence }
 end
