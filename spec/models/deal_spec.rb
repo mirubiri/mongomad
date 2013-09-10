19990b593eb5a975ebeafd90ab5a9fd30fd2def9
alex <alex@alex.no>
2013-09-10 06:13:14 +0200

    Remove garbagge and activate some validations

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Deal do
-  # let(:negotiation) { Fabricate(:negotiation) }
-  # let(:deal) { Fabricate.build(:deal, negotiation:negotiation) }
+
+  let(:deal) { Fabricate.build(:deal) }
 
   # Relations
   it { should have_and_belong_to_many :_users }
@@ -12,29 +12,14 @@ describe Deal do
 
   # Attributes
   it { should be_timestamped_document }
-  # it { should accept_nested_attributes_for :proposals }
-  # it { should accept_nested_attributes_for :messages }
 
   # Validations
-  xit { should_not validate_presence_of :_users }
+  it { should_not validate_presence_of :_users }
   it { should validate_presence_of :proposals }
   it { should validate_presence_of :messages }
   xit 'should validate presence of two user sheets'
 
   # Factories
+  specify { expect(deal).to be_valid }
 
-  #   specify { expect(deal).to be_valid }
-  #   specify { expect(deal.save).to eq true }
-
-  #   it 'creates one negotiation' do
-  #     expect { deal.save }.to change{ Negotiation.count }.by(1)
-  #   end
-
-  #   it 'creates one offer' do
-  #     expect { deal.save }.to change{ Offer.count }.by(1)
-  #   end
-
-  #   it 'creates two different users' do
-  #     expect { deal.save }.to change{ User.count }.by(2)
-  #   end
 end
