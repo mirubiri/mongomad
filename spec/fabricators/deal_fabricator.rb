9cf22e8e6328115b04001497b2513312542ce5b1
alex <alex@alex.no>
2013-09-10 06:37:24 +0200

    Adds messages to Deal and Negotiation fabricators

--- spec/fabricators/deal_fabricator.rb
+++ spec/fabricators/deal_fabricator.rb
@@ -4,4 +4,5 @@ Fabricator(:deal) do
    _users       { |attrs| attrs[:negotiation]._users }
    proposals    { |attrs| attrs[:negotiation].proposals }
    user_sheets  { |attrs| attrs[:negotiation].user_sheets }
+   messages     { |attrs| attrs[:negotiation].messages }
 end
--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -1,9 +1,10 @@
 Fabricator(:negotiation) do
    transient    :offer
    offer        { Fabricate(:offer) }
-   _users        { |attrs| [ attrs[:offer].user_sender,attrs[:offer].user_receiver ] }
+   _users       { |attrs| [ attrs[:offer].user_sender,attrs[:offer].user_receiver ] }
    proposals    { |attrs| [ attrs[:offer].proposal ] }
    user_sheets  { |attrs| attrs[:offer].user_sheets }
+   messages     { |attrs| [ Fabricate.build(:message,message_container:nil,text:attrs[:offer].message) ] }
    # Faltan los mensajes
 end
 
