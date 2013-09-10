81e872d7bb0bfa5596c1f1062797da071a5a6eca
alex <alex@alex.no>
2013-09-10 06:55:05 +0200

    Repairs message and negotiation fabricators

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:negotiation) do
    _users       { |attrs| [ attrs[:offer].user_sender,attrs[:offer].user_receiver ] }
    proposals    { |attrs| [ attrs[:offer].proposal ] }
    user_sheets  { |attrs| attrs[:offer].user_sheets }
-   messages     { |attrs| [ Fabricate.build(:message,message_container:nil,text:attrs[:offer].message) ] }
+   messages     { |attrs| [ Fabricate.build(:message,text:attrs[:offer].message) ] }
    # Faltan los mensajes
 end
 
