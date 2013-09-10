aefae588521b45ed100ec4059c9ee3b000011a29
alex <alex@alex.no>
2013-09-10 06:49:46 +0200

    Add field user_id an its presence validator

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,8 +5,9 @@ class Message
   embedded_in :message_container, polymorphic: true
 
   field :text
+  field :user_id,type:Moped::BSON::ObjectId
 
-  validates_presence_of :text
+  validates_presence_of :text,:user_id
 
   validates :text, length: { minimum: 1, maximum: 160 }
 end
