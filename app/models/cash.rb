41aaba41e76f94a8c04a1d686a3d613108a222bf
Sergio <sergio.detorre@improcex.com>
2013-09-13 12:51:06 +0200

    pass tests for Cash class

--- app/models/cash.rb
+++ app/models/cash.rb
@@ -1,3 +1,8 @@
 class Cash < Good
-  field :_money, type:Money
+
+  field :_id,      type:Moped::BSON::ObjectId,default:nil
+  field :_money,   type:Money
+  field :owner_id, type:Moped::BSON::ObjectId
+
+  validates_presence_of :_money, :owner_id
 end
