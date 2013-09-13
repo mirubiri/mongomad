f64ea6c1a1396b88616faedf16e40fb04f8cb751
alex <alex@alex.no>
2013-09-13 05:33:29 +0200

    Puts some tests for Cash

--- spec/models/cash_spec.rb
+++ spec/models/cash_spec.rb
@@ -6,7 +6,17 @@ describe Cash do
   # Relations
   it { should be_embedded_in :proposal}
 
+  # Validations
+
+  it { should validate_presence_of :_money }
+  it { should validate_presence_of :owner_id }
+
   # Fields
   it { should have_field(:_money).of_type(Money) }
   it { should have_field(:owner_id).of_type(Moped::BSON::ObjectId) }
+
+  # Methods
+  specify '.new' do
+    expect(Cash.new.id).to eq nil
+  end
 end
