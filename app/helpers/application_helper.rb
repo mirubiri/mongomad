0db2ba1c592b12a2551a28b3c9c531667c4d0d6d
Medico <voldemortthegreat@gmail.com>
2013-09-25 10:14:19 +0200

    reduce helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,175 +1,129 @@
  module ApplicationHelper
 
-  # USER HELPERS -----------------------------------------
-  def user_id(user)
-    user.id
+  # COMMON HELPERS 
+  def id(object)
+    object.id
   end
-  def user_nick(user)
-    user.nick
+  def name(object)
+    object.name
   end
-  def user_email(user)
-    user.email
+  def description(object)
+    object.description
   end
-  def user_first_name(user)
-    user.profile.first_name
+  def text(object)
+    object.text
   end
-  def user_last_name(user)
-    user.profile.last_name
-  end
-  def user_fullname(user)
-    user.profile.first_name + " " + user.profile.last_name
-  end
-  # def user_gender(user) #not used
-  #   user.profile.gender
-  # end
-  # def user_language(user) #not used
-  #   user.profile.language
-  # end
-  def user_birthdate(user)
-    user.profile.birth_date
-  end
-  # def user_country(user)   #Does not exist in the model at the moment!!!
-  #   'apatrida'
-  # end
-  def user_image(user) # FIX helper!!!!!
+  def image(object) #FIX helper!!!!!
+    #image_tag(object.image)
     image_tag('/assets/images/sergio.jpg')
   end
-  def user_items(user)
+  def datetime(object)
+    object.created_at
+  end
+
+  # USER HELPERS 
+  def nick(user)
+    user.nick
+  end
+  def email(user)
+    user.email
+  end
+  def first_name(user)
+    user.profile.first_name
+  end
+  def last_name(user)
+    user.profile.last_name
+  end
+  def full_name(user)
+    user.profile.first_name + " " + user.profile.last_name
+  end
+  # def gender(user) #FIX: Not used
+  #   user.profile.gender
+  # end
+  # def language(user) #FIX: Not used
+  #   user.profile.language
+  # end
+  def birth_date(user)
+    user.profile.birth_date
+  end
+  # def country(user) #FIX: Does not exist in the model (at the moment)!!!
+  #   'apatrida'
+  # end
+  def items(user)
     user.items
   end
-  def user_requests(user)
+  def requests(user)
     user.requests
   end
-  # def user_sent_offers(user)  #not used
+  # def sent_offers(user) #FIX: Not used
   #   user.sent_offers
   # end
-  def user_received_offers(user)
+  def received_offers(user)
     user.received_offers
   end
-  def user_negotiations(user)
+  def negotiations(user)
     user.negotiations
   end
-  def user_deals(user)
+  def deals(user)
     user.deals
   end
 
-  # ITEM HELPERS -----------------------------------------
-  def item_id(item)
-    item.id
-  end
-  def item_name(item)
-    item.name
-  end
-  def item_description(item)
-    item.description
-  end
-  def item_stock(item)
+  # ITEM HELPERS 
+  def stock(item)
     item.stock
   end
-  def item_image(item) # FIX helper!!!!!
-    image_tag('/assets/images/sergio.jpg')
-  end
 
-  # REQUEST HELPERS -----------------------------------------
-  def request_id(request)
-    request.id
-  end
-  def request_text(request)
-    request.text
-  end
-  def request_image(request)  # FIX helper!!!!!
-    image_tag('/assets/images/sergio.jpg')
-  end
-
-  # OFFER HELPERS -----------------------------------------
-  def offer_id(offer)
-    offer.id
-  end
-  def offer_message(offer)
+  # OFFER HELPERS 
+  def  message(offer)
     offer.message
   end
-  def offer_datetime(offer)
-    offer.created_at
-  end
 
-  # PRODUCT HELPERS -----------------------------------------
-  # def product_id(product)
-  #   product.id
-  # end
-  def product_name(product)
-    product.name
-  end
-#   def product_description(product)
-#     product.description
-#   end
-  def product_quantity(product)
+  # PRODUCT HELPERS 
+  def quantity(product)
     product.quantity
   end
-  def product_image(product) # FIX helper!!!!!
-    image_tag('/assets/images/sergio.jpg')
-  end
 
-  # NEGOTIATION HELPERS -----------------------------------------
-  def negotiation_id(negotiation)
-    negotiation.id
-  end
-  def negotiation_messages(negotiation)
+  # NEGOTIATION HELPERS 
+  def messages(negotiation)
     negotiation.messages
   end
-  def negotiation_last_proposal(negotiation)
+  def last_proposal(negotiation)
     negotiation.proposals.last
   end
 
-  # PROPOSAL HELPERS -----------------------------------------
-  def proposal_composer_id(proposal)
+  # PROPOSAL HELPERS 
+  def composer_id(proposal)
     proposal.composer_id
   end
-  def proposal_composer_first_name(proposal)
+  def composer_first_name(proposal)
     proposal.proposal_container.user_sheets.where(id:proposal.composer_id).last.first_name
   end
-  def proposal_composer_fullname(proposal)
+  def composer_full_name(proposal)
     sheet = proposal.proposal_container.user_sheets.where(id:proposal.composer_id).last
     sheet.first_name + " " + sheet.last_name
   end
-  def proposal_composer_image(proposal) # FIX helper!!!!!
-    image_tag('/assets/images/sergio.jpg')
-  end
-  def proposal_composer_products(proposal)
+  def composer_products(proposal)
     proposal.left(proposal.composer_id)
   end
-  # def proposal_receiver_id(proposal)
+  # def receiver_id(proposal) #FIX: Not used
   #   proposal.receiver_id
   # end
-  # def proposal_receiver_first_name(proposal) 
+  # def receiver_first_name(proposal) #FIX: Not used
   #   proposal.proposal_container.user_sheets.where(id:proposal.receiver_id).last.first_name
   # end
-  # def proposal_receiver_image(proposal) # FIX helper!!!!!
-  #   image_tag('/assets/images/sergio.jpg')
-  # end
-  def proposal_receiver_products(proposal) # FIX helper!!!!!
+  def receiver_products(proposal) # FIX helper!!!!!
     proposal.right(proposal.receiver_id)
   end
-  def proposal_can_sign?(proposal, user) # FIX helper!!!!!
+  def can_sign?(proposal, user) # FIX helper!!!!!
     #proposal.can_sign?(user)
     true
   end
-  def proposal_can_confirm?(proposal, user) # FIX helper!!!!!
+  def can_confirm?(proposal, user) # FIX helper!!!!!
     #proposal.can_confirm?(user)
     true
   end
-  def proposal_can_cancel?(proposal, user) # FIX helper!!!!!
+  def can_cancel?(proposal, user) # FIX helper!!!!!
     #proposal.can_cancel?(user)
     true
-  end
-
-  # MESSAGE HELPERS -----------------------------------------
-  def message_text(message)
-    message.text
-  end
-  def message_image(message) # FIX helper!!!!!
-    image_tag('/assets/images/sergio.jpg')
-  end
-  def message_datetime(message)
-    message.created_at
-  end
+  end  
  end
