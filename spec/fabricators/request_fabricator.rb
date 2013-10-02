77175a705daba634b35c8213961ca1d3a7eb119f
Medico <voldemortthegreat@gmail.com>
2013-10-02 13:28:23 +0200

    pass request text and fix request fabricator

--- spec/fabricators/request_fabricator.rb
+++ spec/fabricators/request_fabricator.rb
@@ -2,4 +2,5 @@ Fabricator(:request) do
   user  { Fabricate(:user) }
   user_sheet { |attrs| Fabricate.build(:user_sheet, nick:attrs[:user].nick) }
   text { Faker::Lorem.sentence }
+  location  [Faker::Number.number(3),Faker::Number.number(3)] 
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -16,8 +16,8 @@ describe Request do
   it { should validate_presence_of :user }
   it { should validate_presence_of :user_sheet }
   it { should validate_presence_of :text }
-  it { should validate_length_of(:text).within(1..160) }
   it { should validate_presence_of :location }
+  it { should validate_length_of(:text).within(1..160) }
 
   # Factories
   specify { expect( Fabricate.build(:request) ).to be_valid }
