03d2d98f60970f1273f11ac4a9238955457f0ed0
Medico <voldemortthegreat@gmail.com>
2013-10-02 12:21:45 +0200

    .

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -9,7 +9,7 @@ describe Request do
 
   # Attributes
   it { should be_timestamped_document }
-  it { should have_field(:text) }
+  it { should have_field :text }
   it { should have_field(:location).of_type(Array) }
 
   # Validations
