b01f2840d146ab3e9a240bfcdfe3d2395fe73254
Alex <mirubiri@gmail.com>
2013-10-04 07:04:32 +0200

    first implementation for negotiation course

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -5,6 +5,5 @@ Fabricator(:negotiation) do
    proposals    { |attrs| [ attrs[:offer].proposal ] }
    user_sheets  { |attrs| attrs[:offer].user_sheets }
    messages     { |attrs| [ Fabricate.build(:message,text:attrs[:offer].message) ] }
-   performer    { |attrs| attrs[:offer].user_composer.id }
    # Faltan los mensajes
 end
--- spec/models/negotiation_spec.rb
+++ spec/models/negotiation_spec.rb
@@ -5,6 +5,9 @@ describe Negotiation do
   let(:negotiation) { Fabricate.build(:negotiation) }
   let(:first_user)  { negotiation._users.first }
   let(:second_user) { negotiation._users.last }
+  let(:composer_id) { negotiation.proposals.last.composer_id }
+  let(:receiver_id) { negotiation.proposals.last.receiver_id }
+  let(:negotiation_state) { negotiation.negotiation_state }
 
 
   # Relations
@@ -15,8 +18,8 @@ describe Negotiation do
 
   # Attributes
   it { should be_timestamped_document }
-  xit { should have_field(:performer).of_type(Moped::BSON:ObjectId)}
-  it { should have_field :state }
+  it { should have_field(:previous_state).of_type(Array)}
+  it { should have_field(:state).of_type(Array) }
 
   # Validations
   it { should_not validate_presence_of :_users }
@@ -63,158 +66,59 @@ describe Negotiation do
     end
   end
 
-  it 'has a method to sign'
-  it 'has a method to seal the deal'
-
-
-=begin
-  describe '#proposal' do
-    it 'returns the current proposal'
-  end
-
-  describe '#make_proposal(user,products)' do
-
-    it 'make a proposal with the given products'
-    it 'adds the new proposal to proposals list'
-  end
-
-  describe '#send_message(user,message)' do
-    it 'writes a new message for given user and text'
-    it 'returns true if written'
-    it 'returns false if given user is not participating'
-
-    pending 'Como hay que repetirlo para deal, pensar en hacer un shared_example'
-  end
-
-  ['sign', 'confirm','cancel','new'].each do |action|
-    describe "##{action}" do
-      it 'returns false if given user is not present in negotiation' do
-        expect(negotiation.send(action,Fabricate.build(:user))).to eq false
-      end
-
-      it 'returns false if negotiation ended' do
-        negotiation.end(first_user)
-        expect(negotiation.send(action,user)).to eq false
-      end
-
-      it 'does change performer if user is not present in negotiation' do
-        expect { negotiation.send(action,Fabricate.build(:user)) }.not_to change { negotiation.performer }
-      end
-    end
-  end
-
-
-  describe ':states' do
-    context 'when money present' do
-      it 'has initial state set to :new if composer holds the money'
-      it 'has initial state set to :composer_signed if receiver holds the money'
-    end
-
-    context 'when money not present' do
-      it 'has initial_state set to :composer_signed'
-    end
-
-    it 'changes state from :composer_signed to :receiver_confirmed on :receiver_confirm'
-    it 'changes state from :new to :receiver_signed on :receiver_sign'
-    it 'changes state from :receiver_signed to :composer_confirmed on :composer_confirm'
-    it 'changes state from :all to :canceled on :cancel'
-    it 'changes state from :all to :ended on :end'
-  end
-
-  describe '#new(user)' do
-    it 'returns false if user is not present in the negotiation'
-    it 'sets negotiation state to new'
-    it 'sets last action performer to user'
-    it 'saves the negotiation'
-  end
-
-  describe '#sign(user)' do
-    context 'negotiation is new' do
-      it 'sets the negotiation state to signed'
-      it 'sets last action performer to user'
-      it 'saves the negotiation'
-      it 'returns true'
-    end
-
-    context 'negotiation is not new' do
-      it 'returns false'
-    end
-  end
-
-  describe '#confirm(user)' do
-    context 'negotiation is signed by other user' do
-      it 'sets negotiation state to confirmed'
-      it 'sets last action performed to user'
-      it 'generates a deal'
-      it 'removes the negotiation if deal is generated'
-      it 'returns true'
-    end
-
-    context 'negotiation is not signed by second user' do
-      it 'returns false'
-    end
-  end
-
-  describe '#cancel(user)' do
-    it 'returns false if user is not present in the negotiation'
-    it 'sets negotiation in canceled state'
-    it 'sets last action performer to user'
-    it 'saves the negotiation'
-  end
-
-  describe 'end(user)' do
-    context 'user present' do
-      it 'removes the user form the negotiation'
-      it 'sets negotiation to ended state'
-      it 'sets last action performer to user'
-      it 'returns true'
-    end
-
-    context 'user not present' do
-      it 'returns false'
-      it 'does not change the negotiation status'
-    end
-
-    context 'user is alone' do
-      it 'destroys the negotiation'
-    end
-  end
-
   describe '#initial_state' do
-    context 'Money present' do
+    it 'sets state to [composer_id,new] if composer has cash' do
+      negotiation.proposals.first.goods<<Fabricate.build(:cash,owner_id:composer_id)
+      expect(negotiation.initial_state).to eq [composer_id,'new']
     end
-
-    context 'No money present' do
+    it 'sets state to [composer_id,signed] if receiver has cash' do
+      negotiation.proposals.first.goods<<Fabricate.build(:cash,owner_id:receiver_id)
+      expect(negotiation.initial_state).to eq [composer_id,'signed']
+    end
+    it 'sets state to [composer_id,signed] if no cash' do
+      expect(negotiation.initial_state).to eq [composer_id,'signed']
     end
   end
 
-  describe '#actions_for(user)' do
-    it 'returns an array with the allowed actions for user'
-    it 'returns an empty array if not allowed actions for user'
+  describe '#sign' do
+    it 'calls to state.trigger(user_id,:sign)' do
+      expect(negotiation_state).to receive(:trigger).with([composer_id,:sign])
+      negotiation.sign(composer_id)
+    end
   end
 
-  describe 'present?(user)' do
-    it 'returns true if user is present in the negotiation'
-    it 'returns false if user is not present in the negotiation'
+  describe '#confirm' do
+    it 'calls to state.trigger(user_id,:confirm)' do
+      expect(negotiation_state).to receive(:trigger).with([composer_id,:confirm])
+      negotiation.confirm(composer_id)
+    end
   end
 
-  describe 'performed?(user)' do
-    it 'returns true if user was who executed the last action'
-    it 'returns false if user did not executed the las action'
+  describe '#reject' do
+    it 'calls to state.trigger(user_id,:reject)' do
+      expect(negotiation_state).to receive(:trigger).with([composer_id,:reject])
+      negotiation.reject(composer_id)
+    end
   end
 
-  describe 'money_owner' do
-    it 'returns the id of user whose has money'
-    it 'returns nil if nobody has money'
+  describe '#nostock' do
+    it 'calls to state.trigger(:nostock)' do
+      expect(negotiation_state).to receive(:trigger).with(['nostock'])
+      negotiation.nostock
+    end
   end
 
-  describe 'money?' do
-    it 'returns true when money exists'
-    it 'returns false when money not exists'
+  describe '#restock' do
+    it 'calls to state.trigger(:restock)' do
+      expect(negotiation_state).to receive(:trigger).with(['restock'])
+      negotiation.restock
+    end
   end
 
+ describe '#negotiation_state' do
+  it 'is pending'
+ end
+ 
   # Factories
   specify { expect(negotiation).to be_valid }
-=end
 end
-
