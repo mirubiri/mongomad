3c9b7ffa5f8b8190b5ce63995ff4b633b47a932d
Alex <mirubiri@gmail.com>
2013-10-07 02:39:00 +0200

    Remove trailing spaces

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -25,24 +25,24 @@ class Proposal
   end
 
   private
-  def check_composer_goods    
+  def check_composer_goods
     errors.add(:goods, "Composer should have at least one good") unless left(composer_id).count > 0
   end
 
-  def check_receiver_goods    
+  def check_receiver_goods
     errors.add(:goods, "Receiver should have at least one good") unless left(receiver_id).count > 0
   end
 
-  def check_goods_owner    
-    errors.add(:goods, "All goods should be owned by composer or receiver") unless goods.or({owner_id:composer_id}, {owner_id:receiver_id}).size == goods.size 
+  def check_goods_owner
+    errors.add(:goods, "All goods should be owned by composer or receiver") unless goods.or({owner_id:composer_id}, {owner_id:receiver_id}).size == goods.size
   end
 
   def check_duplicate_goods
-    errors.add(:goods, "Proposal should not have duplicated good") unless goods.distinct(:id).size == goods.size 
+    errors.add(:goods, "Proposal should not have duplicated good") unless goods.distinct(:id).size == goods.size
   end
 
   def check_multiple_cash
-    errors.add(:goods, "Proposal should have only one cash") if goods.type(Cash).size > 1 
+    errors.add(:goods, "Proposal should have only one cash") if goods.type(Cash).size > 1
   end
 
   # before_create :set_initial_state
