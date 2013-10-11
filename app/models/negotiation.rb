ee8827e8346522a4bab1056ecf1e0204fea9e22b
Medico <voldemortthegreat@gmail.com>
2013-10-11 12:40:17 +0200

    pass initial_state method test of negotiation

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -6,19 +6,23 @@ class Negotiation
 
   embeds_many :proposals, class_name:'Proposal', as: :proposal_container
   embeds_many :messages,  class_name:'Message', as: :message_container
-  embeds_many :user_sheets, class_name:'UserSheet', as: :sheet_container
 
-  field :previous_state,type:Array
-  field :state,type:Array
+  field :previous_state, type:Array
+  field :state, type:Array
 
   validates_presence_of :proposals, :messages, :state
+
+  validate :check_state_before_persisted,
+           :check_state_after_persisted,
+           :check_state_multiple_values,
+           :check_state_has_valid_value
 
   def cash?
     proposal.cash?
   end
 
   def cash_owner
-    proposal.cash_owner
+    proposal.goods.type(Cash).first.owner_id
   end
 
   def proposal
@@ -34,7 +38,11 @@ class Negotiation
   end
 
   def initial_state
-    self.state=[composer,'signed']
+    if cash? && cash_owner == composer then
+      self.state = ['unsigned']      
+    else 
+      self.state=[composer,'signed']
+    end
   end
 
   def _statemachine
@@ -64,5 +72,35 @@ class Negotiation
 
       fsm
     end
+  end
+
+  def sign
+    _statemachine
+  end
+
+  def confirm
+  end
+
+  def reject 
+  end
+
+  def nostock
+  end
+
+  def restock
+  end
+
+  private
+  def check_state_before_persisted
+    errors.add(:state, "State should be equal to initial_state before persisted") unless !persisted? && state == initial_state
+  end
+  def check_state_after_persisted
+    errors.add(:state, "State should be different to initial_state after persisted") unless persisted? && (state != initial_state)
+  end
+  def check_state_multiple_values
+    true
+  end
+  def check_state_has_valid_value
+    true
   end
 end
