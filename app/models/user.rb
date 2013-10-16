d21f897f6f096b281ff3efcbbd81f17aceab45f1
Medico <voldemortthegreat@gmail.com>
2013-10-16 10:02:24 +0200

    pass user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,7 +62,7 @@ class User
   # field :authentication_token, :type => String
 
   def sheet
-    UserSheet.new(nick:nick, first_name:profile.first_name, last_name:profile.last_name, location:profile.location) do |sheet|
+    UserSheet.new(first_name:profile.first_name, last_name:profile.last_name, nick:nick, location:profile.location, images:profile.images) do |sheet|
       sheet.id=id
     end
   end
