36c7cd6279e83369e30c9d3bb3ab805be0d76d05
Medico <voldemortthegreat@gmail.com>
2013-10-16 10:12:16 +0200

    .

--- app/concerns/attachment/image.rb
+++ app/concerns/attachment/image.rb
@@ -1,7 +1,9 @@
 module Attachment
 	class Image
 		include Mongoid::Document
+
 		embedded_in :attachable,polymorphic:true
+    
 		field :main, type: Boolean, default:false
 		field :_id, type: Moped::BSON::ObjectId, default:nil
 	end
