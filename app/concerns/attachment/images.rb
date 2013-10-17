9b56b132e55ad1d0425a3916862be4d129f0b36f
Medico <voldemortthegreat@gmail.com>
2013-10-17 11:59:59 +0200

    implement image partially

--- app/concerns/attachment/images.rb
+++ app/concerns/attachment/images.rb
@@ -1,8 +1,26 @@
 module Attachment
   module Images
     extend ActiveSupport::Concern
+
     included do
       embeds_many :images,class_name: "Attachment::Image",as: :attachable
+    
+      validate :check_main_image_exist
     end
+
+    def main_image
+    end
+
+    def set_main_image(image_id)
+    end
+
+    private
+    def check_main_image_exist
+      errors.add(:images, "There is no main image") unless images
+    end
+
+    # def check_multiple_main_image
+    #   # errors.add(:images, "Cannot have more than one main image") unless if images.size == 1
+    # end
   end
 end
