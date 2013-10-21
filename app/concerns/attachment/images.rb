c69431385f020d32231bf60b77cce3ffab149eaa
Medico <voldemortthegreat@gmail.com>
2013-10-21 14:54:06 +0200

    passing images testa

--- app/concerns/attachment/images.rb
+++ app/concerns/attachment/images.rb
@@ -5,22 +5,26 @@ module Attachment
     included do
       embeds_many :images,class_name: "Attachment::Image",as: :attachable
     
-      validate :check_main_image_exist
+      validate :check_main_image_exist, :check_multiple_main_image
     end
 
     def main_image
+      images.where(main:true).first
     end
 
     def set_main_image(image_id)
+      main_image.main = false
+      images.find(image_id).main = true
+      true
     end
 
     private
     def check_main_image_exist
-      errors.add(:images, "There is no main image") unless images
+      errors.add(:images, "There is no main image") unless images.count == 1
     end
 
-    # def check_multiple_main_image
-    #   # errors.add(:images, "Cannot have more than one main image") unless if images.size == 1
-    # end
+    def check_multiple_main_image
+      errors.add(:images, "Cannot have more than one main image") unless images.where(main:true).count == 1
+    end
   end
 end
