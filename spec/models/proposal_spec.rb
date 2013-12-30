a48d91a80cec3fafec92e510fefd8e68da6d652a
Medico <voldemortthegreat@gmail.com>
2013-12-30 09:25:52 +0100

    minor changes in tests texts for proposal

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -20,19 +20,19 @@ describe Proposal do
   it { should validate_presence_of :receiver_id }
   it { should validate_inclusion_of(:state).to_allow('new', 'signed', 'confirmed', 'broken', 'ghosted', 'discarded') }
 
-  it 'is invalid when there is not a good for composer' do
+  it 'is invalid when there is any good for composer' do
     composer_id = proposal.composer_id
     proposal.goods.delete_all(owner_id:composer_id)
     expect(proposal).to have(1).error_on(:goods)
   end
 
-  it 'is invalid when there is not a good for receiver' do
+  it 'is invalid when there is any good for receiver' do
     receiver_id = proposal.receiver_id
     proposal.goods.delete_all(owner_id:receiver_id)
     expect(proposal).to have(1).error_on(:goods)
   end
 
-  it 'is invalid if a good is not owned by composer or receiver' do
+  it 'is invalid if a good is not owned by composer nor receiver' do
     proposal.goods << Fabricate.build(:product)
     expect(proposal).to have(1).error_on(:goods)
   end
@@ -43,18 +43,18 @@ describe Proposal do
     expect(proposal).to have(1).error_on(:goods)
   end
 
-  it 'is invalid when is more than one cash in goods' do
+  it 'is invalid when there is more than one cash in goods' do
     proposal.goods << Fabricate.build(:cash,owner_id:proposal.composer_id)
     proposal.goods << Fabricate.build(:cash,owner_id:proposal.receiver_id)
     expect(proposal).to have(1).error_on(:goods)
   end
 
-  it 'is invalid when there is not a sheet for composer_id' do
+  it 'is invalid when there is any sheet for composer_id' do
     proposal.composer_id = nil
     expect(proposal).to have(1).error_on(:user_sheets)
   end
 
-  it 'is invalid when there is not a sheet for receiver_id' do
+  it 'is invalid when there is any sheet for receiver_id' do
     proposal.receiver_id = nil
     expect(proposal).to have(1).error_on(:user_sheets)
   end
