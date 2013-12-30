0184c942f0113ab7dea839927fe8d6fd1b947cbe
Medico <voldemortthegreat@gmail.com>
2013-12-30 09:27:12 +0100

    add new test for offer

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -8,11 +8,11 @@ describe Offer do
   it { should belong_to(:user_composer).of_type(User).as_inverse_of(:sent_offers) }
   it { should belong_to(:user_receiver).of_type(User).as_inverse_of(:received_offers) }
   it { should embed_one :proposal }
-  it { should_not embed_many :user_sheets }
 
   # Attributes
   it { should be_timestamped_document }
   it { should have_field :message }
+  it { should have_field(:state).with_default_value_of('new') }
 
   # Validations
   it { should validate_presence_of :user_composer }
@@ -20,7 +20,7 @@ describe Offer do
   it { should validate_presence_of :proposal }
   it { should validate_presence_of :message }
   it { should validate_length_of(:message).within(1..160) }
-
+  it { should validate_inclusion_of(:state).to_allow('new', 'negotiating', 'negotiated', 'ghosted', 'discarded') }
 
   #Methods
   describe '#composer' do
@@ -33,6 +33,57 @@ describe Offer do
     it 'returns the receiver user sheet' do
       expect(offer.receiver).to eq offer.proposal.user_sheets.find(offer.user_receiver_id)
     end
+  end
+
+  shared_examples 'an state machine event' do |action, initial_state, final_state|
+    before(:each) { offer.state = initial_state }
+
+    it "calls state_machine.trigger(#{action})" do
+      expect(offer.state_machine).to receive(:trigger).with(action)
+      offer.send(action)
+    end
+
+    it "changes offer state from #{initial_state} to #{final_state}" do
+      expect{offer.send(action)}.to change {offer.state}.from(initial_state).to(final_state)
+    end
+
+    it 'do not saves the offer' do
+      offer.send(action)
+      expect(offer).to_not be_persisted
+    end
+  end
+
+  describe '#negotiate' do
+    it_should_behave_like 'an state machine event', :negotiate, 'new', 'negotiating'
+  end
+
+  describe '#negotiated' do
+    it_should_behave_like 'an state machine event', :negotiated, 'negotiating', 'negotiated'
+  end
+
+  describe '#ghost' do
+    it_should_behave_like 'an state machine event', :ghost, 'negotiating', 'ghosted'
+  end
+ 
+  describe '#discard' do
+    it_should_behave_like 'an state machine event', :discard, 'ghosted', 'discarded'
+  end 
+ 
+  describe '#state_machine' do
+    subject(:machine) { double().as_null_object }
+
+    before(:each) { offer.state_machine(machine) }
+
+    it { should have_received(:when).with(:negotiate, 'new' => 'negotiating',
+                                                      'negotiated' => 'negotiating') }
+
+    it { should have_received(:when).with(:negotiated, 'negotiating' => 'negotiated') }
+
+    it { should have_received(:when).with(:ghost, 'new' => 'ghosted',
+                                                  'negotiating' => 'ghosted',
+                                                  'negotiated' => 'ghosted') } 
+
+    it { should have_received(:when).with(:discard, 'ghosted' => 'discarded') }
   end
 
   describe '#negotiate' do
