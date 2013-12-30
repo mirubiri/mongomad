c745e975e443849c5dc6cba81720fb8a88cf51d4
Medico <voldemortthegreat@gmail.com>
2013-12-30 10:27:20 +0100

    add test for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,7 @@ describe Item do
   it { should be_timestamped_document }
   it { should have_fields :name, :description }
   it { should have_field(:stock).of_type(Integer) }
+  it { should have_field(:state).with_default_value_of('available') }
 
   # Validations
   it { should validate_presence_of :user }
@@ -20,11 +21,62 @@ describe Item do
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
   it { should validate_numericality_of(:stock).to_allow(nil: false, only_integer: true, greater_than_or_equal_to: 0) }
+  it { should validate_inclusion_of(:state).to_allow('available', 'unavailable', 'ghosted', 'discarded') }
 
   # Factories
   specify { expect(Fabricate.build(:item)).to be_valid }
 
   # Methods
+  shared_examples 'an state machine event' do |action, initial_state, final_state|
+    before(:each) { item.state = initial_state }
+    
+    it "calls state_machine.trigger(#{action})" do
+      expect(item.state_machine).to receive(:trigger).with(action)
+      item.send(action)
+    end
+
+    it "changes item state from #{initial_state} to #{final_state}" do
+      expect {item.send(action)}.to change {item.state}.from(initial_state).to(final_state)
+    end
+
+    it 'do not saves the item' do
+      item.send(action)
+      expect(item).to_not be_persisted
+    end
+  end
+
+  #TODO: REVISAR LOS NOMBRES (available, unavailable)
+  describe '#available' do
+    it_should_behave_like 'an state machine event', :available, 'available', 'unavailable'
+  end
+
+  describe '#unavailable' do
+    it_should_behave_like 'an state machine event', :unavailable, 'unavailable', 'available'
+  end
+
+  describe '#ghost' do
+    it_should_behave_like 'an state machine event', :ghost, 'available', 'ghosted'
+  end
+ 
+  describe '#discard' do
+    it_should_behave_like 'an state machine event', :discard, 'ghosted', 'discarded'
+  end
+
+  describe '#state_machine(machine)' do
+    subject(:machine) { double().as_null_object }
+
+    before(:each) { proposal.state_machine(machine) }
+
+    it { should have_received(:when).with(:available, 'available' => 'unavailable') }
+
+    it { should have_received(:when).with(:unavailable, 'unavailable' => 'available') }
+
+    it { should have_received(:when).with(:ghost, 'available' => 'ghosted',                                               
+                                                  'unavailable' => 'ghosted') } 
+
+    it { should have_received(:when).with(:discard, 'ghosted' => 'discarded') }
+  end
+
   describe '#pick(quantity)' do
 
     it 'returns a Product filled with item name, description,images and given quantity' do
