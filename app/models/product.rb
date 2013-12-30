12b8cf0c9729a4a3f719f8116ab2b4773511b2d5
Medico <voldemortthegreat@gmail.com>
2013-12-30 12:52:38 +0100

    pass product tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,12 +6,35 @@ class Product < Good
   field :name
   field :description
   field :_id,         type:Moped::BSON::ObjectId,default:nil
-  field :owner_id,       type:Moped::BSON::ObjectId
+  field :owner_id,    type:Moped::BSON::ObjectId
   field :quantity,    type: Integer
+  field :state,       default:'available'
 
   validates_presence_of :name,:description,:_id,:owner_id
 
   validates :quantity, allow_nil: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates_inclusion_of :state, in: [ 'available', 'unavailable', 'ghosted', 'discarded' ]
+
+  def state_machine(machine=nil)
+    @state_machine ||= begin
+      machine ||= MicroMachine.new(state)
+
+      machine.when(:available, 'available' => 'unavailable')
+
+      machine.when(:unavailable, 'unavailable' => 'available')
+
+      machine.when(:ghost, 'available' => 'ghosted',
+                           'unavailable' => 'ghosted')
+
+      machine.when(:discard, 'ghosted' => 'discarded') 
+    
+      machine.on(:any) do
+        self.state = @state_machine.state
+      end
+      machine
+    end
+  end
 
   def item
     Item.find(id)
@@ -24,5 +47,20 @@ class Product < Good
   def available?
     self.item.available?(self.quantity)
   end
-end
+  
+  def available
+    state_machine.trigger(:available)
+  end
 
+  def unavailable
+    state_machine.trigger(:unavailable)
+  end
+
+  def ghost
+    state_machine.trigger(:ghost)
+  end
+
+  def discard
+    state_machine.trigger(:discard)
+  end
+end
