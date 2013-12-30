2effcc1aa0054844447feb4736b291fca15367c1
Medico <voldemortthegreat@gmail.com>
2013-12-30 09:46:16 +0100

    pass offer tests

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -8,6 +8,9 @@ class Offer
   embeds_one :proposal, as: :proposal_container
 
   field :message
+  field :state, default:'new'
+
+  validates_inclusion_of :state, in: [ 'new', 'negotiating', 'negotiated', 'ghosted', 'discarded' ]
 
   validates_presence_of :user_composer, :user_receiver, :proposal, :message
 
@@ -21,7 +24,45 @@ class Offer
     proposal.user_sheets.find(user_receiver_id)
   end
 
+  def state_machine(machine=nil)
+    @state_machine ||= begin
+      machine ||= MicroMachine.new(state)
+
+      machine.when(:negotiating, 'new' => 'negotiating',
+                               'negotiated' => 'negotiating')
+
+      machine.when(:negotiated, 'negotiating' => 'negotiated')
+
+      machine.when(:ghost, 'new' => 'ghosted',
+                           'negotiating' => 'ghosted',
+                           'negotiated' => 'ghosted')
+
+      machine.when(:discard, 'ghosted' => 'discarded') 
+    
+      machine.on(:any) do
+        self.state = @state_machine.state
+      end
+      machine
+    end
+  end
+
   def negotiate
     persisted? && Negotiation.create(_users:[user_composer, user_receiver], proposals:[proposal])
+  end
+
+  def negotiating
+    state_machine.trigger(:negotiating)
+  end
+
+  def negotiated
+    state_machine.trigger(:negotiated)
+  end
+
+  def ghost
+    state_machine.trigger(:ghost)
+  end
+
+  def discard
+    state_machine.trigger(:discard)
   end
 end
