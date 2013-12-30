d3fc7a51f89dbb6ca1ddc6dfa402e3a1516e9e14
Medico <voldemortthegreat@gmail.com>
2013-12-30 10:27:36 +0100

    add test for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,12 +15,64 @@ describe Product do
   it { should have_field(:quantity).of_type(Integer) }
   it { should have_field(:owner_id).of_type(Moped::BSON::ObjectId) }
   it { should have_field(:_id).of_type(Moped::BSON::ObjectId) }
+  it { should have_field(:state).with_default_value_of('available') }
 
   # Validations
   it { should_not validate_presence_of :proposal }
   it { should validate_numericality_of(:quantity).to_allow(nil: false, only_integer: true, greater_than_or_equal_to: 0) }
+  it { should validate_inclusion_of(:state).to_allow('available', 'unavailable', 'ghosted', 'discarded') }
 
   # Methods
+ shared_examples 'an state machine event' do |action, initial_state, final_state|
+    before(:each) { item.state = initial_state }
+    
+    it "calls state_machine.trigger(#{action})" do
+      expect(item.state_machine).to receive(:trigger).with(action)
+      item.send(action)
+    end
+
+    it "changes item state from #{initial_state} to #{final_state}" do
+      expect {item.send(action)}.to change {item.state}.from(initial_state).to(final_state)
+    end
+
+    it 'do not saves the item' do
+      item.send(action)
+      expect(item).to_not be_persisted
+    end
+  end
+
+  #TODO: REVISAR LOS NOMBRES (available, unavailable)
+  describe '#available' do
+    it_should_behave_like 'an state machine event', :available, 'available', 'unavailable'
+  end
+
+  describe '#unavailable' do
+    it_should_behave_like 'an state machine event', :unavailable, 'unavailable', 'available'
+  end
+
+  describe '#ghost' do
+    it_should_behave_like 'an state machine event', :ghost, 'available', 'ghosted'
+  end
+ 
+  describe '#discard' do
+    it_should_behave_like 'an state machine event', :discard, 'ghosted', 'discarded'
+  end
+
+  describe '#state_machine(machine)' do
+    subject(:machine) { double().as_null_object }
+
+    before(:each) { proposal.state_machine(machine) }
+
+    it { should have_received(:when).with(:available, 'available' => 'unavailable') }
+
+    it { should have_received(:when).with(:unavailable, 'unavailable' => 'available') }
+
+    it { should have_received(:when).with(:ghost, 'available' => 'ghosted',                                               
+                                                  'unavailable' => 'ghosted') } 
+
+    it { should have_received(:when).with(:discard, 'ghosted' => 'discarded') }
+  end
+  
   specify '.new' do
     expect(Product.new.id).to eq nil
   end
