a08e7de072dc8b8d61114da52be53ceab72d7404
Medico <voldemortthegreat@gmail.com>
2013-12-30 12:56:19 +0100

    pass item tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,33 @@ class Item
   field :name
   field :description
   field :stock, type:Integer
+  field :state, default:'available'
 
   validates_presence_of :name, :description, :stock, :user
 
   validates :stock, allow_nil: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates_inclusion_of :state, in: [ 'available', 'unavailable', 'ghosted', 'discarded' ]
+
+  def state_machine(machine=nil)
+    @state_machine ||= begin
+      machine ||= MicroMachine.new(state)
+
+      machine.when(:available, 'available' => 'unavailable')
+
+      machine.when(:unavailable, 'unavailable' => 'available')
+
+      machine.when(:ghost, 'available' => 'ghosted',
+                           'unavailable' => 'ghosted')
+
+      machine.when(:discard, 'ghosted' => 'discarded') 
+    
+      machine.on(:any) do
+        self.state = @state_machine.state
+      end
+      machine
+    end
+  end
 
   def pick(quantity)
   	Product.new(name:name,description:description,images:images,quantity:quantity) do |product|
@@ -35,5 +58,21 @@ class Item
 
   def available?(quantity)
     stock != nil && quantity != 0 && quantity <= stock
+  end
+
+  def available
+    state_machine.trigger(:available)
+  end
+
+  def unavailable
+    state_machine.trigger(:unavailable)
+  end
+
+  def ghost
+    state_machine.trigger(:ghost)
+  end
+
+  def discard
+    state_machine.trigger(:discard)
   end
 end
