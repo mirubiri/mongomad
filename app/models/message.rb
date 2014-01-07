c91bf80e9554bf227fe3bc7eb2155e011f9792b2
Medico <voldemortthegreat@gmail.com>
2014-01-07 08:55:22 +0100

    pass tests for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,6 @@ class Message
   field :user_id,type:Moped::BSON::ObjectId
   field :text
 
-  validates_presence_of :user_id, :text
+  validates_presence_of :user_id
   validates :text, length: { minimum: 1, maximum: 160 }
 end
