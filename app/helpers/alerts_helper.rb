90d12b8f027d87f11c2400f186b0081bef46101e
Medico <voldemortthegreat@gmail.com>
2014-01-16 15:03:15 +0100

    implements helpers

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,51 +1,57 @@
 module AlertsHelper
+  def birth_date(object)
+    # del profile del user
+  end
+  def composer(object)
+    object.composer
+  end
   def description(object)
-
+    object.description
   end
   def first_name(object)
-
+    # profile y usersheet
   end
   def full_name(object)
-
+    first_name(object) + last_name(object)
   end
-  def gender(object)
-
-  end
+  # def gender(object)
+  #   del profile del user
+  # end
   def id(object)
-
+    # directo o de usersheet
   end
   def image(object)
-
+    # directo o de usersheet
   end
   def images(object)
-
+    # directo o de usersheet
   end
   def initial_message(object)
-
+    object.message
   end
-  def language(object)
-
-  end
+  # def language(object)
+  #   desde el perfil del user
+  # end
   def last_name(object)
-
+    # profile y usersheet
   end
-  def location(object)
-
-  end
+  # def location(object)
+  #  # profile y usersheet
+  # end
   def main_image(object)
-
+    # directo o de usersheet
   end
   def message(object)
-
+    object.messages.last
   end
   def messages(object)
-
+    object.messages
   end
   def name(object)
-
+    object.name
   end
   def nick(object)
-
+    # profile y usersheet
   end
   def products(object)
 
@@ -57,18 +63,18 @@ module AlertsHelper
 
   end
   def quantity(object)
-
+    object.quantity
   end
   def receiver(object)
-
+    object.receiver
   end
   def state(object)
-
+    object.state
   end
   def stock(object)
-
+    object.stock
   end
   def text(object)
-
+    object.text
   end
 end
