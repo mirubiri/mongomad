2d98f4d8f4291b4f268a5acee8f3622e8e229e21
Medico <voldemortthegreat@gmail.com>
2014-01-16 13:32:14 +0100

    add helpers to replace the old ones

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,2 +1,74 @@
 module AlertsHelper
+  def description(object)
+
+  end
+  def first_name(object)
+
+  end
+  def full_name(object)
+
+  end
+  def gender(object)
+
+  end
+  def id(object)
+
+  end
+  def image(object)
+
+  end
+  def images(object)
+
+  end
+  def initial_message(object)
+
+  end
+  def language(object)
+
+  end
+  def last_name(object)
+
+  end
+  def location(object)
+
+  end
+  def main_image(object)
+
+  end
+  def message(object)
+
+  end
+  def messages(object)
+
+  end
+  def name(object)
+
+  end
+  def nick(object)
+
+  end
+  def products(object)
+
+  end
+  def proposal(object)
+
+  end
+  def proposals(object)
+
+  end
+  def quantity(object)
+
+  end
+  def receiver(object)
+
+  end
+  def state(object)
+
+  end
+  def stock(object)
+
+  end
+  def text(object)
+
+  end
 end
