aa415f7d490f920d8b6b6922ba66bf999398a14f
Medico <voldemortthegreat@gmail.com>
2014-01-17 10:15:10 +0100

    implements helpers

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,79 +1,135 @@
 module AlertsHelper
-  def birth_date(object)
-    # del profile del user
+  def agreement(object)
+    objetc.agreement
   end
+
+  def birth_date(object)
+    objetc.profile.birth_date
+  end
+
   def composer(object)
     object.composer
   end
+
   def description(object)
     object.description
   end
+
   def first_name(object)
-    # profile y usersheet
+    if object.class == User
+      object.profile.first_name
+    else
+      object.first_name
+    end
   end
+
   def full_name(object)
     first_name(object) + last_name(object)
   end
-  # def gender(object)
-  #   del profile del user
+
+  def gender(object)
+    objetc.profile.gender
+  end
+
+  # def id(object)
+  #   # directo o de usersheet
   # end
-  def id(object)
-    # directo o de usersheet
-  end
-  def image(object)
-    # directo o de usersheet
-  end
+  # def image(object)
+  #   if object.class == User
+  #     object.profile.images.last
+  #   else
+  #     object.images
+  #   end
+  # end
+
   def images(object)
-    # directo o de usersheet
+    if object.class == User
+      object.profile.images
+    else
+      object.images
+    end
   end
-  def initial_message(object)
-    object.message
+
+  def language(object)
+    objetc.profile.language
   end
-  # def language(object)
-  #   desde el perfil del user
-  # end
+
   def last_name(object)
-    # profile y usersheet
+    if object.class == User
+      object.profile.last_name
+    else
+      object.last_name
+    end
   end
-  # def location(object)
-  #  # profile y usersheet
+
+  def location(object)
+    if object.class == User
+      object.profile.location
+    else
+      object.location
+    end
+  end
+
+  # def main_image(object)
+  #   # directo o de usersheet
   # end
-  def main_image(object)
-    # directo o de usersheet
-  end
+
   def message(object)
-    object.messages.last
+    if object.class == Offer
+      object.message
+    else
+      object.messages.last
+    end
   end
+
   def messages(object)
     object.messages
   end
+
   def name(object)
     object.name
   end
+
   def nick(object)
-    # profile y usersheet
+      object.nick
+    end
   end
-  def products(object)
 
-  end
+  # def products(object)
+
+
+  #   if object.class == Offer
+  #     object.message
+  #   else
+  #     object.messages.last
+  #   end
+
+  # end
+
   def proposal(object)
-
+    object.proposal
   end
+
   def proposals(object)
-
+    object.proposals
   end
+
   def quantity(object)
     object.quantity
   end
+
   def receiver(object)
     object.receiver
   end
+
   def state(object)
     object.state
   end
+
   def stock(object)
     object.stock
   end
+
   def text(object)
     object.text
   end
