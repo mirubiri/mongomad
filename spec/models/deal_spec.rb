2ff160942a69c4ecca2eebe49450e4913c281e82
Medico <voldemortthegreat@gmail.com>
2014-01-20 13:21:43 +0100

    add test for deal related to user_sheets

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe Deal do
   # Variables
   let(:deal) { Fabricate.build(:deal) }
+  let(:first_user_id) { deal.users.first._id }
+  let(:second_user_id) { deal.users.last._id }
 
   # Relations
   it { should have_and_belong_to_many :users }
+  it { should embed_many :user_sheets }
   it { should embed_many :proposals }
   it { should embed_many :messages }
 
@@ -14,8 +17,24 @@ describe Deal do
 
   # Validations
   it { should validate_presence_of :users }
+  it { should validate_presence_of :user_sheets }
   it { should validate_presence_of :proposals }
   it { should validate_presence_of :messages }
+
+  it 'is invalid when there is no sheet for first user' do
+    deal.user_sheets.find(first_user_id)._id = nil
+    expect(deal).to have(1).error_on(:user_sheets)
+  end
+
+  it 'is invalid when there is no sheet for second user' do
+    deal.user_sheets.find(second_user_id)._id = nil
+    expect(deal).to have(1).error_on(:user_sheets)
+  end
+
+  it 'is invalid if there are more than two user sheets' do
+    deal.user_sheets << Fabricate.build(:user_sheet)
+    expect(deal).to have(1).error_on(:user_sheets)
+  end
 
   # Methods
   describe '#agreement' do
