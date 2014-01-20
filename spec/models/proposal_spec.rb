e24b905c0bbe590fdd3d4d32e9d82d9d0e54515d
Medico <voldemortthegreat@gmail.com>
2014-01-20 13:43:31 +0100

    remove test from proposal related to user sheets

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -6,7 +6,6 @@ describe Proposal do
 
   # Relations
   it { should be_embedded_in :proposal_container }
-  it { should embed_many :user_sheets }
   it { should embed_many :goods }
 
   # Attributes
@@ -17,7 +16,6 @@ describe Proposal do
 
   # Validations
   it { should_not validate_presence_of :proposal_container }
-  it { should validate_presence_of :user_sheets }
   it { should validate_presence_of :goods }
   it { should validate_presence_of :composer_id }
   it { should validate_presence_of :receiver_id }
@@ -50,21 +48,6 @@ describe Proposal do
     proposal.goods << Fabricate.build(:cash, owner_id:proposal.composer_id)
     proposal.goods << Fabricate.build(:cash, owner_id:proposal.receiver_id)
     expect(proposal).to have(1).error_on(:goods)
-  end
-
-  it 'is invalid when there is any sheet for composer_id' do
-    proposal.composer_id = nil
-    expect(proposal).to have(1).error_on(:user_sheets)
-  end
-
-  it 'is invalid when there is any sheet for receiver_id' do
-    proposal.receiver_id = nil
-    expect(proposal).to have(1).error_on(:user_sheets)
-  end
-
-  it 'is invalid if there are more than two user sheets' do
-    proposal.user_sheets << Fabricate.build(:user_sheet)
-    expect(proposal).to have(1).error_on(:user_sheets)
   end
 
   # Methods
@@ -134,16 +117,25 @@ describe Proposal do
   end
 
   describe '#composer' do
+    it 'calls to proposal_container.user_sheets.find with composer_id' do
+      expect(proposal.proposal_container.user_sheets).to receive(:find).with(composer_id)
+      proposal.composer
+    end
+
     it 'returns the composer user sheet' do
-      expect(proposal.composer).to eq proposal.user_sheets.find(proposal.composer_id)
+      expect(proposal.composer).to eq proposal.proposal_container.user_sheets.find(proposal.composer_id)
     end
   end
 
   describe '#receiver' do
-    it 'returns the receiver user sheet' do
-      expect(proposal.receiver).to eq proposal.user_sheets.find(proposal.receiver_id)
+    it 'calls to proposal_container.user_sheets.find with receiver_id' do
+      expect(proposal.proposal_container.user_sheets).to receive(:find).with(receiver_id)
+      proposal.receiver
     end
-  end
+
+    it 'returns the receiver user sheet' do
+      expect(proposal.receiver).to eq proposal.proposal_container.user_sheets.find(proposal.receiver_id)
+    end  end
 
   describe 'products(owner_id)' do
     it 'returns products for given user' do
