58df45036e01942665f2dbd2b1055282f6fa174e
Medico <voldemortthegreat@gmail.com>
2014-01-22 14:05:21 +0100

    fix deal fabricator

--- spec/fabricators/deal_fabricator.rb
+++ spec/fabricators/deal_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:deal) do
   transient   :negotiation
   negotiation { Fabricate(:negotiation) }
   users       { |attrs| attrs[:negotiation].users }
-  user_sheets { |attrs| [ Fabricate.build(:user_sheet, user:attrs[:users].first), Fabricate.build(:user_sheet, user:attrs[:users].last) ] }
+  user_sheets { |attrs| attrs[:negotiation].user_sheets }
   proposals   { |attrs| attrs[:negotiation].proposals }
   messages    { |attrs| attrs[:negotiation].messages }
 end
