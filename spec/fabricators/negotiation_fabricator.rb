9b7b18b57c22571534e9f25b28b0d3cdd425d41a
Medico <voldemortthegreat@gmail.com>
2014-01-22 09:57:21 +0100

    fix bug in negotiation fabricator

--- spec/fabricators/negotiation_fabricator.rb
+++ spec/fabricators/negotiation_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:negotiation) do
   transient   :offer
   offer       { Fabricate(:offer) }
   users       { |attrs| [ attrs[:offer].user_composer, attrs[:offer].user_receiver ] }
-  user_sheets { |attrs| [ Fabricate.build(:user_sheet, user:attrs[:user_composer]), Fabricate.build(:user_sheet, user:attrs[:user_receiver]) ] }
+  user_sheets { |attrs| [ Fabricate.build(:user_sheet, user:attrs[:users].first), Fabricate.build(:user_sheet, user:attrs[:users].last) ] }
   proposals   { |attrs| [ attrs[:offer].proposal ] }
   messages    { |attrs| [ Fabricate.build(:message, text:attrs[:offer].message) ] }
 end
