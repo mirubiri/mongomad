e5544dbf251708560ff1228de104cb4a3600f9a6
Medico <voldemortthegreat@gmail.com>
2014-01-22 14:30:20 +0100

    fix message fabricator

--- spec/fabricators/message_fabricator.rb
+++ spec/fabricators/message_fabricator.rb
@@ -1,5 +1,10 @@
 Fabricator(:message) do
-  message_container { Fabricate.build(:negotiation, messages:nil) }
-  user_id           { |attrs| attrs[:message_container].users.first._id }
+  transient :user
+
+  user  { Fabricate(:user_with_items) }
+
+  message_container { |attrs| Fabricate.build(:negotiation, messages:nil, offer:Fabricate.build(:offer, user_composer:attrs[:user], user_receiver:Fabricate.build(:user_with_items)), messages:nil) }
+
+  user_id           { |attrs| attrs[:user]._id }
   text              { Faker::Lorem.sentence }
 end
