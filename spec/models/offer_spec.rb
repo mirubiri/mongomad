bd26564bf14723b63af758202c6e019e529a97f9
Medico <voldemortthegreat@gmail.com>
2014-01-23 11:28:51 +0100

    add checks for offer

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -28,24 +28,29 @@ describe Offer do
   it { should validate_inclusion_of(:state).to_allow('new','negotiating','negotiated','ghosted','discarded') }
 
   # Checks
-  pending("2xusersheet")
-  pending("1 user sheet para el primer dealer")
-  pending("1 user sheet for 2nd dealer")
-  pending("propuesta de composer y receiver")
-
-  it 'is invalid when there is no sheet for user_composer' do
-    offer.user_composer_id = nil
-    expect(offer).to have(1).error_on(:user_sheets)
-  end
-
-  it 'is invalid when there is no sheet for user_receiver' do
-    offer.user_receiver_id = nil
-    expect(offer).to have(1).error_on(:user_sheets)
+  it 'is invalid if both users are the same' do
+    offer.user_receiver = offer.user_composer
+    expect(offer).to have(1).error_on(:users)
   end
 
   it 'is invalid if there are more than two user sheets' do
     offer.user_sheets << Fabricate.build(:user_sheet)
     expect(offer).to have(1).error_on(:user_sheets)
+  end
+
+  it 'is invalid if there is no sheet for composer' do
+    offer.user_composer_id = nil
+    expect(offer).to have(1).error_on(:user_sheets)
+  end
+
+  it 'is invalid if there is no sheet for receiver' do
+    offer.user_receiver_id = nil
+    expect(offer).to have(1).error_on(:user_sheets)
+  end
+
+  it 'is invalid if proposal is not owned by both users simultaneously' do
+    offer.proposal.composer_id = nil
+    expect(offer).to have(1).error_on(:proposals)
   end
 
   # Methods
