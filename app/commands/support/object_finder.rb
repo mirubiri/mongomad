2cfad8aeb33ee393cc99c41df169a26f215bbcc6
Medico <voldemortthegreat@gmail.com>
2014-02-03 11:14:56 +0100

    add new decorators and change commands structure to be more clear

---
+++ app/commands/support/object_finder.rb
@@ -0,0 +1,7 @@
+class ObjectFinder
+
+  # busca el objeto
+      # si es user en request, offer , negotiation y deal
+      # si es item en user,negotiation, deal
+  # devuelve un array con todos los objetos encontrados
+end
