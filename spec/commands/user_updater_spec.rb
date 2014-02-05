a40058c15fb2c743153ebac2ea1b2d4723a9bfd7
Medico <voldemortthegreat@gmail.com>
2014-02-05 13:30:06 +0100

    removes word 'decorator' from clas names

--- spec/commands/user_updater_spec.rb
+++ spec/commands/user_updater_spec.rb
@@ -16,6 +16,9 @@ describe UserUpdater do
   end
 
   # Methods
+
+  #TODO: Se podria poner un initialice para eliminar el parametro user del metodo execute.
+
   describe '#execute(user)' do
     context 'when user exists' do
       before(:each) { user.save }
@@ -49,68 +52,68 @@ describe UserUpdater do
 
         #TODO: Usar stubs declarando las variables devueltas (darle una vuelta)
         let(:related_documents) { ObjectFinder.execute(user) }
-        let(:outdated_documents) { ObjectOutdaterDecorator.new(related_documents) }
-        let(:outdated_parents) { ParentsOutdaterDecorator.new(outdated_documents) }
+        let(:outdated_documents) { ObjectOutdater.new(related_documents) }
+        let(:outdated_parents) { ParentsOutdater.new(outdated_documents) }
 
-        it 'calls ObjectOutdaterDecorator.new method with related_documents' do
-          expect(ObjectOutdaterDecorator).to receive(:new).with(related_documents)
+        it 'calls ObjectOutdater.new method with related_documents' do
+          expect(ObjectOutdater).to receive(:new).with(related_documents)
           execute(updated_user)
         end
 
         it 'calls outdate_objects method with user' do
-          expect_any_instance_of(ObjectOutdaterDecorator).to receive(:outdate_objects).with(user)
+          expect_any_instance_of(ObjectOutdater).to receive(:outdate_objects).with(user)
           execute(updated_user)
         end
 
-        it 'calls ParentsOutdaterDecorator.new method with outdate_objects' do
-          expect(ParentsOutdaterDecorator).to receive(:new).with(outdate_objects)
+        it 'calls ParentsOutdater.new method with outdate_objects' do
+          expect(ParentsOutdater).to receive(:new).with(outdate_objects)
           execute(updated_user)
         end
 
         it 'calls outdate_parents method with user' do
-          expect_any_instance_of(ParentsOutdaterDecorator).to receive(:outdate_parents).with(user)
+          expect_any_instance_of(ParentsOutdater).to receive(:outdate_parents).with(user)
           execute(updated_user)
         end
 
-        it 'calls ArraySaverDecorator.new method with outdate_objects' do
-          expect(ArraySaverDecorator).to receive(:new).with(outdate_parents)
+        it 'calls ArraySaver.new method with outdate_objects' do
+          expect(ArraySaver).to receive(:new).with(outdate_parents)
           execute(updated_user)
         end
 
         it 'calls save_all method' do
-          expect_any_instance_of(ArraySaverDecorator).to receive(:save_all)
+          expect_any_instance_of(ArraySaver).to receive(:save_all)
           execute(updated_user)
         end
       end
 
       context 'when there are not documents related to user' do
-        it 'does not call ObjectOutdaterDecorator.new method' do
-          expect(ObjectOutdaterDecorator).to_not receive(:new)
+        it 'does not call ObjectOutdater.new method' do
+          expect(ObjectOutdater).to_not receive(:new)
           execute(updated_user)
         end
 
         it 'does not call outdate_objects method' do
-          expect_any_instance_of(ObjectOutdaterDecorator).to_not receive(:outdate_objects)
+          expect_any_instance_of(ObjectOutdater).to_not receive(:outdate_objects)
           execute(updated_user)
         end
 
-        it 'does not call ParentsOutdaterDecorator.new method' do
-          expect(ParentsOutdaterDecorator).to receive(:new)
+        it 'does not call ParentsOutdater.new method' do
+          expect(ParentsOutdater).to receive(:new)
           execute(updated_user)
         end
 
         it 'does not call outdate_parents method' do
-          expect_any_instance_of(ParentsOutdaterDecorator).to receive(:outdate_parents)
+          expect_any_instance_of(ParentsOutdater).to receive(:outdate_parents)
           execute(updated_user)
         end
 
-        it 'does not call ArraySaverDecorator.new method' do
-          expect(ArraySaverDecorator).to receive(:new)
+        it 'does not call ArraySaver.new method' do
+          expect(ArraySaver).to receive(:new)
           execute(updated_user)
         end
 
         it 'does not call save_all method' do
-          expect_any_instance_of(ArraySaverDecorator).to receive(:save_all)
+          expect_any_instance_of(ArraySaver).to receive(:save_all)
           execute(updated_user)
         end
       end
@@ -131,33 +134,33 @@ describe UserUpdater do
         execute(updated_user)
       end
 
-      it 'does not call ObjectOutdaterDecorator.new method' do
-        expect(ObjectOutdaterDecorator).to_not receive(:new)
+      it 'does not call ObjectOutdater.new method' do
+        expect(ObjectOutdater).to_not receive(:new)
         execute(updated_user)
       end
 
       it 'does not call outdate_objects method' do
-        expect_any_instance_of(ObjectOutdaterDecorator).to_not receive(:outdate_objects)
+        expect_any_instance_of(ObjectOutdater).to_not receive(:outdate_objects)
         execute(updated_user)
       end
 
-      it 'does not call ParentsOutdaterDecorator.new method' do
-        expect(ParentsOutdaterDecorator).to receive(:new)
+      it 'does not call ParentsOutdater.new method' do
+        expect(ParentsOutdater).to receive(:new)
         execute(updated_user)
       end
 
       it 'does not call outdate_parents method' do
-        expect_any_instance_of(ParentsOutdaterDecorator).to receive(:outdate_parents)
+        expect_any_instance_of(ParentsOutdater).to receive(:outdate_parents)
         execute(updated_user)
       end
 
-      it 'does not call ArraySaverDecorator.new method' do
-        expect(ArraySaverDecorator).to receive(:new)
+      it 'does not call ArraySaver.new method' do
+        expect(ArraySaver).to receive(:new)
         execute(updated_user)
       end
 
       it 'does not call save_all method' do
-        expect_any_instance_of(ArraySaverDecorator).to receive(:save_all)
+        expect_any_instance_of(ArraySaver).to receive(:save_all)
         execute(updated_user)
       end
     end
