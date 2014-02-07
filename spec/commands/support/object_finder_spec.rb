3a38d5e1ed8605a6fcbfbaf91938c9c7061a0070
Medico <voldemortthegreat@gmail.com>
2014-02-07 12:33:10 +0100

    add test for initialize method from ObjectFinder

--- spec/commands/support/object_finder_spec.rb
+++ spec/commands/support/object_finder_spec.rb
@@ -4,6 +4,28 @@ describe ObjectFinder do
   # Variables
 
   # Methods
+  describe '#initialize(pattern_object)' do
+    context 'when pattern_object is not nil' do
+      let(:object_finder) { ObjectFinder.new(pattern_object) }
+
+      it 'uses given pattern_object' do
+        expect(object_finder.object).to eq pattern_object
+      end
+
+      it 'creates an instance of ObjectFinder' do
+        expect(object_finder).to be_instance_of ObjectFinder
+      end
+    end
+
+    context 'when pattern_object is nil' do
+      let(:object_finder) { ObjectFinder.new(nil) }
+
+      it 'returns nil' do
+        expect(object_finder).to eq nil
+      end
+    end
+  end
+
   describe '#execute(object)' do
     context 'when object is an user' do
       pending 'calls request where with user'
