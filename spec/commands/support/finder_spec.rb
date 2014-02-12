6c8d1ec6de9fce6abc8e5360aa6b5d727be4d2d9
Medico <voldemortthegreat@gmail.com>
2014-02-12 14:37:59 +0100

    adds test for Finder

--- spec/commands/support/finder_spec.rb
+++ spec/commands/support/finder_spec.rb
@@ -1,140 +1,74 @@
 require 'spec_helper'
 
 describe Finder do
+  # Variables
+  let(:user) { Fabricate.build(:user_with_items) }
+  let(:request) { Fabricate.build(:request, user:user) }
+  let(:offer) { Fabricate.build(:offer, user_composer:user) }
+  let(:negotiation) { Fabricate.build(:negotiation, offer:offer) }
+  let(:deal) { Fabricate.build(:deal, negotiation:negotiation) }
+  let(:object) { user.sheet }
+  let(:collections) { ['Request','Offer','Negotiation','Deal'] }
+  let(:related_documents) do
+    related_documents = Array.new
+    related_documents << request.user_sheet
+    related_documents << offer.user_sheets.where(_id:user._id)
+    related_documents << negotiation.user_sheets.where(_id:user._id)
+    related_documents << deal.user_sheets.where(_id:user._id)
+    related_documents
+  end
+
+  # Methods
+  describe 'self.find(object, collections[])' do
+    context 'when object is not nil and every collection in array exists' do
+      it 'calls where method with object for every member' do
+        collections.each do |member|
+          expect(member).to receive(:where).with(object)
+        end
+        Finder.find(object, collections)
+      end
+
+      context 'when object has related documents' do
+        it 'returns related documents' do
+          user.save
+          request.save
+          offer.save
+          negotiation.save
+          deal.save
+          expect(Finder.find(object, collections)).to eq related_documents
+        end
+      end
+
+      context 'when object has not related documents' do
+        it 'returns an empty array' do
+          expect(Finder.find(object, collections)).to eq Array.new
+        end
+      end
+    end
+
+    context 'when object is nil' do
+      it 'raises an error' do
+        expect{ Finder.find(nil, collections) }.to raise_error(StandardError, "given object is nil.")
+      end
+    end
+
+    context 'when collections array contains a non existent collection' do
+      it 'raises an error' do
+        collections << 'Collection'
+        expect{ Finder.find(object, collections) }.to raise_error(StandardError, "collections array contains a non existent collection.")
+      end
+    end
+
+    context 'when collections array is empty' do
+      it 'raises an error' do
+        expect{ Finder.find(object, Array.new) }.to raise_error(StandardError, "collections array is empty.")
+      end
+    end
+
+    context 'when collections array is nil' do
+      it 'raises an error' do
+        expect{ Finder.find(object, nil) }.to raise_error(StandardError, "collections array is nil.")
+      end
+    end
+  end
 end
-  # # Variables
-  # let(:user) { Fabricate.build(:user_with_items) }
-  # let(:request) { Fabricate.build(:request, user:user) }
-  # let(:offer) { Fabricate.build(:offer, user_composer:user) }
-  # let(:negotiation) { Fabricate.build(:negotiation, offer:offer) }
-  # let(:deal) { Fabricate.build(:deal, negotiation:negotiation) }
-
-  # # Methods
-  # describe '#initialize(pattern_object)' do
-  #   context 'when pattern_object is not nil' do
-  #     let(:object_finder) { ObjectFinder.new(pattern_object) }
-
-  #     it 'uses given pattern_object' do
-  #       expect(object_finder.object).to eq pattern_object
-  #     end
-
-  #     it 'creates an instance of ObjectFinder' do
-  #       expect(object_finder).to be_instance_of ObjectFinder
-  #     end
-  #   end
-
-  #   context 'when pattern_object is nil' do
-  #     let(:object_finder) { ObjectFinder.new(nil) }
-
-  #     it 'returns nil' do
-  #       expect(object_finder).to eq nil
-  #     end
-  #   end
-  # end
-
-  # describe '#execute()' do
-  #   context 'when object to find is a user' do
-  #     let(:object_finder) { ObjectFinder.new(user) }
-
-  #     it 'searches user in Request collection' do
-  #       expect(Request).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     it 'searches user in Offer collection' do
-  #       expect(Offer).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     it 'searches user in Negotiation collection' do
-  #       expect(Negotiation).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     it 'searches user in Deal collection' do
-  #       expect(Deal).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     context 'when user has related documents' do
-  #       before(:each) do
-  #         request.save
-  #         offer.save
-  #         negotiation.save
-  #         deal.save
-  #       end
-
-  #       let(:related_documents) do
-  #         related_documents = Array.new
-  #         related_documents << request
-  #         related_documents << offer
-  #         related_documents << negotiation
-  #         related_documents << deal
-  #         related_documents
-  #       end
-
-  #       it 'returns an array' do
-  #         expect(object_finder.execute()).to be_instance_of Array
-  #       end
-
-  #       it 'returns related documents' do
-  #         expect(object_finder.execute()).to eq related_documents
-  #       end
-  #     end
-
-  #     context 'when user does not have any related documents' do
-  #       it 'returns nil' do
-  #         expect(object_finder.execute()).to eq nil
-  #       end
-  #     end
-  #   end
-
-  #   context 'when object to find is a user' do
-  #     let(:object_finder) { ObjectFinder.new(user) }
-
-  #     it 'searches user in Request collection' do
-  #       expect(Request).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     #TODO: Meter todo esto en un shared_example
-  #     it 'searches user in Offer collection' do
-  #       expect(Offer).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     it 'searches user in Negotiation collection' do
-  #       expect(Negotiation).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     it 'searches user in Deal collection' do
-  #       expect(Deal).to receive(:where).with(user)
-  #       object_finder.execute()
-  #     end
-
-  #     context 'when user has related documents' do
-  #       before(:each) do
-  #         request.save
-  #         offer.save
-  #         negotiation.save
-  #         deal.save
-  #       end
-
-  #       it 'returns an array' do
-  #         expect(object_finder.execute()).to be_instance_of Array
-  #       end
-
-  #       it 'returns related documents' do
-  #         expect(object_finder.execute()).to eq related_documents
-  #       end
-  #     end
-
-  #     context 'when user does not have any related documents' do
-  #       it 'returns nil' do
-  #         expect(object_finder.execute()).to eq nil
-  #       end
-  #     end
-  #   end
-  # end
-# end
