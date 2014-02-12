27b51779a4af7f673c772f58c72dfd2235e3f14f
Medico <voldemortthegreat@gmail.com>
2014-02-12 13:02:26 +0100

    pass all outdater tests again

--- app/commands/support/outdater.rb
+++ app/commands/support/outdater.rb
@@ -2,9 +2,11 @@ class Outdater
   def self.outdate(to_outdate=[])
     raise StandardError, "given array is empty" unless to_outdate != []
     raise StandardError, "given array is nil" unless to_outdate != nil
+    result = true
     to_outdate.each do |member|
       member.outdate
+      result &&= member.outdated
     end
-    to_outdate
+    result
   end
 end
