3154d112b30a53b1681eb70c5fdd06d9e3bd66f4
Medico <voldemortthegreat@gmail.com>
2014-02-12 13:27:55 +0100

    pass Saver tests

--- app/commands/support/saver.rb
+++ app/commands/support/saver.rb
@@ -1,10 +1,13 @@
 class Saver
   def self.save(to_save=[])
-    raise StandardError, "given array is empty" unless to_save != []
-    raise StandardError, "given array is nil" unless to_save != nil
+    raise StandardError, "given array is empty." unless to_save != []
+    raise StandardError, "given array is nil." unless to_save != nil
+    result = true
     to_save.each do |member|
+      raise StandardError, "given array contains a no valid member." unless member.valid?
       member.save
+      result &&= member.persisted?
     end
-    to_save
+    result
   end
 end
