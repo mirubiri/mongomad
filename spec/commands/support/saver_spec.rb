a5e730e26f6c97b780fecd807cc35bdcde680309
Medico <voldemortthegreat@gmail.com>
2014-02-13 09:41:37 +0100

    remove raise_error test for saver :)

--- spec/commands/support/saver_spec.rb
+++ spec/commands/support/saver_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe Saver do
+  # Variables
+  let(:array_to_save) do
+    array_to_save = Array.new
+    3.times { array_to_save << Fabricate.build(:product) }
+    array_to_save
+  end
+
+  # Methods
   describe 'self.save(to_save[])' do
     context 'when given array is not empty' do
-      let(:array_to_save) do
-        array_to_save = Array.new
-        3.times { array_to_save << Fabricate.build(:product) }
-        array_to_save
-      end
-
       it 'calls save method for every member' do
         array_to_save.each do |member|
           expect(member).to receive(:save)
@@ -24,22 +26,11 @@ describe Saver do
         array_to_save.last.stub(:save).and_return(:false)
         expect(Saver.save(array_to_save)).to eq false
       end
-
-      it 'raises an error if any member is not valid' do
-        array_to_save.last.quantity = nil
-        expect{ Saver.save(array_to_save) }.to raise_error(StandardError, "given array contains a no valid member.")
-      end
     end
 
     context 'when given array is empty' do
       it 'returns true' do
         expect(Saver.save(Array.new)).to eq true
-      end
-    end
-
-    context 'when given array is nil' do
-      it 'raises an error' do
-        expect{ Saver.save(nil) }.to raise_error(StandardError, "given array is nil.")
       end
     end
   end
