7c381fcb52b38dd9e0aeebf2b9665cb944a794b7
Medico <voldemortthegreat@gmail.com>
2014-02-13 10:03:41 +0100

    remove useless code :)

--- app/commands/support/saver.rb
+++ app/commands/support/saver.rb
@@ -1,9 +1,7 @@
 class Saver
   def self.save(to_save=[])
-    # raise StandardError, "given array is nil." unless to_save != nil
     result = true
     to_save.each do |member|
-    #   raise StandardError, "given array contains a no valid member." unless member.valid?
       member.save
       result &&= member.persisted?
     end
