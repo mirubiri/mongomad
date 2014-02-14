fdde36a4a0b64d0c4257bf351d6e7086e65bea3e
Medico <voldemortthegreat@gmail.com>
2014-02-14 11:42:33 +0100

    add test for Finder

--- spec/commands/support/finder_spec.rb
+++ spec/commands/support/finder_spec.rb
@@ -7,47 +7,69 @@ describe Finder do
   let(:offer) { Fabricate.build(:offer, user_composer:user) }
   let(:negotiation) { Fabricate.build(:negotiation, offer:offer) }
   let(:deal) { Fabricate.build(:deal, negotiation:negotiation) }
-  let(:object) { user.sheet }
-  let(:collections) { [Request,Offer,Negotiation,Deal] }
-  let(:related_documents) do
-    related_documents = Array.new
-    related_documents << request.user_sheet
-    related_documents << offer.user_sheets.where(_id:user._id)
-    related_documents << negotiation.user_sheets.where(_id:user._id)
-    related_documents << deal.user_sheets.where(_id:user._id)
-    related_documents
-  end
 
   # Methods
   describe 'self.find(object, collections[])' do
-    context 'when object and collections array are not empty' do
-      it 'calls where method with object for every member' do
-        collections.each do |member|
-          expect(member).to receive(:where).with(object)
-        end
-        Finder.find(object, collections)
-      end
+    context 'when object is a user_sheet' do
+      let(:object) { user.sheet }
+      let(:collections) { [Request, Offer, Negotiation, Deal] }
 
       context 'when object has related documents' do
-        it 'returns related documents' do
-          user.save
-          related_documents.each do |document|
+        before(:each) do
+          [user, request, offer, negotiation, deal].each do |document|
             document.save
           end
-          expect(Finder.find(object, collections)).to eq related_documents
+        end
+
+        let(:matched_objets) do
+          matched_objets = Array.new
+          matched_objets << request.user_sheet
+          matched_objets << offer.user_sheets.where(_id:object._id).first
+          matched_objets << negotiation.user_sheets.where(_id:object._id).first
+          matched_objets << deal.user_sheets.where(_id:object._id).first
+          matched_objets
+        end
+
+        it 'returns an array with matched objects' do
+           expect(Finder.find(object, collections)).to eq matched_objets
         end
       end
 
-      context 'when object has not related documents' do
+      context 'when object does not have related documents' do
         it 'returns an empty array' do
           expect(Finder.find(object, collections)).to eq Array.new
         end
       end
     end
 
-    context 'when collections array is empty' do
-      it 'returns an empty array' do
-        expect(Finder.find(object, Array.new)).to eq Array.new
+    context 'when object is a product' do
+      let(:object) { offer.proposal.goods.first }
+      let(:collections) { [Offer, Negotiation, Deal] }
+
+      context 'when object has related documents' do
+        before(:each) do
+          [user, offer, negotiation, deal].each do |document|
+            document.save
+          end
+        end
+
+        let(:matched_objets) do
+          matched_objets = Array.new
+          matched_objets << offer.proposal.goods.where(_id:object._id).first
+          matched_objets << negotiation.proposal.goods.where(_id:object._id).first
+          matched_objets << deal.agreement.goods.where(_id:object._id).first
+          matched_objets
+        end
+
+        it 'returns an array with matched objects' do
+          expect(Finder.find(object, collections)).to eq matched_objets
+        end
+      end
+
+      context 'when object does not have related documents' do
+        it 'returns an empty array' do
+          expect(Finder.find(object, collections)).to eq Array.new
+        end
       end
     end
   end
