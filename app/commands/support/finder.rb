cb42765b54a66c67c116548739471d16b7f52d76
Medico <voldemortthegreat@gmail.com>
2014-02-14 14:07:14 +0100

    pass all tests for Finder command

--- app/commands/support/finder.rb
+++ app/commands/support/finder.rb
@@ -3,12 +3,12 @@ class Finder
     matched_objects = Array.new
     if object.class == UserSheet
       collections.each do |collection|
-        if collection.class == Request
+        if collection == Request
           documents = collection.where("user_sheet._id" => object._id)
           documents.each do |document|
             matched_objects << document.user_sheet
           end
-        else # collection.class != Request
+        else # collection != Request
           documents = collection.where("user_sheets._id" => object._id)
           documents.each do |document|
             matched_objects << document.user_sheets.where(_id:object._id).first
@@ -17,15 +17,15 @@ class Finder
       end
     else # object.class != UserSheet
       collections.each do |collection|
-        if collection.class == Deal
-          documents = collection.where("agreement.goods._id" => object._id)
-          documents.each do |document|
-            matched_objects << document.agreement.goods.where(_id:object._id).first
-          end
-        else # collection.class != Deal
+        if collection == Offer
           documents = collection.where("proposal.goods._id" => object._id)
           documents.each do |document|
             matched_objects << document.proposal.goods.where(_id:object._id).first
+          end
+        else # collection.class != Offer
+          documents = collection.where("proposals.goods._id" => object._id)
+          documents.each do |document|
+            matched_objects << document.proposals.last.goods.where(_id:object._id).first
           end
         end
       end
