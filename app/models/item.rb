8409eada6a052dd8552cabf43fcd2a759a734236
Medico <voldemortthegreat@gmail.com>
2014-02-21 10:05:44 +0100

    change item states and remove useless methods and triggers

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,19 +7,17 @@ class Item
 
   field :name
   field :description
-  field :state,      default:'available'
+  field :state, default:'on_sale'
 
   validates_presence_of :user, :name, :description
-  validates_inclusion_of :state, in: ['available','unavailable','ghosted','discarded' ]
+  validates_inclusion_of :state, in: ['on_sale','withdrawn','sold']
 
   def state_machine(machine = nil)
     @state_machine ||= begin
       machine ||= MicroMachine.new(state)
 
-      machine.when(:available, 'unavailable' => 'available')
-      machine.when(:unavailable, 'available' => 'unavailable')
-      machine.when(:ghost, 'available' => 'ghosted', 'unavailable' => 'ghosted')
-      machine.when(:discard, 'ghosted' => 'discarded')
+      machine.when(:withdraw, 'on_sale' => 'withdrawn')
+      machine.when(:sell, 'on_sale' => 'sold')
 
       machine.on(:any) do
         self.state = @state_machine.state
@@ -28,43 +26,11 @@ class Item
     end
   end
 
-  def available
-    state_machine.trigger(:available)
+  def withdraw
+    state_machine.trigger(:withdraw)
   end
 
-  def unavailable
-    state_machine.trigger(:unavailable)
-  end
-
-  def ghost
-    state_machine.trigger(:ghost)
-  end
-
-  def discard
-    state_machine.trigger(:discard)
-  end
-
-  def pick(quantity)
-    Product.new(name:name, description:description, images:images, quantity:quantity) do |product|
-      product._id = id
-      product.owner_id = user_id
-    end
-  end
-
-  def sell(quantity)
-    quantity <= stock &&
-    begin
-      self.stock -= quantity
-      save
-    end
-  end
-
-  def supply(quantity)
-    self.stock += quantity
-    save
-  end
-
-  def available?(quantity)
-    stock != nil && quantity != 0 && quantity <= stock
+  def sell
+    state_machine.trigger(:sell)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,24 +12,22 @@ describe Item do
   # Attributes
   it { should be_timestamped_document }
   it { should have_fields :name, :description }
-  it { should have_field(:state).with_default_value_of('available') }
+  it { should have_field(:state).with_default_value_of('on_sale') }
 
   # Validations
   it { should validate_presence_of :user }
   it { should_not have_autosave_on :user }
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
-  it { should validate_inclusion_of(:state).to_allow('available','unavailable','ghosted','discarded') }
+  it { should validate_inclusion_of(:state).to_allow('on_sale','withdrawn','sold') }
 
   # Methods
   describe '#state_machine(machine)' do
     subject(:machine) { double().as_null_object }
     before(:each) { item.state_machine(machine) }
 
-    it { should have_received(:when).with(:available, 'unavailable' => 'available') }
-    it { should have_received(:when).with(:unavailable, 'available' => 'unavailable') }
-    it { should have_received(:when).with(:ghost, 'available' => 'ghosted', 'unavailable' => 'ghosted') }
-    it { should have_received(:when).with(:discard, 'ghosted' => 'discarded') }
+    it { should have_received(:when).with(:withdraw, 'on_sale' => 'withdrawn') }
+    it { should have_received(:when).with(:sell, 'on_sale' => 'sold') }
   end
 
   shared_examples 'an state machine event' do |action, initial_state, final_state|
@@ -50,83 +48,12 @@ describe Item do
     end
   end
 
-  describe '#available' do
-    it_should_behave_like 'an state machine event', :available, 'unavailable', 'available'
+  describe '#withdraw' do
+    it_should_behave_like 'an state machine event', :withdraw, 'on_sale', 'withdrawn'
   end
 
-  describe '#unavailable' do
-    it_should_behave_like 'an state machine event', :unavailable, 'available', 'unavailable'
-  end
-
-  describe '#ghost' do
-    it_should_behave_like 'an state machine event', :ghost, 'available', 'ghosted'
-  end
-
-  describe '#discard' do
-    it_should_behave_like 'an state machine event', :discard, 'ghosted', 'discarded'
-  end
-
-  describe '#pick(quantity)' do
-    it 'returns a Product filled with item name, description, images and given quantity' do
-      expect(Product).to receive(:new).with(name:item.name, description:item.description, images:item.images, quantity:1)
-      item.pick(1)
-    end
-
-    specify { expect(item.pick(1)._id).to eq item._id }
-    specify { expect(item.pick(1).owner_id).to eq item.user_id }
-  end
-
-  describe '#sell(quantity)' do
-    it 'removes the given quantity of items from the stock' do
-      expect{ item.sell(1) }.to change { item.stock }.by(-1)
-    end
-
-    it 'saves the change' do
-      item.sell(1)
-      expect(item).to be_persisted
-    end
-
-    context 'when given quantity is not available' do
-      it 'returns false' do
-        expect(item.sell(100)).to eq false
-      end
-
-      it 'does not change the stock attribute' do
-        expect{ item.sell(100) }.to_not change { item.stock }
-      end
-    end
-  end
-
-  describe '#supply(quantity)' do
-    it 're-stock this item with the given quantity' do
-      expect{ item.supply(1) }.to change { item.stock }.by(1)
-    end
-
-    it 'saves the change' do
-      item.supply(1)
-      expect(item).to be_persisted
-    end
-  end
-
-  describe '#available?(quantity)' do
-    it 'returns true if item has enough asked stock' do
-      asked = item.stock - 1
-      expect(item.available?(asked)).to eq true
-    end
-
-    it 'returns false if item has not enough asked stock' do
-      asked = item.stock + 1
-      expect(item.available?(asked)).to eq false
-    end
-
-    it 'returns false if stock is nil' do
-      item.stock = nil
-      expect(item.available?(1)).to eq false
-    end
-
-    it 'returns false if asked stock is 0' do
-      expect(item.available?(0)).to eq false
-    end
+  describe '#sell' do
+    it_should_behave_like 'an state machine event', :sell, 'on_sale', 'sold'
   end
 
   # Factories
