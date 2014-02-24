e29fee137f2a28a46c51cbff5e6e1c9b6aa40366
Medico <voldemortthegreat@gmail.com>
2014-02-24 10:40:32 +0100

    remove states and state_machine code from proposal

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -8,10 +8,9 @@ class Negotiation
   embeds_many             :proposals,  as: :proposal_container
   embeds_many             :messages,   as: :message_container
 
-  field :state, default:'open'
+  field :absent_user, type:Moped::BSON::ObjectId, default:nil
 
   validates_presence_of :users, :user_sheets, :proposals, :messages
-  validates_inclusion_of :state, in: ['open','successful','ghosted','closed']
 
   validate :check_number_of_users,
            :check_user_equality,
@@ -52,94 +51,29 @@ class Negotiation
   end
 
   public
-  def state_machine(machine = nil)
-    @state_machine ||= begin
-      machine ||= MicroMachine.new(state)
-
-      machine.when(:success, 'open' => 'successful')
-      machine.when(:ghost, 'open' => 'ghosted')
-      machine.when(:close, 'ghosted' => 'closed')
-      machine.when(:reset, 'ghosted' => 'open')
-      machine.when(:reopen, 'closed' => 'open')
-
-      machine.on(:any) do
-        self.state = @state_machine.state
-      end
-      machine
-    end
-  end
-
-  def success
-    state_machine.trigger(:success)
-  end
-
-  def ghost
-    state_machine.trigger(:ghost)
-  end
-
-  def close
-    state_machine.trigger(:close)
-  end
-
-  def reset
-    state_machine.trigger(:reset)
-  end
-
-  def reopen
-    state_machine.trigger(:reopen)
-  end
-
   def sign_proposal(user_id)
-    if gatekeeper(user_id, :sign)
-      proposal.sign
-    else
-      false
-    end
+    gatekeeper(user_id, :sign) ? proposal.sign : false
   end
 
   def confirm_proposal(user_id)
-    if gatekeeper(user_id, :confirm)
-      proposal.confirm
-    else
-      false
-    end
+    gatekeeper(user_id, :confirm) ? proposal.confirm : false
   end
 
-  def break_proposal
-    proposal.break
-  end
+  # def gatekeeper(user_id, action)
+  #   return false if state != 'open'
+  #   return false if ![proposal.composer_id, proposal.receiver_id].include? user_id
 
-  def reset_proposal
-    proposal.reset
-  end
+  #   return false if money_owner?(user_id) && action == :sign
+  #   return false if !money_owner?(user_id) && action == :confirm
 
-  def ghost_proposal
-    proposal.ghost
-  end
-
-  def discard_proposal
-    proposal.discard
-  end
-
-  def gatekeeper(user_id, action)
-    return false if state != 'open'
-    return false if ![proposal.composer_id, proposal.receiver_id].include? user_id
-
-    return false if money_owner?(user_id) && action == :sign
-    return false if !money_owner?(user_id) && action == :confirm
-
-    true
-  end
+  #   true
+  # end
 
   def proposal
     proposals.last
   end
 
-  def money_owner?(user_id)
-    if proposal.cash?
-      proposal.goods.type(Cash).last.owner_id == user_id
-    else
-      false
-    end
+  def cash_owner?(user_id)
+    proposal.cash? ? proposal.goods.type(Cash).last.owner_id == user_id : false
   end
 end
