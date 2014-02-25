8986ae6302b83d28527b534a518f61638c0e972b
Medico <voldemortthegreat@gmail.com>
2014-02-25 10:38:25 +0100

    remove not used fabricators

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -8,15 +8,3 @@ Fabricator(:user_with_items, from: :user) do
     user.items = 3.times.map { Fabricate(:item, user:user) }
   end
 end
-
-Fabricator(:user_with_sent_offers, from: :user_with_items) do
-  after_build do |user|
-    user.sent_offers << Fabricate(:offer, user_composer: user)
-  end
-end
-
-Fabricator(:user_with_received_offers, from: :user_with_items) do
-  after_build do |user|
-    user.received_offers << Fabricate(:offer, user_receiver: user)
-  end
-end
