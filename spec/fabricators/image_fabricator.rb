6a48d98238049db33da88113de5864c3e6f122bb
Medico <voldemortthegreat@gmail.com>
2014-02-26 13:29:30 +0100

    add new image examples to factory

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -1,10 +1,10 @@
 Fabricator(:image_face, from: :"attachment/image") do
-  id { %w(cara4_eddrkb cara1_khux83 cara3_pbobew cara_2_pkup80).sample }
+  id { %w(skzrrjmmxvrkavyktpqb esbuzqkoz0jt815cmcnz zlc3p5qoxg1h2ni4ntlo p1nintszjipizakntewc cara4_eddrkb cara1_khux83 cara3_pbobew cara_2_pkup80).sample }
   main true
 end
 
 Fabricator(:image_product, from: :"attachment/image") do
-  id { %w(product6_yrwcdq product5_asmr05 product2_cvlckt product4_dvcj1y product1_zugxjl product3_owqrux).sample }
+  id { %w(skzrrjmmxvrkavyktpqb esbuzqkoz0jt815cmcnz zlc3p5qoxg1h2ni4ntlo p1nintszjipizakntewc product6_yrwcdq product5_asmr05 product2_cvlckt product4_dvcj1y product1_zugxjl product3_owqrux).sample }
   main false
 end
 
