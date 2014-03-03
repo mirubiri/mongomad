86865c0ab943d161a2976c5d5e148be0e9951105
Medico <voldemortthegreat@gmail.com>
2014-03-03 13:10:01 +0100

    change images from factories

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -1,14 +1,14 @@
 Fabricator(:image_face, from: :"attachment/image") do
-  id { %w(cara4_eddrkb cara1_khux83 cara3_pbobew cara_2_pkup80).sample }
+  id { %w(boy1_dark, boy1_white, boy2_dark, boy2_white, boy3_dark, boy3_white, boy4_dark, boy4_white, boy5_dark, boy5_white, girl1_dark, girl1_white, girl2_dark, girl2_white, girl3_dark, girl3_white, girl4_dark, girl4_white, girl5_dark, girl5_white).sample }
   main true
 end
 
 Fabricator(:image_product, from: :"attachment/image") do
-  id { %w(product6_yrwcdq product5_asmr05 product2_cvlckt product4_dvcj1y product1_zugxjl product3_owqrux).sample }
+  id { %w(bicycle_dark, bicycle_white, car_dark, car_white, dog_dark, dog_white, house_dark, house_white, watch_dark, watch_white).sample }
   main false
 end
 
 Fabricator(:image_money, from: :"attachment/image") do
-  id "money_jcwnsz"
+ id { %w(cash1, cash2, cash3, cash4, cash5).sample }
   main true
 end
