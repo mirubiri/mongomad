0e3251e5ca91c91a630c07bd29e0661cc9feaa3c
Sergio <sergiodetorre@improcex.com>
2014-03-03 14:01:39 +0100

    fix image fabricators

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -1,14 +1,14 @@
 Fabricator(:image_face, from: :"attachment/image") do
-  id { %w(boy1_dark, boy1_white, boy2_dark, boy2_white, boy3_dark, boy3_white, boy4_dark, boy4_white, boy5_dark, boy5_white, girl1_dark, girl1_white, girl2_dark, girl2_white, girl3_dark, girl3_white, girl4_dark, girl4_white, girl5_dark, girl5_white).sample }
+  id { %w(uploads/users/boy1_dark uploads/users/boy1_white uploads/users/boy2_dark uploads/users/boy2_white uploads/users/boy3_dark uploads/users/boy3_white uploads/users/boy4_dark uploads/users/boy4_white uploads/users/boy5_dark uploads/users/boy5_white uploads/users/girl1_dark uploads/users/girl1_white uploads/users/girl2_dark uploads/users/girl2_white uploads/users/girl3_dark uploads/users/girl3_white uploads/users/girl4_dark uploads/users/girl4_white uploads/users/girl5_dark uploads/users/girl5_white).sample }
   main true
 end
 
 Fabricator(:image_product, from: :"attachment/image") do
-  id { %w(bicycle_dark, bicycle_white, car_dark, car_white, dog_dark, dog_white, house_dark, house_white, watch_dark, watch_white).sample }
+  id { %w(uploads/items/bicycle_dark uploads/items/bicycle_white uploads/items/car_dark uploads/items/car_white uploads/items/dog_dark uploads/items/dog_white uploads/items/house_dark uploads/items/house_white uploads/items/watch_dark uploads/items/watch_white).sample }
   main false
 end
 
 Fabricator(:image_money, from: :"attachment/image") do
- id { %w(cash1, cash2, cash3, cash4, cash5).sample }
+ id { %w(uploads/cash/cash1 uploads/cash/cash2 uploads/cash/cash3 uploads/cash/cash4 uploads/cash/cash5).sample }
   main true
 end
