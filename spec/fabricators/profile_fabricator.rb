518dbc2b5ffc36450612256f87b98c301b2a7396
Medico <voldemortthegreat@gmail.com>
2014-03-12 12:34:19 +0100

    add more data to profile fabricator

--- spec/fabricators/profile_fabricator.rb
+++ spec/fabricators/profile_fabricator.rb
@@ -2,7 +2,9 @@ Fabricator(:profile) do
   user       { Fabricate.build(:user, profile:nil) }
   first_name { Faker::Name.first_name }
   last_name  { Faker::Name.last_name }
+  gender     'male'
   language   'english'
-  location   [ Faker::Number.number(3), Faker::Number.number(3) ]
+  birth_date '05/08/1958'
+  location   [ Faker::Address.latitude, Faker::Address.longitude ]
   images     { [ Fabricate.build(:image_face) ] }
 end
