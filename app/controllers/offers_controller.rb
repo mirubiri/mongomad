bbae270c05f1f2a2ee17cbcf4b499349cd581572
Medico <voldemortthegreat@gmail.com>
2014-03-17 14:43:17 +0100

    create offers wth money

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -45,22 +45,87 @@ class OffersController < ApplicationController
     @offer = Offer.new(message:params[:offer][:message], user_composer:current_user, user_receiver:@user, user_sheets: [current_user.sheet, @user.sheet])
     @proposal = Proposal.new(composer_id:current_user.id, receiver_id:@user.id)
 
-    params[:offer][:goods].each do |good_params|
-      if (good_params[:type] == 'Product')
-        #TODO: reducir la búsqueda a los items del composer y del receiver
-        @item = Item.find(good_params[:item_id])
-        @good = Product.new(name:@item.name, description:@item.description, owner_id:@item.user.id, images:@item.images)
-        @good.id = @item.id
-      else
+puts "*******************************************************************************"
+puts params[:offer][:cash]
+# puts params[:offer][:cash]!=nil
+puts params[:offer][:cash]!=nil
+# puts params[:offer][:cash]==""
+# puts params[:offer][:cash]==[]
+puts params[:offer][:products]
+puts params[:offer][:products]!=nil
+puts "*******************************************************************************"
+
+
+    if (params[:offer][:cash]!=nil)
+      puts "ENTRAMOS A CASH"
+      params[:offer][:cash].each do |cash_params|
         @image = Attachment::Image.new(main:true)
         @image.id = 'static/images/money'
-        @good = Cash.new(owner_id:good_params[:owner_id])
-        @good.money = Money.new(good_params[:amount])
+        @good = Cash.new(owner_id:cash_params[:owner_id])
+        @good.money = Money.new(cash_params[:amount])
         @good.images << @image
+    puts "*******************************************************************************"
+puts @good.valid?
+puts "*******************************************************************************"
+        @proposal.goods << @good
       end
-      @proposal.goods << @good
     end
+
+    if (params[:offer][:products]!=nil)
+      puts "ENTRAMOS A products"
+      params[:offer][:products].each do |product_params|
+        #TODO: reducir la búsqueda a los items del composer y del receiver
+        @item = Item.find(product_params[:item_id])
+        @good = Product.new(name:@item.name, description:@item.description, owner_id:@item.user.id, images:@item.images)
+        @good.id = @item.id
+        @proposal.goods << @good
+puts "*******************************************************************************"
+puts @good.valid?
+puts "*******************************************************************************"
+      end
+    end
+puts @proposal.valid?
+
+    # params[:offer][:products].each do |good_params|
+    #   if (good_params[:type] == 'Product')
+    #     #TODO: reducir la búsqueda a los items del composer y del receiver
+    #     @item = Item.find(good_params[:item_id])
+    #     @good = Product.new(name:@item.name, description:@item.description, owner_id:@item.user.id, images:@item.images)
+    #     @good.id = @item.id
+    #   else
+    #     @image = Attachment::Image.new(main:true)
+    #     @image.id = 'static/images/money'
+    #     @good = Cash.new(owner_id:good_params[:owner_id])
+    #     @good.money = Money.new(good_params[:amount])
+    #     @good.images << @image
+    #   end
+    #   @proposal.goods << @good
+    # end
+
+
+
+
+    # params[:offer][:goods].each do |good_params|
+    #   if (good_params[:type] == 'Product')
+    #     #TODO: reducir la búsqueda a los items del composer y del receiver
+    #     @item = Item.find(good_params[:item_id])
+    #     @good = Product.new(name:@item.name, description:@item.description, owner_id:@item.user.id, images:@item.images)
+    #     @good.id = @item.id
+    #   else
+    #     @image = Attachment::Image.new(main:true)
+    #     @image.id = 'static/images/money'
+    #     @good = Cash.new(owner_id:good_params[:owner_id])
+    #     @good.money = Money.new(good_params[:amount])
+    #     @good.images << @image
+    #   end
+    #   @proposal.goods << @good
+    # end
     @offer.proposal = @proposal
+
+puts "*******************************************************************************"
+puts @proposal.errors.messages
+puts "*******************************************************************************"
+
 
     respond_to do |format|
       if @offer.save
