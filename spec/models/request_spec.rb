034db698fabe9097a7ec465c784070d3f03d95f9
Medico <voldemortthegreat@gmail.com>
2014-04-01 10:53:02 +0200

    add discarded field tests for request

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -11,18 +11,65 @@ describe Request do
   # Attributes
   it { should be_timestamped_document }
   it { should have_field :text }
+  it { should have_field(:discarded).of_type(Boolean).with_default_value_of(false) }
 
   # Validations
   it { should validate_presence_of :user }
   it { should_not have_autosave_on :user }
   it { should validate_presence_of :user_sheet }
   it { should validate_length_of(:text).within(1..160) }
+  it { should validate_presence_of :discarded }
 
   # Checks
   it 'is invalid if there is no sheet for user' do
     request.user_id = nil
     expect(request).to have(1).error_on(:user_sheets)
     expect(request.errors_on(:user_sheets)).to include('Request should have one user_sheet for user.')
+  end
+
+  # Methods
+  describe '#discarded?' do
+    context 'when request is discarded' do
+      before(:each) { request.discarded = true }
+
+      it 'returns true' do
+        expect(request.discarded?).to eq true
+      end
+    end
+
+    context 'when request is undiscarded' do
+      before(:each) { request.discarded = false }
+
+      it 'returns false' do
+        expect(request.discarded?).to eq false
+      end
+    end
+  end
+
+  describe '#discard' do
+    context 'when request is discarded' do
+      before(:each) { request.discarded = true }
+
+      it 'does not change request discarded field' do
+        expect{ request.discard }.to_not change{ request.discarded }
+      end
+
+      it 'returns false' do
+        expect(request.discard).to eq false
+      end
+    end
+
+    context 'when request is undiscarded' do
+      before(:each) { request.discarded = false }
+
+      it 'changes request discarded field to true' do
+        expect{ request.discard }.to change{ request.discarded }.from(false).to(true)
+      end
+
+      it 'returns true' do
+        expect(request.discard).to eq true
+      end
+    end
   end
 
   # Factories
