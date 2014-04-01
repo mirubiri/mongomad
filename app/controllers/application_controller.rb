42ce1ea305a71a1032808e04938601e6338832f1
Medico <voldemortthegreat@gmail.com>
2014-04-01 13:29:07 +0200

    move methods from offer and negotiation controller to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,16 @@ class ApplicationController < ActionController::Base
     @current_user = User.find(current_user_id)
   end
 
-  helper_method :current_user #make this method available in views
+  def get_products_from_items(item_ids)
+    item_ids.map { |id| Item.find(id[:item_id]).to_product }
+  end
 
+  def parse_cash(cash_params)
+    cash = Cash.new(owner_id:cash_params[:owner_id])
+    cash.money = Money.new(cash_params[:amount])
+    cash.images << Attachment::Image.new(_id:'static/images/money', main:true)
+    cash
+  end
+
+  helper_method :current_user
 end
