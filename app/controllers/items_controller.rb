026b5770be2fa8794cc71b1b0699cfcb5dc36f56
Sergio <sergiodetorre@improcex.com>
2014-04-02 14:47:54 +0200

    fix create, update and destroy(discard) item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,13 +41,7 @@ class ItemsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @item = Item.new(name:params[:item][:name], description:params[:item][:description])
-
-    params[:item][:images].each do |image_params|
-      @image = Attachment::Image.new(main:image_params[:main])
-      @image.id = image_params[:public_id]
-      @item.images << @image
-    end
+    @item = update_item(Item.new, params[:item])
 
     #TODO: REVISAR SERGIO
     respond_to do |format|
@@ -64,19 +58,15 @@ class ItemsController < ApplicationController
     @user = User.find(params[:user_id])
     @item = @user.items.find(params[:id])
 
-    @item.name = params[:item][:name]
-    @item.description = params[:item][:description]
-    @item.images.all.delete
+    @item = update_item(@item, params[:item])
 
-    params[:item][:images].each do |image_params|
-      @image = Attachment::Image.new(main:image_params[:main])
-      @image.id = image_params[:public_id]
-      @item.images << @image
-    end
+    products = merge_arrays(search_in_offers_products_to_outdate(@item.id).to_a, search_in_negotiations_products_to_outdate(@item.id).to_a)
+
+    products = outdate_products(products)
 
     #TODO: REVISAR SERGIO
     respond_to do |format|
-      if @item.save
+      if save_products(products) && save_item(@item)
         @items = @user.items.desc(:updated_at)
         format.html { redirect_to user_items_path(@user), notice: 'Item has been successfully updated.' }
         format.js { render 'reload_items', :layout => false }
@@ -87,15 +77,112 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(params[:id])
+
+    @item.withdraw
+
+    products = merge_arrays(search_in_offers_products_to_withdraw(@item.id).to_a, search_in_negotiations_products_to_withdraw(@item.id).to_a)
+
+    products = withdraw_products(products)
 
     #TODO: REVISAR SERGIO
     respond_to do |format|
-      if @item.destroy
+      if save_products(products) && save_item(@item)
         format.html { redirect_to user_items_url, notice: 'Item has been successfully deleted.' }
       else
         # ni idea :)
       end
     end
+  end
+
+  private
+  def update_item(item, params)
+    name = params[:name]
+    description = params[:description]
+    images = []
+    params[:images].each do |image_params|
+      image = Attachment::Image.new(main:image_params[:main])
+      image.id = image_params[:public_id]
+      images << image
+    end
+
+    item.name = name
+    item.description = description
+    item.images = nil
+    item.images << images
+    item
+  end
+
+  def search_in_offers_products_to_outdate(item_id)
+    offers_array = Offer.where('proposal.goods._id' => item_id).and('proposal.goods.outdated' => false)
+    extract_products_from_offers_array(offers_array, item_id)
+  end
+
+  def search_in_offers_products_to_withdraw(item_id)
+    offers_array = Offer.where('proposal.goods._id' => item_id)
+    extract_products_from_offers_array(offers_array, item_id)
+  end
+
+  def search_in_negotiations_products_to_outdate(item_id)
+    negotiations_array = Negotiation.where('proposals.goods._id' => item_id).and('proposals.goods.outdated' => false)
+    extract_products_from_negotiations_array(negotiations_array, item_id)
+  end
+
+  def search_in_negotiations_products_to_withdraw(item_id)
+    negotiations_array = Negotiation.where('proposals.goods._id' => item_id)
+    extract_products_from_offers_array(negotiations_array, item_id)
+  end
+
+  def extract_products_from_offers_array(offers_array, item_id)
+    products = []
+    offers_array.each do |offer|
+      products = offer.proposal.goods.where(id:item_id)
+    end
+    products
+  end
+
+  def extract_products_from_negotiations_array(negotiations_array, item_id)
+    products = []
+    negotiations_array.each do |negotiation|
+      negotiation.proposals.each do |proposal|
+        products = proposal.goods.where(id:item_id)
+      end
+    end
+    products
+  end
+
+  def outdate_products(products)
+    products.each do |product|
+      product.outdated = true
+    end
+  end
+
+  def withdraw_products(products)
+    products.each do |product|
+      product.withdraw
+    end
+  end
+
+  def save_products(products)
+    products.each do |product|
+      product.valid? ? product.save : false
+    end
+  end
+
+  def save_item(item)
+    item.valid? ? item.save : false
+  end
+
+  #TODO: Eliminar esta horribilidad :)
+  def merge_arrays(array1, array2)
+    array = []
+    array1.each do |element|
+      array << element
+    end
+    array2.each do |element|
+      array << element
+    end
+    array
   end
 end
