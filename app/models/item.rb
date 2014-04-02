98d72d74acaa052dc60719a73656e08b505de883
Sergio <sergiodetorre@improcex.com>
2014-04-02 13:08:51 +0200

    comment discard method and field

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,9 @@ class Item
   field :name
   field :description
   field :state,      default:'on_sale'
-  field :discarded,  type:Boolean, default:false
+ # field :discarded,  type:Boolean, default:false
 
-  validates_presence_of  :user, :discarded
+  validates_presence_of  :user#, :discarded
   validates_length_of    :name, minimum: 1, maximum: 20
   validates_length_of    :description, minimum: 1, maximum: 200
   validates_inclusion_of :state, in: ['on_sale','withdrawn','sold']
@@ -49,7 +49,7 @@ class Item
     end
   end
 
-  def discard
-    discarded? ? false : self.discarded = true
-  end
+  # def discard
+  #   discarded? ? false : self.discarded = true
+  # end
 end
