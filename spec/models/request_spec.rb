59e59ce4cde252a308388eb01ca56b39fc8e73ff
Sergio <sergiodetorre@improcex.com>
2014-04-03 09:47:53 +0200

    rename discarded field to hidden in request

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -11,14 +11,14 @@ describe Request do
   # Attributes
   it { should be_timestamped_document }
   it { should have_field :text }
-  it { should have_field(:discarded).of_type(Boolean).with_default_value_of(false) }
+  it { should have_field(:hidden).of_type(Boolean).with_default_value_of(false) }
 
   # Validations
   it { should validate_presence_of :user }
   it { should_not have_autosave_on :user }
   it { should validate_presence_of :user_sheet }
   it { should validate_length_of(:text).within(1..160) }
-  it { should validate_presence_of :discarded }
+  it { should validate_presence_of :hidden }
 
   # Checks
   it 'is invalid if there is no sheet for user' do
@@ -28,28 +28,28 @@ describe Request do
   end
 
   # Methods
-  describe '#discard' do
-    context 'when request is discarded' do
-      before(:each) { request.discarded = true }
+  describe '#hide' do
+    context 'when request is hidden' do
+      before(:each) { request.hidden = true }
 
-      it 'does not change request discarded field' do
-        expect{ request.discard }.to_not change{ request.discarded }
+      it 'does not change request hidden field' do
+        expect{ request.hide }.to_not change{ request.hidden }
       end
 
       it 'returns false' do
-        expect(request.discard).to eq false
+        expect(request.hide).to eq false
       end
     end
 
-    context 'when request is undiscarded' do
-      before(:each) { request.discarded = false }
+    context 'when request is unhidden' do
+      before(:each) { request.hidden = false }
 
-      it 'changes request discarded field to true' do
-        expect{ request.discard }.to change{ request.discarded }.from(false).to(true)
+      it 'changes request hidden field to true' do
+        expect{ request.hide }.to change{ request.hidden }.from(false).to(true)
       end
 
       it 'returns true' do
-        expect(request.discard).to eq true
+        expect(request.hide).to eq true
       end
     end
   end
