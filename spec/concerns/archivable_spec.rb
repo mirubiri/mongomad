8182d9bfba0387a2552c1442574715453b168e3a
Alex <mirubiri@gmail.com>
2014-04-22 01:26:57 +0200

    refactors Archivable test

--- spec/concerns/archivable_spec.rb
+++ spec/concerns/archivable_spec.rb
@@ -9,9 +9,7 @@ describe Archivable do
   subject { Test::Archivable.new }
   it { should have_field(:archived).of_type(Boolean).with_default_value_of(true) }
 
-  describe 'default_scope' do
-  	it 'retrives only elements with archived = false' do
-  		expect(subject.default_scoping.call.selector).to eq({"archived"=>false})
-  	end
+  it 'scopes all queries to archived=false' do
+  	expect(subject.default_scoping.call.selector).to eq({"archived"=>false})
   end
 end
