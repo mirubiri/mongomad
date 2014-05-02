1adc259eb76baa38a91556ae91c70e95814e9f4c
Sergio <sergiodetorre@improcex.com>
2014-05-02 13:37:43 +0200

    pass tests for offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,23 +1,10 @@
 class Offer
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Proposable
 
-  has_and_belongs_to_many :users
-  belongs_to  :negotiation
-  embeds_many :user_sheets, class_name: 'UserSheet', as: :user_sheet_container
-  embeds_one  :proposal,      as: :proposal_container
+  proposal_historic :false
 
+  belongs_to :negotiation
   field :message
-
-  private :users,:proposal,:user_sheets,:user_ids
-
-  def add_participant(user)
-    users<<user
-    user_sheets<<user.sheet
-  end
-
-  def composer
-    user_sheets.where(id:proposal.composer)
-  end
-
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -13,64 +13,18 @@ describe Offer do
   let(:receiver_sheet) { receiver.sheet }
   let(:offer) { Offer.new }
 
+  specify { expect(Offer).to be < Proposable }
+  
   # Relations
-  it { should have_and_belong_to_many(:users) }
   it { should belong_to :negotiation }
-  it { should embed_one :proposal }
-  it { should embed_many(:user_sheets).of_type(UserSheet) }
 
   # Attributes
   it { should be_timestamped_document }
   it { should have_field :message }
 
   # Methods
-  it { should_not respond_to(:users,:proposal,:proposal=,:user_sheets,:user_ids) }
-
-  describe '#add_participant' do
-    it 'adds a participant' do
-      offer.add_participant(composer)
-      expect(offer.composer).to eq composer_sheet
-    end
-  end
-
-
-  describe '#composer' do
-    it 'returns the composer sheet if composer was set' do
-      offer.composer=composer
-      expect(offer.composer).to eq composer_sheet
-    end
-
-    it 'returns nil' do
-      expect(offer.composer).to eq nil
-    end
-  end
-
-  describe '#receiver' do
-    it 'returns the receiver sheet if composer was set' do
-      offer.receiver=receiver
-      expect(proposal.receiver).to eq receiver_sheet
-    end
-
-    it 'returns nil if receiver is not set' do
-      expect(offer.receiver).to eq nil
-    end
-  end
 
   describe '#negotiate' do
-    describe 'returned negotiation' do
-      it 'includes composer as participant' do
-        expect(negotiate.composer).to eq offer.composer
-      end
-
-      it 'includes receiver as participant' do
-        expect(negotiation.receiver).to eq offer.receiver
-      end
-
-      it 'includes the proposal in offer' do
-
-      end
-    end
   end
-
   # Factories
 end
