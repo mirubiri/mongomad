814f4c3a665342833aef06fcc31c2921cf8d3f7c
Medico <voldemortthegreat@gmail.com>
2014-05-06 14:04:22 +0200

    add helpers for partials and layouts paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def unique_partial(partial_name)
+    "application/unique_components/#{partial_name}"
+  end
+
+  def unique_layout(layout_name)
+    "application/unique_components/layouts/#{layout_name}"
+  end
+
+  def shared_partial(partial_name)
+    "application/shared_components/#{partial_name}"
+  end
+
+  def shared_layout(layout_name)
+    "application/unique_components/layouts/#{layout_name}"
+  end
 end
