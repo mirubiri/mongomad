4961d53a48b5f1be8e904a77e19385841ca9851a
Medico <voldemortthegreat@gmail.com>
2014-05-06 14:24:10 +0200

    use new path helpers in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def unique_partial(partial_name)
-    "application/unique_components/#{partial_name}"
+    "application/unique_components/_#{partial_name}"
   end
 
   def unique_layout(layout_name)
-    "application/unique_components/layouts/#{layout_name}"
+    "application/unique_components/_#{layout_name}_layout"
   end
 
   def shared_partial(partial_name)
-    "application/shared_components/#{partial_name}"
+    "application/shared_components/_#{partial_name}"
   end
 
   def shared_layout(layout_name)
-    "application/unique_components/layouts/#{layout_name}"
+    "application/unique_components/_#{layout_name}_layout"
   end
 end
