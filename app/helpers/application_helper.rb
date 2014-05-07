9cb830c6411df287ff5644f1d7762a5e0d33691b
Medico <voldemortthegreat@gmail.com>
2014-05-07 14:24:18 +0200

    fix minor errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,17 @@
 module ApplicationHelper
-  
-  def unique_partial(partial_name)
-    "application/unique_components/"+partial_name
-  end
-
-  def unique_layout(layout_name)
-    "application/unique_components/"+layout_name+"_layout"
-  end
-
   def shared_partial(partial_name)
-    "application/shared_components/"+partial_name
+    "application/shared_components/#{partial_name}"
   end
 
   def shared_layout(layout_name)
-    "application/shared_components/"+layout_name+"_layout"
+    "application/shared_components/#{layout_name}_layout"
   end
 
+  def unique_partial(partial_name)
+    "application/unique_components/#{partial_name}"
+  end
+
+  def unique_layout(layout_name)
+    "application/unique_components/#{layout_name}_layout"
+  end
 end
