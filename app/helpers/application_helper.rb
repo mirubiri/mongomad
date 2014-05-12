2221a9fe3040a228c96fe2345da2a7a13268db03
Medico <voldemortthegreat@gmail.com>
2014-05-12 12:22:56 +0200

    add image_tag helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,25 @@ module ApplicationHelper
   def unique_layout(layout_name)
     "application/unique_components/#{layout_name}_layout"
   end
+
+  def rounded_image_tag(public_id, width, height)
+    cl_image_tag(public_id,
+      :width => 200, :height => 200,
+      :html_width => width, :html_height => height,
+      :crop => :fit, :radius => :max)
+  end
+
+  def squared_image_tag(public_id, width, height)
+    cl_image_tag(public_id,
+      :width => 200, :height => 200,
+      :html_width => width, :html_height => height,
+      :crop => :fit, :radius => 20)
+  end
+
+  def static_image_tag(public_id, width, height)
+    cl_image_tag(public_id,
+      :width => width, :height => height,
+      :html_width => width, :html_height => height,
+      :crop => :fit)
+  end
 end
