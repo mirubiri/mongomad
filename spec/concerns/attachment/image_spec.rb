ac97846968429d1da975f17c4f19d0e0b035ba90
Alex <mirubiri@gmail.com>
2014-05-19 00:27:47 +0200

    add x, y, width, height fields to image

--- spec/concerns/attachment/image_spec.rb
+++ spec/concerns/attachment/image_spec.rb
@@ -6,6 +6,10 @@ describe Attachment::Image do
 
   # Attributes
   it { should have_field(:main).of_type(Boolean) }
+  it { should have_field(:height).of_type(Integer) }
+  it { should have_field(:width).of_type(Integer) }
+  it { should have_field(:x).of_type(Integer) }
+  it { should have_field(:y).of_type(Integer) }
 
   # Factories
   specify { expect(Fabricate.build(:image_face)).to be_valid }
