a847ce0153435f5c7e6ec242474bf51bf15b572a
Alex <mirubiri@gmail.com>
2014-05-19 00:28:53 +0200

    pass test for image

--- app/models/concerns/attachment/image.rb
+++ app/models/concerns/attachment/image.rb
@@ -5,5 +5,9 @@ module Attachment
     embedded_in :attachable, polymorphic:true
 
     field :main, type: Boolean, default:false
+    field :width, type: Integer
+    field :height, type: Integer
+    field :x, type: Integer
+    field :y, type: Integer
   end
 end
