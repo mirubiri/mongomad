1f3381f7d12652e916c09f4883afa1aeac178da1
Alex <mirubiri@gmail.com>
2014-05-22 01:35:42 +0200

    Initial tests for OfferBuilder

--- spec/builders/offer_builder_spec.rb
+++ spec/builders/offer_builder_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 describe OfferBuilder do
+	let(:builder) { OfferBuilder.new }
+	let(:composer) { Fabricate.build(:user) }
+	let(:receiver) { Fabricate.build(:user) }
+	let(:item) { Fabricate.build(:item) }
+	let(:goods) { [{item.id,_type:'Product'},{user_id:composer.id,amount:100,_type:'Cash'}] }
+
+	let(:filled_builder) do
+		builder.composer(composer).receiver(receiver).goods(goods)
+	end
+
+	let(:offer) { filled_builder.build }
 
 	describe '#composer(user)' do
+		expect(builder.composer composer).to eq builder
 	end
 
 	describe '#receiver(user)' do
+		expect(builder.receiver receiver).to eq builder
 	end
 
-	describe '#items(goods)' do
+	describe '#goods(goods)' do
+		expect(builder.goods goods).to eq builder
 	end
 
-	describe '#cash(cash)' do
+	describe '#build' do
+		specify { expect(offer.composer).to eq composer.sheet }
+		specify { expect(offer.receiver).to eq receiver.sheet }
+		specify { expect(offer).goods to include(item.to_product) }
 	end
-
  end
