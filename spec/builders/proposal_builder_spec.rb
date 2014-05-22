430d33fce1e42882c1b4b6c068de3c242d4ae78a
Alex <mirubiri@gmail.com>
2014-05-22 02:16:48 +0200

    implement proposal builder

--- spec/builders/proposal_builder_spec.rb
+++ spec/builders/proposal_builder_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
 
 describe ProposalBuilder do
-	describe '#composer' do
+	let(:builder) { ProposalBuilder.new }
+	let(:composer) { Fabricate.build(:user) }
+	let(:receiver) { Fabricate.build(:user) }
+	let(:item) { Fabricate(:item) }
+	let(:product) { item.to_product }
+	let(:cash) { Fabricate.build(:cash,user_id:composer.id)}
+	let(:goods) { [{id:item.id,_type:'Product'},{user_id:cash.user_id,amount:cash.amount,_type:'Cash'}] }
+
+	let(:filled_builder) do
+		builder.composer(composer).receiver(receiver).goods(goods)
 	end
 
-	describe '#receiver' do
+	let(:offer) { filled_builder.build }
+
+	describe '#composer(user)' do
+		specify { expect(builder.composer(composer)).to eq builder }
 	end
 
-	describe '#items' do
+	describe '#receiver(user)' do
+		specify { expect(builder.receiver(receiver)).to eq builder }
 	end
 
-	describe '#cash' do
+	describe '#goods(goods)' do
+		specify { expect(builder.goods(goods)).to eq builder }
 	end
-end
+
+	describe '#build' do
+		specify { expect(offer.composer_id).to eq composer.id }
+		specify { expect(offer.receiver_id).to eq receiver.id }
+		specify { expect(offer.goods).to include(product) }
+		specify { expect(offer.goods).to include(cash) }
+	end
+ end
