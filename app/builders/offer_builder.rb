74003ccfd8268c549e7d59c04520c8d2161c6db7
Alex <mirubiri@gmail.com>
2014-05-22 05:04:58 +0200

    implement OfferBuilder

--- app/builders/offer_builder.rb
+++ app/builders/offer_builder.rb
@@ -1,2 +1,30 @@
 class OfferBuilder
+	def initialize()
+		@offer=Offer.new
+		@proposal_builder=ProposalBuilder.new
+	end
+
+	def composer(user)
+		@offer.user_ids<<user.id
+		@offer.user_sheets<<user.sheet
+		@proposal_builder.composer user
+		self
+	end
+
+	def receiver(user)
+		@offer.user_ids<<user.id
+		@offer.user_sheets<<user.sheet
+		@proposal_builder.receiver user
+		self
+	end
+
+	def goods(goods)
+		@proposal_builder.goods goods
+		self
+	end
+
+	def build
+		@offer.proposal=@proposal_builder.build
+		@offer
+	end
 end
--- spec/builders/offer_builder_spec.rb
+++ spec/builders/offer_builder_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe OfferBuilder do
+
+	let(:builder) { OfferBuilder.new }
+	let(:composer) { Fabricate.build(:user) }
+	let(:receiver) { Fabricate.build(:user) }
+	let(:item) { Fabricate(:item) }
+	let(:product) { item.to_product }
+	let(:cash) { Fabricate.build(:cash,user_id:composer.id)}
+	let(:goods) { [{id:item.id,_type:'Product'},{user_id:cash.user_id,amount:cash.amount,_type:'Cash'}] }
+
+	let(:filled_builder) do
+		builder.composer(composer).receiver(receiver).goods(goods)
+	end
+
+	let(:proposal) { builder.instance_variable_get(:@proposal_builder).build }
+	let(:offer) { filled_builder.build }
+
+	describe '#composer(user)' do
+		specify { expect(builder.composer composer).to eq builder }
+	end
+
+	describe '#receiver(user)' do
+		specify { expect(builder.receiver receiver).to eq builder }
+	end
+
+	describe '#goods(goods)' do
+		specify { expect(builder.goods goods).to eq builder }
+	end
+
+	describe '#build' do
+		specify { expect(offer.composer).to eq composer.sheet }
+		specify { expect(offer.receiver).to eq receiver.sheet }
+		specify { expect(offer.user_ids).to include(composer.id) }
+		specify { expect(offer.user_ids).to include(receiver.id) }
+		specify { expect(offer.proposal).to equal proposal }
+	end
 end
