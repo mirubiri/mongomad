2680d72b002f38eddc2df543795e2d70ed05c0ae
Alex <mirubiri@gmail.com>
2014-05-23 05:07:29 +0200

    implement cash builder

--- app/builders/cash_builder.rb
+++ app/builders/cash_builder.rb
@@ -1,2 +1,19 @@
 class CashBuilder
+	def initialize(cash=Cash.new)
+		@cash=cash
+	end
+
+	def user(user)
+		@cash.user_id=user.id
+		self
+	end
+
+	def amount(string)
+		@cash.amount=string
+		self
+	end
+
+	def build
+		@cash
+	end
 end
--- spec/builders/cash_builder_spec.rb
+++ spec/builders/cash_builder_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe CashBuilder do
+	let(:cash) { Fabricate.build(:cash) }
+	let(:user) { Fabricate(:user) }
+	let(:cash_params) { {id:'cash', user_id:user.id,amount:'100$'} }
+	let(:builder) { CashBuilder.new }
+	let(:filled_builder) { builder.user(user).amount(cash_params[:amount]) }
+	let(:cash) { filled_builder.build }
+
+	describe '#user(user)' do
+		specify { expect(builder.user user).to eq builder }
+	end
+
+	describe '#amount(string)' do
+		specify { expect(builder.amount cash_params[:amount]).to eq builder }
+	end
+
+	describe '#build' do
+		specify { expect(cash.id).to eq 'cash' }
+		specify { expect(cash.amount).to eq cash_params[:amount] }
+		specify { expect(cash.user_id).to eq cash_params[:user_id] }
+	end
 end
