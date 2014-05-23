a79497ec63a63f7c9ff1a4ec4ccd2dce354bcf68
Sergio <sergiodetorre@improcex.com>
2014-05-23 12:11:24 +0200

    activate devise_for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
-  protect_from_forgery
-
-  # TODO: Una vez insertado devise todo esto se debe quitar
-  def current_user
-    current_user_id = session[:current_user_id]
-    @current_user = User.find(current_user_id)
+  def after_sign_in_path_for(user)
+    user_offers_url(user)
   end
-
-  helper_method :current_user
-
 end
