f4ece43fca99848fc84bdfde07e3a07d4dd4e1a4
Alex <mirubiri@gmail.com>
2014-05-26 00:20:54 +0200

    Remove validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,9 +7,6 @@ class Message
   field :user_id, type:Moped::BSON::ObjectId
   field :text
 
-  validates_presence_of :user_id
-  validates_length_of   :text, minimum: 1, maximum: 400
-
   def user_sheet
     message_container.user_sheets.find(user_id)
   end
