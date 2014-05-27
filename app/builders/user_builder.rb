d6155703a6043c75fbbe645ca81b375fb5fc456f
Alex <mirubiri@gmail.com>
2014-05-27 07:39:58 +0200

    Implements UserBuilder

--- app/builders/user_builder.rb
+++ app/builders/user_builder.rb
@@ -1,2 +1,54 @@
 class UserBuilder
+	def initialize(user=User.new)
+		@user=user
+	end
+
+	def nick(nick)
+		@user.nick=nick
+		self
+	end
+
+	def password(password)
+		@user.password=password
+		self
+	end
+
+	def first_name(first_name)
+		@user.first_name=first_name
+		self
+	end
+
+	def last_name(last_name)
+		@user.last_name=last_name
+		self
+	end
+
+	def gender(gender)
+		@user.gender=gender
+		self
+	end
+
+	def birth_date(birth_date)
+		@user.birth_date=birth_date
+		self
+	end
+
+	def location(location)
+		@user.location=location
+		self
+	end
+
+	def language(language)
+		@user.language=language
+		self
+	end
+
+	def email(email)
+		@user.email=email
+		self
+	end
+
+	def build
+		@user
+	end
 end
--- spec/builders/user_builder_spec.rb
+++ spec/builders/user_builder_spec.rb
@@ -18,49 +18,49 @@ describe UserBuilder do
 	let(:user) { filled_builder.build }
 
 	describe '#first_name' do
-		specify { expect(builder.first_name first_name ).to eq builder }
+		specify { expect(builder.first_name 'first_name' ).to eq builder }
 	end
 
 	describe '#last_name' do
-		specify { expect(builder.last_name last_name ).to eq builder }
+		specify { expect(builder.last_name 'last_name' ).to eq builder }
 	end
 
 	describe '#gender' do
-		specify { expect(builder.gender gender ).to eq builder }
+		specify { expect(builder.gender ':male' ).to eq builder }
 	end
 
 	describe '#language' do
-		specify { expect(builder.language language ).to eq builder }
+		specify { expect(builder.language ':spanish' ).to eq builder }
 	end
 
 	describe '#birth_date' do
-		specify { expect(builder.birth_date birth_date ).to eq builder }
+		specify { expect(builder.birth_date '15/10/1981' ).to eq builder }
 	end
 
 	describe '#location' do
-		specify { expect(builder.location location ).to eq builder }
+		specify { expect(builder.location [1,2] ).to eq builder }
 	end
 
 	describe '#email' do
-		specify { expect(builder.email email ).to eq builder }
+		specify { expect(builder.email 'an@email.es' ).to eq builder }
 	end
 
 	describe '#nick' do
-		specify { expect(builder.nick nick ).to eq builder }
+		specify { expect(builder.nick 'nick' ).to eq builder }
 	end
 
 	describe '#password' do
-		specify { expect(builder.password password ).to eq builder }
+		specify { expect(builder.password 'password' ).to eq builder }
 	end
 
 	describe '#build' do
 		specify { expect(user.first_name).to eq 'first_name'}
 		specify { expect(user.last_name).to eq 'last_name' }
-		specify { expect(user.emal).to eq 'an@email.es' }
+		specify { expect(user.email).to eq 'an@email.es' }
 		specify { expect(user.password).to eq 'password' }
 		specify { expect(user.nick).to eq 'nick' }
 		specify { expect(user.location).to eq [1,2] }
-		specify { expect(user.birth_date).to eq '15/10/1981'}
+		specify { expect(user.birth_date).to eq Date.parse '15/10/1981'}
 		specify { expect(user.gender).to eq :male }
 		specify { expect(user.disabled).to eq false }
 	end
