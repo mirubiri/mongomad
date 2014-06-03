f0356cbd932efce938c75340549904e363320ca7
Medico <voldemortthegreat@gmail.com>
2014-06-03 12:12:42 +0200

    implement profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  
+
   layout 'exposition'
 
   def show
@@ -15,10 +15,25 @@ class ProfilesController < ApplicationController
   def update
     @requests = Request.all
 
+    if params[:image].present?
+      preloaded_file = Cloudinary::PreloadedFile.new(params[:image])
+      params[:profile][:images] = preloaded_file.public_id
+    end
+
+    builder = UserBuilder.new(@current_user)
+                         .first_name(params[:profile][:first_name])
+                         .last_name(params[:profile][:last_name])
+                         .location(params[:profile][:location])
+                         .images(params[:profile][:images])
+    user = builder.build
+
     respond_to do |format|
-      format.html
-      #format.json { render json: @items }
+      if user
+        user.save
+        format.html
+      else
+        format.html
+      end
     end
   end
-
 end
