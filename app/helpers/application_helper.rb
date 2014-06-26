d83fa675c60a5ad105304f4f8cc866bedd025458
Medico <voldemortthegreat@gmail.com>
2014-06-26 11:38:38 +0200

    remove object parameter in element and component application helpers and make needed cascade changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,35 @@
 module ApplicationHelper
 
-  def element(name,view:'default',data:nil)
+  def yield_content(content_key)
+    view_flow.content.delete(content_key)
+  end
+
+  def element(name,view:'default',locals:nil)
     element="elements/#{name}/#{name}"
     view="elements/#{name}/views/#{view}"
 
     render partial: element,
       layout: view,
-      object: data
+      locals: locals
   end
 
-  def component(name,view:'default',data:nil)
+  def component(name,view:'default',locals:nil)
     element="elements/components/#{name}/#{name}"
     view="elements/components/#{name}/views/#{view}"
 
     render partial: element,
       layout: view,
-      object: data
+      locals: locals
+  end
+
+  def active_tab?(*controller_names)
+    active = false
+    controller_names.each { |name| active ||= params[:controller] == name }
+    active ? 'active_nav' : nil
   end
 
   def static_image_tag(public_id)
     cl_image_tag("static/#{public_id}")
-  end
-
-  def yield_content(content_key)
-    view_flow.content.delete(content_key)
   end
 
   def object_main_image_tag(object,width,height)
@@ -40,11 +46,5 @@ module ApplicationHelper
       :html_width => width,
       :html_height => height,
       :crop => :fit)
-  end
-
-  def active_tab?(*controller_names)
-    active = false
-    controller_names.each { |name| active ||= params[:controller] == name }
-    active ? 'active_nav' : nil
   end
 end
