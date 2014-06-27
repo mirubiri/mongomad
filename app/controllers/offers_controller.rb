b2eef3306e8bf431fd4fdb7ddc83a8e06823a195
Medico <voldemortthegreat@gmail.com>
2014-06-27 12:30:46 +0200

    adds @data variable to offers_controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,64 +1,64 @@
 class OffersController < ApplicationController
 
   def index
-    @offers = Offer.all
-    # @requests = Request.where(user_id:current_user.id)
-    @requests = Request.all
+    @data.offers = Offer.where(user_id:@data.visited_user.id)
 
     respond_to do |format|
       format.html
-      #format.json { render json: @offers }
+      #format.json { render json: @data.offers }
     end
   end
 
   def new
-    @offer = Offer.new
-    # @items = Item.where(user_id:current_user.id)
-    # @requests = Request.where(user_id:current_user.id)
-    @items = Item.all
-    @requests = Request.all
+    @data.offer = Offer.new
+    @data.current_user = current_user
+    @data.visited_user_items = Item.where(user_id:@data.visited_user.id)
+    @data.current_user_items = Item.where(user_id:@data.current_user.id)
 
     respond_to do |format|
       format.html { render :template => "proposal/new" }
-      #format.json { render json: @offer }
+      #format.json { render json: @data.offer }
     end
   end
 
   def edit
-    @offer = Offer.find(params[:id])
+    @data.offer = Offer.find(params[:id])
+    @data.current_user = current_user
+    @data.visited_user_items = Item.where(user_id:@data.visited_user.id)
+    @data.current_user_items = Item.where(user_id:@data.current_user.id)
   end
 
   def create
-    @offer = Offer.new(params[:offer])
+    @data.offer = Offer.new(params[:offer])
 
     respond_to do |format|
       if @offer.save
-        format.html { redirect_to @offer, notice: 'Offer was successfully created.' }
-        #format.json { render json: @offer, status: :created, location: @offer }
+        format.html { redirect_to @data.offer, notice: 'Offer was successfully created.' }
+        #format.json { render json: @data.offer, status: :created, location: @data.offer }
       else
         format.html { render action: "new" }
-        #format.json { render json: @offer.errors, status: :unprocessable_entity }
+        #format.json { render json: @data.offer.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
-    @offer = Offer.find(params[:id])
+    @data.offer = Offer.find(params[:id])
 
     respond_to do |format|
-      if @offer.update_attributes(params[:offer])
-        format.html { redirect_to @offer, notice: 'Offer was successfully updated.' }
+      if @data.offer.update_attributes(params[:offer])
+        format.html { redirect_to @data.offer, notice: 'Offer was successfully updated.' }
         #format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        #format.json { render json: @offer.errors, status: :unprocessable_entity }
+        #format.json { render json: @data.offer.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @offer = Offer.find(params[:id])
-    @offer.destroy
+    @data.offer = Offer.find(params[:id])
+    @data.offer.destroy
 
     respond_to do |format|
       format.html { redirect_to offers_url }
