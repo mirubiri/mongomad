3f4d09dfe935c98036bb4043021353736b0d098c
Medico <voldemortthegreat@gmail.com>
2014-06-27 12:53:38 +0200

    add @data variable to profiles_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,6 @@
 class ProfilesController < ApplicationController
 
   def show
-    @profile = Profile.new
-    # @requests = Request.where(user_id:current_user.id)
-    @requests = Request.all
-
     respond_to do |format|
       format.html
       #format.json { render json: @items }
@@ -12,9 +8,6 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    # @requests = Request.where(user_id:current_user.id)
-    @requests = Request.all
-
     if params[:image].present?
       preloaded_file = Cloudinary::PreloadedFile.new(params[:image])
       params[:profile][:images] = preloaded_file.public_id
@@ -24,7 +17,7 @@ class ProfilesController < ApplicationController
                          .first_name(params[:profile][:first_name])
                          .last_name(params[:profile][:last_name])
                          .location(params[:profile][:location])
-                         #.images(params[:profile][:images])
+                         .images(params[:profile][:images])
     user = builder.build
 
     respond_to do |format|
