da5edd4da703b153f52953d4a60a84227a3e403b
Medico <voldemortthegreat@gmail.com>
2014-06-30 12:14:20 +0200

    refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :data_variable_definition
+
   def after_sign_in_path_for(user)
     offers_url
   end
 
-  def visited_user
-    User.find(params[:user_id])
+  def data_variable_definition
+    @data = OpenStruct.new
+    # @data.visited_user = visited_user
+    # @data.requests = Request.where(user_id:@data.visited_user.id)
   end
 
-  @data = OpenStruct.new
-  @data.visited_user = visited_user
-  @data.requests = Request.where(user_id:@data.visited_user.id)
+  def visited_user
+    #recuperar id del usuario
+  end
 end
