62394af9d9648e80348cf45f803a643a8dd32444
Sergio <sergiodetorre@gmail.com>
2014-07-01 12:48:29 +0200

    refactor data_variable_declaration method in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,28 @@ class ApplicationController < ActionController::Base
   end
 
   def data_variable_definition
-    @data = OpenStruct.new
-    @data.viewed_user = viewed_user
-    @data.requests = Request.where(user_id:@data.viewed_user.id)
+    data = OpenStruct.new
+    data.current_user = current_user
+    data.viewed_user = viewed_user
+    data.current_user_items = Item.where(user_id:data.current_user.id)
+    data.viewed_user_items = Item.where(user_id:data.viewed_user.id)
+
+    data.omnibar = OpenStruct.new
+    data.omnibar.avatar = data.current_user
+
+    data.dashboard = OpenStruct.new
+    data.dashboard.profile_card = data.viewed_user
+    data.dashboard.request_list = Request.where(user_id:data.viewed_user.id)
+
+    data.expositor = OpenStruct.new
+    #TODO: Completar
+    # data.expositor.items
+    # data.expositor.offers
+    # data.expositor.negotiations
+    # data.expositor.deals
   end
 
   def viewed_user
-    #recuperar id del usuario
-    User.last
+    #TODO: Recuperar id del usuario
   end
 end
