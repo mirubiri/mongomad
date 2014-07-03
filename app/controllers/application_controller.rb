01fd7987872ce75ce5e2ae989e18498a170c9d2a
medico <voldemortthegreat@gmail.com>
2014-07-03 11:46:29 +0200

    adds after signin path for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   helper_method :data,:visited_user
   layout :layout_by_resource
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || user_offers_path(current_user)
+  end
 
   def layout_by_resource
     if devise_controller?
