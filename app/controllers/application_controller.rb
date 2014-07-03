9869ab8636ad13372d1930598cb6ce7b00fc765c
Sergio <sergiodetorre@gmail.com>
2014-07-03 09:46:50 +0200

    first implementation of @data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :define_data
+
+  class DataValue
+    def offers
+      @offers ||= Offer.where("proposal.composer_id"=>visited_user.id)
+    end
+
+    def negotiations
+      @negotiations || = Negotiation.where(user_ids:current_user.id)
+    end
+
+    def deals
+      @deals ||= Deal.where(user_ids:current_user.id)
+    end
+
+    def requests
+      @requests ||= Request.where(user_id:visited_user.id)
+    end
+
+    def items
+      @requests ||= Item.where(user_id:visited_user.id)
+    end
+  end
 
   def visited_user
     @_visited_user ||= User.find(params[:id])
+  end
+
+  def define_data
+    @data ||= DataValue.new
   end
 end
