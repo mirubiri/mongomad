ae1120c06d8fd134fd19a4d42ad0b27de004cc86
Alex <mirubiri@gmail.com>
2014-07-03 06:41:39 +0200

    remove default layout and add state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,22 +4,22 @@ module ApplicationHelper
     view_flow.content.delete(content_key)
   end
 
-  def element(name,view:'default',data:nil)
+  def element(name,view:,data:nil,state:nil)
     element="elements/#{name}/#{name}"
     view="elements/#{name}/views/#{view}"
 
     render partial: element,
       layout: view,
-      locals:{name.to_sym => data}
+      locals:{name.to_sym => data,state:state}
   end
 
-  def component(name,view:'default',data:nil)
+  def component(name,view:,data:nil,state:nil)
     component="elements/components/#{name}/#{name}"
     view="elements/components/#{name}/views/#{view}"
 
     render partial: component,
       layout: view,
-      locals:{name.to_sym => data}
+      locals:{name.to_sym => data,state:state}
   end
 
   def active_tab?(*controller_names)
