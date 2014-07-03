806b9f33e6514118d89cb61c709a5e916d129522
Sergio <sergiodetorre@gmail.com>
2014-07-03 11:45:55 +0200

    put patch for devise in rails 4.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User
   include Mongoid::Document
+  class << self
+    def serialize_from_session(key, salt)
+      record = to_adapter.get(key.to_s)
+      record if record && record.authenticatable_salt == salt
+    end
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
