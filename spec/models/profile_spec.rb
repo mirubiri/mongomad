ce5abe8707eae030482b49dd21168841b6f217a6
Alex <mirubiri@gmail.com>
2014-07-08 22:38:01 +0200

    add some test to profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe Profile do
   let(:profile) { Fabricate.build(:profile) }
-  let(:splited_name) { profile.full_name.split}
-  let(:first_name) { splited_name.take(1)[0] }
-  let(:surnames) { splited_name.drop(1).join(' ') }
+  let(:one_word_full_name) { "one" }
+  let(:many_words_full_name) { "one two three"}
+  let(:first_word) { "one" }
+  let(:other_words) { "two three"}
 
   # Modules
   it { is_expected.to include_module Attachment::Images }
@@ -22,14 +23,38 @@ describe Profile do
   specify { expect(Fabricate.build(:profile)).to be_valid }
 
   describe '#first_name' do
-    it 'returns the first word in full_name ' do
-      expect(profile.first_name).to eq first_name
+    context 'one word name' do
+      before(:each) { profile.full_name=one_word_full_name}
+
+      it 'returns the first word in full_name ' do
+        expect(profile.first_name).to eq first_word
+      end
+    end
+
+    context 'more than one word name' do
+      before(:each) { profile.full_name=many_words_full_name }
+
+      it 'returns the first word in full_name' do
+        expect(profile.first_name).to eq first_word
+      end
     end
   end
 
   describe '#surnames' do
-    it 'returns the surnames on full_name' do
-      expect(profile.surnames).to eq surnames
+    context 'one word name' do
+      before(:each) { profile.full_name=one_word_full_name}
+
+      it 'returns an empty string' do
+        expect(profile.surnames).to eq String.new
+      end
+    end
+
+    context 'more than one word name' do
+      before(:each) { profile.full_name=many_words_full_name }
+
+      it 'returns the surnames on full_name' do
+        expect(profile.surnames).to eq other_words
+      end
     end
   end
 end
