2a30478824415c5ba33890b677cf0e2bac537b3f
medico <voldemortthegreat@gmail.com>
2014-07-09 10:47:37 +0200

    create requester component

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,23 +1,5 @@
 class RequestsController < ApplicationController
 
-  def new
-    request = Request.new
-
-    respond_to do |format|
-      format.html { render 'new', locals: { request:request } }
-      #format.json { render json: request }
-    end
-  end
-
-  def edit
-    request = Request.find(params[:id])
-
-    respond_to do |format|
-      format.html { render 'edit', locals: { request:request } }
-      #format.json { render json: request }
-    end
-  end
-
   def create
     builder = RequestBuilder.new
                             .user(current_user)
@@ -36,25 +18,6 @@ class RequestsController < ApplicationController
     end
   end
 
-  def update
-    request = Request.find(params[:id])
-
-    builder = RequestBuilder.new(request)
-                            .text(params[:request][:text])
-    request = builder.build
-
-    respond_to do |format|
-      if request
-        request.save
-        format.html { redirect_to user_offers_url(current_user), notice: 'Request was successfully updated.' }
-        #format.json { head :no_content }
-      else
-        format.html { redirect_to user_offers_url(current_user) }
-        #format.json { render json: request.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
     request = Request.find(params[:id])
     request.destroy
@@ -64,4 +27,5 @@ class RequestsController < ApplicationController
       #format.json { head :no_content }
     end
   end
+
 end
