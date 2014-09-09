c31445615c91d76a5f03524ddf93b8de81035cf6
medico <voldemortthegreat@gmail.com>
2014-09-09 12:25:06 +0200

    remove spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,7 +33,7 @@ module ApplicationHelper
   end
 
   def static_image_tag(public_id, css_class)
-    cl_image_tag("static/#{public_id}", :class => css_class )
+    cl_image_tag("static/#{public_id}", :class => css_class)
   end
 
   def user_image_tag(user, css_class)
@@ -41,7 +41,7 @@ module ApplicationHelper
       image = user.images.first
       cl_image_tag(image.id,
         :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => 80, :height => 80, :crop => :fit, :class => css_class )
+        :width => 80, :height => 80, :crop => :fit, :class => css_class)
     else
       static_image_tag('user_default', css_class)
     end
@@ -53,7 +53,7 @@ module ApplicationHelper
       image = images.first
       cl_image_tag(image.id,
         :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => width, :height => height, :crop => :fit, :class => css_class )
+        :width => width, :height => height, :crop => :fit, :class => css_class)
     end
   end
 
