76f7a1dbe413a5178dbfcbc3c0c51b421bf5f9ad
medico <voldemortthegreat@gmail.com>
2014-09-09 12:03:43 +0200

    add css_class parameter to image helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,36 +32,36 @@ module ApplicationHelper
     active ? 'active_nav' : nil
   end
 
-  def static_image_tag(public_id, width=35, height=35)
-    cl_image_tag("static/#{public_id}", :width => width, :height => height, :crop => :fill )
+  def static_image_tag(public_id, css_class, width=35, height=35)
+    cl_image_tag("static/#{public_id}", :width => width, :height => height, :crop => :fill, :class => css_class )
   end
 
-  def user_image_tag(user, width=80, height=80)
+  def user_image_tag(user, css_class, width=80, height=80)
     if user != nil && user.images.size != 0
       image = user.images.first
       cl_image_tag(image.id,
         :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => width, :height => height, :crop => :fit)
+        :width => width, :height => height, :crop => :fit, :class => css_class )
     else
-      static_image_tag('user_default',width,height)
+      static_image_tag('user_default', css_class, width, height)
     end
   end
 
-  def item_main_image_tag(item, width=155, height=155)
+  def item_main_image_tag(item, css_class, width=155, height=155)
     if item != nil
       image = item.images.where(main:true).first
       cl_image_tag(image.id,
         :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => width, :height => height, :crop => :fit)
+        :width => width, :height => height, :crop => :fit, :class => css_class )
     end
   end
 
-  def item_image_tag(item, index=0, width=155, height=155)
+  def item_image_tag(item, index=0, css_class, width=155, height=155)
     if item != nil
       image = item.images[index]
       cl_image_tag(image.id,
         :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => width, :height => height, :crop => :fit)
+        :width => width, :height => height, :crop => :fit, :class => css_class )
     end
   end
 end
