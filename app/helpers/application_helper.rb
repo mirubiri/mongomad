de7e70c2d9df4963cace584b08feaab342b8180f
Sergio <sergiodetorre@gmail.com>
2014-09-17 13:18:00 +0200

    remove comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,15 +31,4 @@ module ApplicationHelper
     controller_names.each { |name| active ||= params[:controller] == name }
     active ? 'active-nav' : nil
   end
-
-  
-
-  # def item_image_tag(item, index=0, css_class, width=155, height=155)
-  #   if item != nil && item.images.size > index
-  #     image = item.images[index]
-  #     cl_image_tag(image.id,
-  #       :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-  #       :width => width, :height => height, :crop => :fit, :class => css_class )
-  #   end
-  # end
 end
