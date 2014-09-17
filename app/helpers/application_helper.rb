a09e136bdb8416a78754af8a7c7466a87d9239c0
Sergio <sergiodetorre@gmail.com>
2014-09-17 13:17:07 +0200

    change image helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,30 +32,7 @@ module ApplicationHelper
     active ? 'active-nav' : nil
   end
 
-  def static_image_tag(public_id, css_class)
-    cl_image_tag("static/#{public_id}", :class => css_class)
-  end
-
-  def user_image_tag(user, css_class)
-    if user != nil && user.images.size != 0
-      image = user.images.first
-      cl_image_tag(image.id,
-        :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => 80, :height => 80, :crop => :fit, :class => css_class)
-    else
-      static_image_tag('user_default', css_class)
-    end
-  end
-
-  def item_main_image_tag(item, css_class, width=155, height=155)
-    images = item.images.where(main:true)
-    if item != nil && images.size != 0
-      image = images.first
-      cl_image_tag(image.id,
-        :transformation => { :x => image.x, :y => image.y, :width => image.w, :height => image.h, :crop => :crop },
-        :width => width, :height => height, :crop => :fit, :class => css_class)
-    end
-  end
+  
 
   # def item_image_tag(item, index=0, css_class, width=155, height=155)
   #   if item != nil && item.images.size > index
---
+++ app/helpers/image_helper.rb
@@ -0,0 +1,28 @@
+module ImageHelper
+
+  def static_image_tag(public_id, klass)
+    cl_image_tag("static/#{public_id}", class:klass)
+  end
+
+  def big_image_tag(image,klass)
+    base(h:300,w:300,klass:klass,image:image)
+  end
+
+  def medium_image_tag(image,klass)
+    base(h:155,w:155,klass:klass,image:image)
+  end
+
+  def small_image_tag(image,klass)
+    base(h:80,w:80,klass:klass,image:image)
+  end  
+
+  private
+  
+  def base(h:,w:,klass:,image:)
+    cl_image_tag(image_id,
+       transformation:[ 
+        {x:image.x, y:image.y, width:image.w, height:image.h, crop:crop },
+        {width:width, height:height, crop:fit, class:klass}
+       ])
+  end
+end
