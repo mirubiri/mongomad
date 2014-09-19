578d126b91f76402857ab1c8052a525dbea2fd62
medico <voldemortthegreat@gmail.com>
2014-09-19 10:00:05 +0200

    Remove class from image helpers

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,24 +1,24 @@
 module ImageHelper
 
-  def static_image_tag(public_id, klass)
-    cl_image_tag("static/#{public_id}", class:klass)
+  def static_image_tag(public_id)
+    cl_image_tag("static/#{public_id}")
   end
 
-  def big_image_tag(image,klass)
-    base(h:300,w:300,klass:klass,image:image)
+  def big_image_tag(image)
+    base(h:300,w:300,image:image)
   end
 
-  def medium_image_tag(image,klass)
-    base(h:155,w:155,klass:klass,image:image)
+  def medium_image_tag(image)
+    base(h:155,w:155,image:image)
   end
 
-  def small_image_tag(image,klass)
-    base(h:80,w:80,klass:klass,image:image)
+  def small_image_tag(image)
+    base(h:80,w:80,image:image)
   end
 
   private
-  def base(h:,w:,klass:,image:)
-    cl_image_tag(image.id, class:klass,
+  def base(h:,w:,image:)
+    cl_image_tag(image.id,
       transformation:[
         {x:image.x, y:image.y, width:image.w, height:image.h, crop: :crop },
         {width:w, height:h, crop: :fit}
