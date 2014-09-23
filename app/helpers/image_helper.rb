17629b5685452747a387533e387265cb91234cbc
Sergio <sergiodetorre@gmail.com>
2014-09-23 09:44:55 +0200

    fix image helpers

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,27 +1,27 @@
 module ImageHelper
 
-  def static_image_tag(public_id)
+  def static_image_path(public_id)
     cl_image_tag("static/#{public_id}")
   end
 
-  def big_image_tag(image)
+  def big_image_path(image)
     base(h:300,w:300,image:image)
   end
 
-  def medium_image_tag(image)
+  def medium_image_path(image)
     base(h:155,w:155,image:image)
   end
 
-  def small_image_tag(image)
+  def small_image_path(image)
     base(h:80,w:80,image:image)
   end
 
   private
   def base(h:,w:,image:)
-    cl_image_tag(image.id,
+    cl_image_path(image.id,
       transformation:[
-        {x:image.x, y:image.y, width:image.w, height:image.h, crop: :crop },
-        {width:w, height:h, crop: :fit}
+        { x:image.x, y:image.y, width:image.w, height:image.h, crop: :crop },
+        { width:w, height:h, crop: :fit }
       ]) if image
   end
 end
