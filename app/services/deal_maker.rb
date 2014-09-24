80567b1e2a87280e3582171d2fe60c165fb5886d
Sergio <sergiodetorre@gmail.com>
2014-09-24 09:17:27 +0200

    initial implementation for DealMaker

--- app/services/deal_maker.rb
+++ app/services/deal_maker.rb
@@ -1,9 +1,17 @@
 class DealMaker
+  attr_accessor :negotiation
+  
   def initialize(negotiation)
-    @negotiation=negotiation
+    self.negotiation=negotiation
   end
 
   def make_deal
-    true
+    return false unless negotiation.confirmer?
+    deal=Deal.new
+    deal.user_ids=negotiation.user_ids
+    deal.messages=negotiation.messages
+    deal.proposals=negotiation.proposals
+    negotiation.destroy
+    deal
   end
 end
--- spec/services/deal_maker_spec.rb
+++ spec/services/deal_maker_spec.rb
@@ -2,12 +2,43 @@ require 'rails_helper'
 
 describe DealMaker do
   let(:negotiation) { Fabricate.build(:negotiation) }
+  let(:deal_maker) { DealMaker.new(negotiation) }
   
-  xdescribe '#make_deal(negotiation)' do
+  describe '#make_deal(negotiation)' do
     context 'Negotiation is confirmed' do
+      before(:example) { allow(negotiation).to receive(:confirmer?) { true } }
+
+      it 'makes a new deal with the given negotiation state' do
+        deal=deal_maker.make_deal
+
+        expect(deal).to have_attributes(
+          user_ids:negotiation.user_ids,
+          proposals: negotiation.proposals,
+          messages: negotiation.messages
+          )
+      end
+
+      it 'destroys the negotiation' do
+        deal_maker.make_deal
+        expect(negotiation).to be_destroyed
+      end
     end
 
     context 'Negotiation is not confirmed' do
+      before(:example) { allow(negotiation).to receive(:confirmer?) { false } }
+      
+      it 'returns false' do
+        expect(deal_maker.make_deal).to eq false
+      end
+
+      it 'does not make a new deal' do
+        expect{deal_maker.make_deal}.not_to change{ Deal.count }
+      end
+
+      it 'does not destroy the negotiation' do
+        deal_maker.make_deal
+        expect(negotiation).not_to be_destroyed
+      end
     end
   end
 end
