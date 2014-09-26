c65b208e2bb709f02df2cb6c9d351d381bdad74f
Sergio <sergiodetorre@gmail.com>
2014-09-26 10:07:40 +0200

    remove elements and push it to components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,13 @@ module ApplicationHelper
     view_flow.content.delete(content_key)
   end
 
-  def element(name, view: :default, data:NullObject.new, state:nil)
-    base_element(name,view:view,data:data,state:state,
-      route:"elements")
-  end
+  def component(name, view: :default,data:NullObject.new,state:nil)
+    component = "components/#{name}/#{name}"
+    view = "components/#{name}/views/#{view}"
 
-  def component(name, view: :default, data:NullObject.new, state:nil)
-    base_element(name,view:view,data:data,state:state,
-      route:"elements/components")
+    render partial: component,
+    layout: view,
+    locals:{ name.to_sym=>data,state:state }
   end
 
   def my_page?
@@ -22,16 +21,5 @@ module ApplicationHelper
     active = false
     controller_names.each { |name| active ||= params[:controller] == name }
     active ? 'active-nav' : nil
-  end
-
-  private
-
-  def base_element(name, view:,data:,state:,route:)
-    element = "#{route}/#{name}/#{name}"
-    view = "#{route}/#{name}/views/#{view}"
-
-    render partial: element,
-    layout: view,
-    locals:{ name.to_sym=>data,state:state }
   end
 end
